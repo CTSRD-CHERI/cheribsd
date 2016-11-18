3adc090cb9ffdd02926c09cf89933883ffa6d67e
Brooks Davis <brooks@one-eyed-alien.net>
2016-11-18 16:06:57 +0000

    Declare SSL_get_selected_srtp_profile().

--- crypto/openssl/ssl/srtp.h
+++ crypto/openssl/ssl/srtp.h
@@ -137,6 +137,8 @@ int SSL_set_tlsext_use_srtp(SSL *ctx, const char *profiles);
 
 STACK_OF(SRTP_PROTECTION_PROFILE) *SSL_get_srtp_profiles(SSL *ssl);
 
+SRTP_PROTECTION_PROFILE *SSL_get_selected_srtp_profile(SSL *s);
+
 # endif
 
 #ifdef  __cplusplus
