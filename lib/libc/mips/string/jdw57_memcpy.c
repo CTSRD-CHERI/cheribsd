4506bf59958367493f3c3f9cbd1aef03345bed08
Jonathan Woodruff <jdw57@cam.ac.uk>
2017-10-20 12:50:10 +0100

    Add in overlap support for MEMMOVE_C case which was missing.

--- lib/libc/mips/string/jdw57_memcpy.c
+++ lib/libc/mips/string/jdw57_memcpy.c
@@ -131,7 +131,7 @@ bcopy(const void *src0, void *dst0, size_t length)
 #endif
 	size_t t;
 
-#if defined(MEMMOVE) || defined(BCOPY) || defined(CMEMMOVE)
+#if defined(MEMMOVE) || defined(BCOPY) || defined(CMEMMOVE) || defined(MEMMOVE_C)
 	const int handle_overlap = 1;
 #else
 	const int handle_overlap = 0;
