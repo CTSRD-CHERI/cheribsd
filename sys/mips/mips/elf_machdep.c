2865cc981d3766a4e1ea3d4314295c9dec38bdf6
Edward Tomasz Napierala <trasz@FreeBSD.org>
2017-11-13 13:17:00 +0000

    Unbreak PIE executables on MIPS by adding missing BI_CAN_EXEC_DYN.

--- sys/mips/mips/elf_machdep.c
+++ sys/mips/mips/elf_machdep.c
@@ -113,7 +113,7 @@ static Elf64_Brandinfo freebsd_brand_info = {
 	.sysvec		= &elf64_freebsd_sysvec,
 	.interp_newpath	= NULL,
 	.brand_note	= &elf64_freebsd_brandnote,
-	.flags		= BI_BRAND_NOTE,
+	.flags		= BI_CAN_EXEC_DYN | BI_BRAND_NOTE,
 #ifdef CPU_CHERI
 	.header_supported = mips_elf_header_supported
 #endif
@@ -182,7 +182,7 @@ static Elf32_Brandinfo freebsd_brand_info = {
 	.sysvec		= &elf32_freebsd_sysvec,
 	.interp_newpath	= NULL,
 	.brand_note	= &elf32_freebsd_brandnote,
-	.flags		= BI_BRAND_NOTE
+	.flags		= BI_CAN_EXEC_DYN | BI_BRAND_NOTE
 };
 
 SYSINIT(elf32, SI_SUB_EXEC, SI_ORDER_FIRST,
