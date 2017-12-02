684470011ce407838caea8d11ffe6884857854aa
John Baldwin <jhb@FreeBSD.org>
2017-12-02 10:56:15 -0800

    Add a custom implementation of __assert().
    timsort (used by mergesort for CheriABI) uses assert().  This was using
    the __assert() from libc which uses fprintf() which pulled stdio into
    rtld which breaks usage of stdio in binaries.  Instead, add a local
    version of __assert() which uses rtld_fdprintf() instead of fprintf().

--- libexec/rtld-cheri-elf/rtld.c
+++ libexec/rtld-cheri-elf/rtld.c
@@ -5296,6 +5296,21 @@ void (*__cleanup)(void);
 int __isthreaded = 0;
 int _thread_autoinit_dummy_decl = 1;
 
+void
+__assert(const char *func, const char *file, int line, const char *failedexpr)
+{
+	if (func == NULL)
+		rtld_fdprintf(STDERR_FILENO,
+		     "Assertion failed: (%s), file %s, line %d.\n", failedexpr,
+		     file, line);
+	else
+		rtld_fdprintf(STDERR_FILENO,
+		     "Assertion failed: (%s), function %s, file %s, line %d.\n",
+		     failedexpr, func, file, line);
+	abort();
+	/* NOTREACHED */
+}
+
 /*
  * No unresolved symbols for rtld.
  */
