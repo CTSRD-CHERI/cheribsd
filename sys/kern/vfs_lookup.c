0f7941abb06edf8780f4624532f9de13cc3d4355
Brooks Davis <brooks@one-eyed-alien.net>
2018-03-08 23:56:06 +0000

    Remove no-longer needed workarounds.

--- sys/kern/vfs_lookup.c
+++ sys/kern/vfs_lookup.c
@@ -325,24 +325,13 @@ namei(struct nameidata *ndp)
 	if ((cnp->cn_flags & HASBUF) == 0)
 		cnp->cn_pnbuf = uma_zalloc(namei_zone, M_WAITOK);
 	if (ndp->ni_segflg == UIO_SYSSPACE)
-/* XXX: work around CTSRD-CHERI/clang#157 */
-#if __has_feature(capabilities)
-		error = copystr((void *)(uintptr_t)cheri_getoffset(ndp->ni_dirp),
-#else
-		error = copystr(ndp->ni_dirp,
-#endif
+		error = copystr((__cheri_fromcap void *)ndp->ni_dirp,
 		    cnp->cn_pnbuf, MAXPATHLEN, &ndp->ni_pathlen);
 	else
-/* XXX: work around CTSRD-CHERI/clang#157 */
-#if __has_feature(capabilities)
 		error = copyinstr_c(ndp->ni_dirp,
-		    cheri_ptr(cnp->cn_pnbuf, MAXPATHLEN), MAXPATHLEN,
-		    cheri_ptr_to_bounded_cap(&ndp->ni_pathlen));
-#else
-		error = copyinstr_c(ndp->ni_dirp,
-		    (__cheri_tocap char * __CAPABILITY)cnp->cn_pnbuf, MAXPATHLEN,
-		    (__cheri_tocap size_t * __CAPABILITY)&ndp->ni_pathlen);
-#endif
+		    (__cheri_tocap char * __capability)cnp->cn_pnbuf,
+		    MAXPATHLEN,
+		    (__cheri_tocap size_t * __capability)&ndp->ni_pathlen);
 
 	/*
 	 * Don't allow empty pathnames.
