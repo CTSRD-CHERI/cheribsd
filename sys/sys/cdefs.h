dd6ca493ba744f84b9779b1c00af51eca388e467
Brooks Davis <brooks@one-eyed-alien.net>
2018-03-09 00:34:45 +0000

    Add a set of macros to create capablities from user pointers.
     - __USER_CAP(ptr, len): derive a capablity from userspace and bound it
     - __USER_CAP_OBJ(objp): derive a capability to a single C object and bound it
     - __USER_CAP_STR(str): derive capability to a string
     - __USER_CAP_UNBOUND(ptr): derive a capability from ddc
     - __USER_CAP_ARRAY(ptr, count): derive capability to an array of objects
       and bound it
     - __USER_CAP_ADDR(ptr): for things that are just used as an address
    
    NOTE: none of these currently set bounds.

--- sys/sys/cdefs.h
+++ sys/sys/cdefs.h
@@ -667,6 +667,39 @@
 #define	__DEQUALIFY_CAP		__DEQUALIFY
 #endif
 
+#ifndef __USER_CAP_UNBOUND
+#if __has_feature(capabilities)
+/*
+ * XXX-BD: should look up ddc and derive.
+ */
+#define	__USER_CAP_UNBOUND(ptr)	\
+    ((__cheri_tocap void * __capability)(ptr))
+#else
+#define	__USER_CAP_UNBOUND(ptr)	(ptr)
+#endif
+#endif
+
+#ifndef __USER_CAP
+#define	__USER_CAP(ptr, len)	__USER_CAP_UNBOUND(ptr)
+#endif
+#ifndef __USER_CAP_ADDR
+#define	__USER_CAP_ADDR(ptr)	__USER_CAP_UNBOUND(ptr)
+#endif
+#ifndef __USER_CAP_ARRAY
+#define	__USER_CAP_ARRAY(objp, cnt) \
+     __USER_CAP((objp), sizeof(*(objp)) * (cnt))
+#endif
+#ifndef __USER_CAP_OBJ
+#define	__USER_CAP_OBJ(objp)	__USER_CAP((objp), sizeof(*(objp)))
+#endif
+#ifndef __USER_CAP_STR
+/*
+ * NOTE: we can't place tigher bounds because we don't know what the
+ * length is until after we use it.
+ */
+#define	__USER_CAP_STR(strp)	__USER_CAP_UNBOUND(strp)
+#endif
+
 /*-
  * The following definitions are an extension of the behavior originally
  * implemented in <sys/_posix.h>, but with a different level of granularity.
