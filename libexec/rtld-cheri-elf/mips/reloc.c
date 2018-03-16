d8c29d10e12c5d365370fcf78dea077907b76cb5
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-03-16 15:03:52 +0000

    rtld-cheri-elf: Use make_function_pointer in R_CHERI_CAPABILITY processing
    This ensures that libraries that depend on a base zero $pcc don't crash.

--- libexec/rtld-cheri-elf/mips/reloc.c
+++ libexec/rtld-cheri-elf/mips/reloc.c
@@ -678,17 +678,20 @@ reloc_non_plt(Obj_Entry *obj, Obj_Entry *obj_rtld, int flags,
 			assert(ELF_ST_TYPE(def->st_info) != STT_GNU_IFUNC &&
 			    "IFUNC not implemented!");
 			// TODO: derive from correct permissions cap
-			void* symval = defobj->relocbase + def->st_value;
-			symval = cheri_csetbounds(symval, def->st_size);
+			void* symval = NULL;
 			uint64_t remove_perms = __CHERI_CAP_PERMISSION_PERMIT_SEAL__;
 			if (ELF_ST_TYPE(def->st_info) == STT_FUNC) {
+				symval = make_function_pointer(def, defobj);
 				/* remove write permission from functions */
 				remove_perms |= (__CHERI_CAP_PERMISSION_PERMIT_STORE__ |
 				    __CHERI_CAP_PERMISSION_PERMIT_STORE_CAPABILITY__ |
 				    __CHERI_CAP_PERMISSION_PERMIT_STORE_LOCAL__);
 			} else {
+				// TODO: add make_data_pointer() function
+				symval = defobj->relocbase + def->st_value;
 				/* remove execute permission from functions */
 				remove_perms |= __CHERI_CAP_PERMISSION_PERMIT_EXECUTE__;
+				symval = cheri_csetbounds(symval, def->st_size);
 			}
 			symval = cheri_andperm(symval, ~remove_perms);
 			/*
