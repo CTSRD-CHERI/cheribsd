5b65641480a5ccfee609c65ec73d1aada21f7730
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-03-28 13:20:15 +0100

    Use cheri_init_globals.h from clang instead of copying it 5 times
    It also has the advantage that it should work in cap-table mode . The
    current code does ` &__start___cap_relocs` which does not work before the
    cap-table has been initialized (it will just load a null value otherwise).

--- lib/csu/cheri/crt_init_globals.c
+++ lib/csu/cheri/crt_init_globals.c
@@ -32,28 +32,9 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#include <cheri_init_globals.h>
+
 void crt_init_globals(void) __hidden;
-
-struct capreloc
-{
-	uint64_t capability_location;
-	uint64_t object;
-	uint64_t offset;
-	uint64_t size;
-	uint64_t permissions;
-};
-static const uint64_t function_reloc_flag = 1ULL<<63;
-static const uint64_t function_pointer_permissions =
-	~0 &
-	~__CHERI_CAP_PERMISSION_PERMIT_STORE_CAPABILITY__ &
-	~__CHERI_CAP_PERMISSION_PERMIT_STORE__;
-static const uint64_t global_pointer_permissions =
-	~0 & ~__CHERI_CAP_PERMISSION_PERMIT_EXECUTE__;
-
-__attribute__((weak))
-extern struct capreloc __start___cap_relocs;
-__attribute__((weak))
-extern struct capreloc __stop___cap_relocs;
 
 __attribute__((weak)) extern int _DYNAMIC;
 
@@ -115,25 +96,6 @@ crt_init_globals(void)
 	if (_has__DYNAMIC)
 		return;
 #endif
-
-	void *gdc = __builtin_cheri_global_data_get();
-	void *pcc = __builtin_cheri_program_counter_get();
-
-	gdc = __builtin_cheri_perms_and(gdc, global_pointer_permissions);
-	pcc = __builtin_cheri_perms_and(pcc, function_pointer_permissions);
-	for (struct capreloc *reloc = &__start___cap_relocs ;
-	     reloc < &__stop___cap_relocs ; reloc++)
-	{
-		_Bool isFunction = (reloc->permissions & function_reloc_flag) ==
-			function_reloc_flag;
-		void **dest = __builtin_cheri_offset_set(gdc, reloc->capability_location);
-		void *base = isFunction ? pcc : gdc;
-		void *src = __builtin_cheri_offset_set(base, reloc->object);
-		if (!isFunction && (reloc->size != 0))
-		{
-			src = __builtin_cheri_bounds_set(src, reloc->size);
-		}
-		src = __builtin_cheri_offset_increment(src, reloc->offset);
-		*dest = src;
-	}
+	/* Otherwise we need to initialize globals manually */
+	cheri_init_globals();
 }
