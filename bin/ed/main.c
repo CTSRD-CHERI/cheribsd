d5c7c76e72071b8874e8e9aacf0f9dd084c168ea
Brooks Davis <brooks@one-eyed-alien.net>
2018-05-23 18:32:49 +0100

    Reduce diff to FreeBSD now that the compiler is ok with volatile argc.

--- bin/ed/main.c
+++ bin/ed/main.c
@@ -103,7 +103,7 @@ static const char *usage = "usage: %s [-] [-sx] [-p string] [file]\n";
 
 /* ed: line editor */
 int
-main(int argc, char ** argv)
+main(volatile int argc, char ** volatile argv)
 {
 	int c, n;
 	long status = 0;
