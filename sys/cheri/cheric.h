c36f3ca9c645bc1f1106f6127319a279f9b77f8d
Nathaniel Filardo <nwf20@cl.cam.ac.uk>
2018-07-13 11:39:24 +0100

    cheric.h: add CHERI_FPRINT_PTR for other stdio out

--- sys/cheri/cheric.h
+++ sys/cheri/cheric.h
@@ -203,9 +203,15 @@ cheri_bytes_remaining(const void * __capability cap)
 #define cheri_cap_to_typed_ptr(cap, type)				\
 	(type *)cheri_cap_to_ptr(cap, sizeof(type))
 
-
 #define CHERI_PRINT_PTR(ptr)						\
 	printf("%s: " #ptr " v:%lu b:%016jx l:%016zx o:%jx\n", __func__, \
+	   cheri_gettag((const void * __capability)(ptr)),	        \
+	   cheri_getbase((const void * __capability)(ptr)),		\
+	   cheri_getlen((const void * __capability)(ptr)),		\
+	   cheri_getoffset((const void * __capability)(ptr)))
+
+#define CHERI_FPRINT_PTR(f, ptr)					\
+	fprintf(f, "%s: " #ptr " v:%lu b:%016jx l:%016zx o:%jx\n", __func__, \
 	   cheri_gettag((const void * __capability)(ptr)),	        \
 	   cheri_getbase((const void * __capability)(ptr)),		\
 	   cheri_getlen((const void * __capability)(ptr)),		\
