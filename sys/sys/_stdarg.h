7acfa53dc769e216d98dd5a8eca601569e62db28
Brooks Davis <brooks@one-eyed-alien.net>
2018-08-17 16:07:12 +0100

    Attempt to work around va_start() etc definitions in clang headers.

--- sys/sys/_stdarg.h
+++ sys/sys/_stdarg.h
@@ -42,6 +42,7 @@
   typedef __va_list       va_list;
 #endif
 
+#ifndef va_start
 #ifdef __GNUCLIKE_BUILTIN_STDARG
   #define	va_start(ap, last)	__builtin_va_start((ap), (last))
   #define	va_arg(ap, type)	__builtin_va_arg((ap), type)
@@ -50,6 +51,7 @@
     #define	va_copy(dest, src)	__va_copy(dest, src)
   #endif
   #define	va_end(ap)		__builtin_va_end(ap)
+#endif
 #endif
 
 #if defined(lint) && !defined(va_start)
