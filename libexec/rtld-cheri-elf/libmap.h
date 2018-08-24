ed89d50acdf79f0a428eee8c7580bf91e8aeb4ce
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-08-24 12:18:49 +0100

    Reuse some rtld-elf files in rtld-cheri-elf
    Currently this is done with symlinks since it is not possible to include
    the headers from rtld-elf without creating compilation failures

--- libexec/rtld-cheri-elf/libmap.h
+++ libexec/rtld-cheri-elf/libmap.h
@@ -1,8 +1,1 @@
-/*
- * $FreeBSD$
- */
-
-int	lm_init (char *);
-void	lm_fini (void);
-char *	lm_find (const char *, const char *);
-char *	lm_findn (const char *, const char *, const int);
+../rtld-elf/libmap.h
