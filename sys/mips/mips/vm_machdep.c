1ff23b339e025b5537e01a8adbf043a57591d80d
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2018-10-19 16:48:51 +0100

    Always initialize thread mmap capability when compiling kernel with capability support.

--- sys/mips/mips/vm_machdep.c
+++ sys/mips/mips/vm_machdep.c
@@ -170,7 +170,7 @@ cpu_fork(struct thread *td1, struct proc *p2, struct thread *td2,int flags)
 	td2->td_md.md_saved_intr = MIPS_SR_INT_IE;
 	td2->td_md.md_spinlock_count = 1;
 #ifdef CPU_CHERI
-#ifdef COMPAT_CHERIABI
+#if __has_feature(capabilities)
 	td2->td_md.md_cheri_mmap_cap = td1->td_md.md_cheri_mmap_cap;
 #endif
 	/*
@@ -481,7 +481,7 @@ cpu_copy_thread(struct thread *td, struct thread *td0)
 	/* Setup to release spin count in in fork_exit(). */
 	td->td_md.md_spinlock_count = 1;
 	td->td_md.md_saved_intr = MIPS_SR_INT_IE;
-#if defined(CPU_CHERI) && defined(COMPAT_CHERIABI)
+#if defined(CPU_CHERI) && __has_feature(capabilities)
 	td->td_md.md_cheri_mmap_cap = td0->td_md.md_cheri_mmap_cap;
 #endif
 #if 0
