301a0023c50f77f05f9a5cf4e84e1eb76b9ddcaa
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2018-11-16 14:42:23 +0000

    Include default kernel sysstubs instead of compat/cheriabi sysstubs in libcheri.

--- lib/libcheri/libcheri_system.c
+++ lib/libcheri/libcheri_system.c
@@ -296,7 +296,7 @@ __libcheri_system_sys_##_sys _protoargs_err				\
 #pragma clang diagnostic ignored "-Wunused-parameter"
 #endif
 
-#include <compat/cheriabi/cheriabi_sysstubs.h>
+#include <sys/sysstubs.h>
 
 #ifndef __CHERI_PURE_CAPABILITY__
 #pragma clang diagnostic pop
