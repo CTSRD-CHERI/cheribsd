1da71d36b860dd20dac5ae1999f05cf2c5c2c445
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-01-07 14:48:33 +0000

    Fix warnings in cheriabitest

--- bin/cheritest/cheritest.h
+++ bin/cheritest/cheritest.h
@@ -498,11 +498,13 @@ DECLARE_CHERI_TEST(test_inflate_zeroes);
 DECLARE_CHERI_TEST(test_sandbox_inflate_zeroes);
 
 #ifdef CHERI_C_TESTS
-#define DECLARE_TEST(name, desc) \
-    void	cheri_c_test_ ## name(const struct cheri_test *ctp __unused);
-#define	DECLARE_TEST_FAULT(name, desc)	/* Not supported */
+#define	DECLARE_TEST(name, desc) \
+    void cheri_c_test_ ## name(const struct cheri_test *ctp __unused);
+#define DECLARE_TEST_FAULT(name, desc)	\
+    void cheri_c_test_ ## name(const struct cheri_test *ctp __unused);
 #include <cheri_c_testdecls.h>
 #undef DECLARE_TEST
+#undef DECLARE_TEST_FAULT
 #endif
 
 #endif /* !_CHERITEST_H_ */
