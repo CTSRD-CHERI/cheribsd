CTSRD-CHERI/cheribsd

commit 40ff7aecd098046038f0a2dca70bae9e1a9bcc3e

Fix malloc having assertions enabled only for purecap
Due to the #include of <assert.h> in sys/cheric.h we were redefining
the jemalloc assert() (which is a no-op with MALLOC_PRODUCTION) to a real
check.
After removing the assert.h include from cheric.h jemalloc no longer has
assertions enable for CHERI. This should hopefully result in much better
performance numbers.

I have now used some preprocessor magic to ensure that assert()
always expands to je_assert() inside jemalloc so that we get consistent
performance between MIPS and CHERI.

sys/cheri/cheric.h
@@ -298,13 +298,15 @@ __cheri_clear_low_ptr_bits(uintptr_t ptr, size_t bits_mask) {
 /* Turn on the checking by default for now (until we have fixed everything)*/
 #define __check_low_ptr_bits_assignment
 #if defined(_KERNEL) /* Don't pull in assert.h when building the kernel */
-#undef __check_low_ptr_bits_assignment
+#define _cheri_bits_assert(e) (void)0
 #endif
 #ifdef __check_low_ptr_bits_assignment
-#include <assert.h>
+#ifndef _cheri_bits_assert
+#define _cheri_bits_assert(e) assert(e)
+#endif
 #define __runtime_assert_sensible_low_bits(bits)                               \
   __extension__({                                                              \
-    assert(bits < 32 && "Should only use the low 5 pointer bits");             \
+    _cheri_bits_assert(bits < 32 && "Should only use the low 5 pointer bits"); \
     bits;                                                                      \
   })
 #else

contrib/jemalloc/include/jemalloc/internal/assert.h
@@ -5,8 +5,8 @@
  * Define a custom assert() in order to reduce the chances of deadlock during
  * assertion failure.
  */
-#ifndef assert
-#define assert(e) do {							\
+#ifndef je_assert
+#define je_assert(e) do {						\
 	if (unlikely(config_debug && !(e))) {				\
 		malloc_printf(						\
 		    "<jemalloc>: %s:%d: Failed assertion: \"%s\"\n",	\
@@ -14,6 +14,16 @@
 		abort();						\
 	}								\
 } while (0)
+#define _assert_macro_expansion_is_je_assert 1
+#define _assert_macro_expansion_is_assert "should expand to je_assert() and not assert()"
+#endif
+
+#ifndef assert
+/*
+ * Note: This is not declared as a function-like macro to allow checking that
+ * assert is defined to je_assert
+ */
+#define assert je_assert
 #endif
 
 #ifndef not_reached

contrib/jemalloc/include/jemalloc/internal/jemalloc_internal_includes.h
@@ -95,4 +95,8 @@
 #include "jemalloc/internal/prof_inlines_b.h"
 #include "jemalloc/internal/background_thread_inlines.h"
 
+/* Check that we are using je_assert instead of assert(): */
+_Static_assert(__CONCAT(_assert_macro_expansion_is_, assert) == 1,
+    "Should be using je_assert and not assert() from assert.h");
+
 #endif /* JEMALLOC_INTERNAL_INCLUDES_H */

commit 85a23b5e2497ece8e8d59d8f7a2dcc4542b101bc

cheric.h: #include <assert.h> if we need assert but it's not defined
This fixes 40ff7aecd0, which broke building opensolaris bits due to
assert becoming an implicitly-defined function with no prototype.

sys/cheri/cheric.h
@@ -302,6 +302,9 @@ __cheri_clear_low_ptr_bits(uintptr_t ptr, size_t bits_mask) {
 #endif
 #ifdef __check_low_ptr_bits_assignment
 #ifndef _cheri_bits_assert
+#ifndef assert
+#include <assert.h>
+#endif
 #define _cheri_bits_assert(e) assert(e)
 #endif
 #define __runtime_assert_sensible_low_bits(bits)                               \