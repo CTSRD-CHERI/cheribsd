9d25e4c432c22146b7b297b1441ad49e5bb87d7d
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2019-06-03 10:06:27 +0100

    Fix pointer to integer conversion and manipulation in vfs_cluster.

--- sys/kern/vfs_cluster.c
+++ sys/kern/vfs_cluster.c
@@ -397,8 +397,8 @@ cluster_rbuild(struct vnode *vp, u_quad_t filesize, daddr_t lbn,
 	if ((gbflags & GB_UNMAPPED) != 0) {
 		bp->b_data = unmapped_buf;
 	} else {
-		bp->b_data = (char *)((vm_offset_t)bp->b_data |
-		    ((vm_offset_t)tbp->b_data & PAGE_MASK));
+		bp->b_data = (char *)((uintptr_t)bp->b_data |
+		    (ptr_to_va(tbp->b_data) & PAGE_MASK));
 	}
 	bp->b_iocmd = BIO_READ;
 	bp->b_iodone = cluster_callback;
@@ -899,8 +899,8 @@ cluster_wbuild(struct vnode *vp, long size, daddr_t start_lbn, int len,
 		 */
 		if ((gbflags & GB_UNMAPPED) == 0 ||
 		    (tbp->b_flags & B_VMIO) == 0) {
-			bp->b_data = (char *)((vm_offset_t)bp->b_data |
-			    ((vm_offset_t)tbp->b_data & PAGE_MASK));
+			bp->b_data = (char *)((uintptr_t)bp->b_data |
+			    (ptr_to_va(tbp->b_data) & PAGE_MASK));
 		} else {
 			bp->b_data = unmapped_buf;
 		}
