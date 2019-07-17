9f5ff2c73120ad00742222d30f3ae46cea4f9c3e
Brooks Davis <brooks@one-eyed-alien.net>
2019-07-17 20:10:03 +0100

    Revert "Add some prototypes to libsm to fix new compiler error"
    We now support K&R declerations so this reduces diffs to upstream.
    
    It's a little unclear why the incorrect casts to function pointers in
    vfscanf.c are ok, but they compile and the actual call should be correct
    due to the variable's type.
    
    This reverts commit 6ecc349cf152c73e501c667374d33b59221037de.

--- contrib/sendmail/libsm/vfscanf.c
+++ contrib/sendmail/libsm/vfscanf.c
@@ -97,7 +97,8 @@ static jmp_buf		ScanTimeOut;
 
 /* ARGSUSED0 */
 static void
-scanalrm(int sig)
+scanalrm(sig)
+	int sig;
 {
 	longjmp(ScanTimeOut, 1);
 }
@@ -251,13 +252,13 @@ literal:
 			/* FALLTHROUGH */
 		  case 'd':
 			c = CT_INT;
-			ccfn = (ULONGLONG_T (*)(const char *, char **, int))sm_strtoll;
+			ccfn = (ULONGLONG_T (*)())sm_strtoll;
 			base = 10;
 			break;
 
 		  case 'i':
 			c = CT_INT;
-			ccfn = (ULONGLONG_T (*)(const char *, char **, int))sm_strtoll;
+			ccfn = (ULONGLONG_T (*)())sm_strtoll;
 			base = 0;
 			break;
 
@@ -335,7 +336,7 @@ literal:
 			if (isupper(c))
 				flags |= LONG;
 			c = CT_INT;
-			ccfn = (ULONGLONG_T (*)(const char *, char **, int)) sm_strtoll;
+			ccfn = (ULONGLONG_T (*)()) sm_strtoll;
 			base = 10;
 			break;
 		}
