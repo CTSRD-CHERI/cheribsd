88b06c1f9eb40487e9351c7f3534ddad10b85ec3
Brooks Davis <brooks@one-eyed-alien.net>
2019-08-12 18:21:50 +0100

    Copy sival with memcpy.
    They types always pun even if they don't match.

--- sys/sys/signalvar.h
+++ sys/sys/signalvar.h
@@ -314,7 +314,7 @@ static __inline void
 ksiginfo_set_sigev(ksiginfo_t *dst, ksigevent_t *sigev)
 {
 	dst->ksi_signo = sigev->sigev_signo;
-	dst->ksi_value = sigev->sigev_value;
+	memcpy(&dst->ksi_value, &sigev->sigev_value, sizeof(dst->ksi_value));
 }
 
 struct pgrp;
