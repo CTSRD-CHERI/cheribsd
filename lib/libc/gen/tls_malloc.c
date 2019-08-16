15591eedad194d712cbd6e70eee9fe6bcc345961
Brooks Davis <brooks@one-eyed-alien.net>
2019-08-16 23:43:29 +0100

    Handle alignment to ensure precise bounds.
    Use the tls_malloc_aligned machinery as required to return appropriatly
    aligned things from the un-aligned interfaces.  Detect when that has happend
    in find_overhead() and compensate accordingly.  In the pure-capability
    case let tls_free_aligned() just call tls_free().
    
    Bound to the requested size (plus rounding up) in tls_*alloc_aligned
    rather than exposing malloc internals.

--- lib/libc/gen/tls_malloc.c
+++ lib/libc/gen/tls_malloc.c
@@ -74,6 +74,9 @@
 
 #ifndef __CHERI_PURE_CAPABILITY__
 #define	cheri_csetbounds(ptr, size)	((void *)(ptr))
+#define	cheri_andperm(ptr, size)	((void *)(ptr))
+#define	CHERI_PERMS_USERSPACE_DATA	0
+#define	CHERI_PERM_CHERIABI_VMMAP	0
 #endif
 
 static spinlock_t tls_malloc_lock = _SPINLOCK_INITIALIZER;
@@ -81,6 +84,7 @@ static spinlock_t tls_malloc_lock = _SPINLOCK_INITIALIZER;
 #define	TLS_MALLOC_UNLOCK	_SPINUNLOCK(&tls_malloc_lock)
 union overhead;
 static void morecore(int);
+static void *__tls_malloc_aligned(size_t size, size_t align);
 
 /*
  * The overhead on a block is one pointer. When free, this space
@@ -96,6 +100,8 @@ union	overhead {
 #define	ov_magic	ovu.ovu_magic
 #define	ov_index	ovu.ovu_index
 };
+
+#define	MALLOC_ALIGNMENT	sizeof(union overhead)
 
 #define	MAGIC		0xef		/* magic # on accounting info */
 
@@ -207,8 +213,19 @@ __rederive_pointer(void *ptr)
 #endif
 }
 
-void *
-tls_malloc(size_t nbytes)
+static void *
+bound_ptr(void *mem, size_t nbytes)
+{
+	void *ptr;
+
+	ptr = cheri_csetbounds(mem, nbytes);
+	ptr = cheri_andperm(ptr,
+	    CHERI_PERMS_USERSPACE_DATA & ~CHERI_PERM_CHERIABI_VMMAP);
+	return (ptr);
+}
+
+static void *
+__tls_malloc(size_t nbytes)
 {
 	union overhead *op;
 	int bucket;
@@ -244,7 +261,26 @@ tls_malloc(size_t nbytes)
 	TLS_MALLOC_UNLOCK;
 	op->ov_magic = MAGIC;
 	op->ov_index = bucket;
-	return (cheri_csetbounds(op + 1, nbytes));
+	return (op + 1);
+}
+
+void *
+tls_malloc(size_t nbytes)
+{
+	void *mem;
+#ifdef __CHERI_PURE_CAPABILITY__
+	size_t align, mask;
+
+	nbytes = __builtin_cheri_round_representable_length(nbytes);
+	mask = __builtin_cheri_representable_alignment_mask(nbytes);
+	align = 1 + ~mask;
+
+	if (mask != SIZE_MAX && align > MALLOC_ALIGNMENT)
+		mem = __tls_malloc_aligned(nbytes, align);
+	else
+#endif
+		mem = __tls_malloc(nbytes);
+	return (bound_ptr(mem, nbytes));
 }
 
 void *
@@ -317,6 +353,25 @@ find_overhead(void * cp)
 		return (NULL);
 	op--;
 
+#ifdef __CHERI_PURE_CAPABILITY__
+	/*
+	 * In pure-capability mode our allocation might have come from
+	 * __tls_malloc_aligned.  In that case we need to get back to the
+	 * real overhead pointer.  To make sure we aren't tricked, the
+	 * pointer must:
+	 *  - Be an internal allocator pointer (have the VMMAP permision).
+	 *  - Point somewhere before us and within the current pagepool.
+	 */
+	if (cheri_gettag(op->ov_next) &&
+	    (cheri_getperm(op->ov_next) & CHERI_PERM_CHERIABI_VMMAP) != 0) {
+		vaddr_t base, pp_base;
+
+		pp_base = cheri_getbase(op);
+		base = cheri_getbase(op->ov_next);
+		if (base >= pp_base && base < cheri_getaddress(op))
+			op = op->ov_next;
+	}
+#endif
 	if (op->ov_magic == MAGIC)
 		return (op);
 
@@ -349,19 +404,34 @@ tls_free(void *cp)
 	TLS_MALLOC_UNLOCK;
 }
 
-void *
-tls_malloc_aligned(size_t size, size_t align)
+static void *
+__tls_malloc_aligned(size_t size, size_t align)
 {
 	vaddr_t memshift;
 	void *mem, *res;
 	if (align < sizeof(void *))
 		align = sizeof(void *);
 
-	mem = tls_malloc(size + sizeof(void *) + align - 1);
+	mem = __tls_malloc(size + sizeof(void *) + align - 1);
 	memshift = roundup2((vaddr_t)mem + sizeof(void *), align) - (vaddr_t)mem;
 	res = (void *)((uintptr_t)mem + memshift);
 	*(void **)((uintptr_t)res - sizeof(void *)) = mem;
 	return (res);
+}
+
+void *
+tls_malloc_aligned(size_t nbytes, size_t align)
+{
+#ifdef __CHERI_PURE_CAPABILITY__
+	size_t mask;
+
+	nbytes = __builtin_cheri_round_representable_length(nbytes);
+	mask = __builtin_cheri_representable_alignment_mask(nbytes);
+	if (align < 1 + ~mask)
+		align = 1 + ~mask;
+#endif
+	return (bound_ptr(__tls_malloc_aligned(nbytes, align), nbytes));
+
 }
 
 void *
@@ -380,13 +450,16 @@ void
 tls_free_aligned(void *ptr)
 {
 	void *mem;
+#ifndef __CHERI_PURE_CAPABILITY__
 	uintptr_t x;
 
 	if (ptr == NULL)
 		return;
-
 	x = (uintptr_t)ptr;
 	x -= sizeof(void *);
 	mem = *(void **)x;
+#else
+	mem = ptr;
+#endif
 	tls_free(mem);
 }
