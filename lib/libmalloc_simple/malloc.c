f43496f993be00d90981f5262595851e132fc366
Brooks Davis <brooks@one-eyed-alien.net>
2019-09-05 14:33:07 +0100

    libmalloc_simple: Add revocation.
    Memory is queued for revocation on free() and revocation is triggered on
    allocation when memory would have to be allocated by mmap() and
    appropriate memory is in quarantine.

--- lib/libmalloc_simple/malloc.c
+++ lib/libmalloc_simple/malloc.c
@@ -45,8 +45,15 @@ static char *rcsid = "$FreeBSD$";
  */
 
 #include <sys/param.h>
+#ifdef CAPREVOKE
+#include <sys/caprevoke.h>
+#include <sys/stdatomic.h>
+#endif
 #include <sys/types.h>
 
+#ifdef CAPREVOKE
+#include <cheri/libcaprevoke.h>
+#endif
 #include <cheri/cheri.h>
 #include <cheri/cheric.h>
 
@@ -98,9 +105,23 @@ union	overhead {
  * (FIRST_BUCKET_SIZE << i).  The overhead information precedes the
  * data area returned to the user.
  */
-#define	FIRST_BUCKET_SIZE	32
+#define	FIRST_BUCKET_SHIFT	5
+#define	FIRST_BUCKET_SIZE	(1 << FIRST_BUCKET_SHIFT)
 #define	NBUCKETS 30
 static	union overhead *nextf[NBUCKETS];
+
+#ifdef CAPREVOKE
+/*
+ * In the CAPREVOKE case, we encode the bucket in the next pointer's low
+ * bit in the quarantine pool.  The static assert ensures that remains
+ * possible.
+ */
+_Static_assert(NBUCKETS < FIRST_BUCKET_SIZE,
+    "Not enough alignment to encode bucket in pointer bits");
+#define	MAX_QUARANTINE	(1024 * 1024)
+static	union overhead *quarantine_bufs[NBUCKETS];
+static	size_t quarantine_size;
+#endif
 
 static	size_t pagesz;			/* page size */
 
@@ -130,6 +151,46 @@ bound_ptr(void *mem, size_t nbytes)
 	    CHERI_PERMS_USERSPACE_DATA & ~CHERI_PERM_CHERIABI_VMMAP);
 	return (ptr);
 }
+
+#ifdef CAPREVOKE
+static void
+try_revoke(int target_bucket)
+{
+	int bucket;
+	union overhead *op, *next_op;
+
+	/*
+	 * Don't revoke unless there is enough in quarantine and some of
+	 * it would be returned to the bucket we want to refill.
+	 */
+	if (quarantine_bufs[target_bucket] == NULL ||
+	    quarantine_size < MAX_QUARANTINE)
+	    return;
+
+	for (bucket = 0; bucket < NBUCKETS; bucket++)
+		for (op = quarantine_bufs[bucket]; op != NULL;
+		    op = op->ov_next)
+			__paint_shadow(op, FIRST_BUCKET_SIZE << bucket);
+	atomic_thread_fence(memory_order_acq_rel);
+
+	__do_revoke();
+
+	for (bucket = 0; bucket < NBUCKETS; bucket++)
+		for (op = quarantine_bufs[bucket]; op != NULL;
+		    op = op->ov_next)
+			__clear_shadow(op, FIRST_BUCKET_SIZE << bucket);
+	atomic_thread_fence(memory_order_acq_rel);
+
+	for (bucket = 0; bucket < NBUCKETS; bucket++) {
+		op = quarantine_bufs[bucket];
+		while (op != NULL) {
+			next_op = op->ov_next;
+			op->ov_next = nextf[bucket];
+			nextf[bucket] = op;
+		}
+	}
+}
+#endif
 
 static void *
 __simple_malloc_unaligned(size_t nbytes)
@@ -166,6 +227,10 @@ __simple_malloc_unaligned(size_t nbytes)
 	 * If nothing in hash bucket right now,
 	 * request more memory from the system.
 	 */
+#ifdef CAPREVOKE
+	if ((op = nextf[bucket]) == NULL)
+		try_revoke(bucket);
+#endif
 	if ((op = nextf[bucket]) == NULL) {
 		morecore(bucket);
 		if ((op = nextf[bucket]) == NULL)
@@ -340,8 +405,14 @@ __simple_free(void *cp)
 		return;
 	bucket = op->ov_index;
 	ASSERT(bucket < NBUCKETS);
+#ifdef CAPREVOKE
+	op->ov_next = quarantine_bufs[bucket];
+	quarantine_bufs[bucket] = op;
+	quarantine_size += FIRST_BUCKET_SIZE << bucket;
+#else
 	op->ov_next = nextf[bucket];	/* also clobbers ov_magic */
 	nextf[bucket] = op;
+#endif
 }
 
 static void *
