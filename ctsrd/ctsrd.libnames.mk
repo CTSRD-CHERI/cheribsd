8caa596bb81780f7ed7397dcff5d5780204a6275
Brooks Davis <brooks@one-eyed-alien.net>
2019-11-21 23:25:51 +0000

    Remove unused libvuln_png.

--- ctsrd/ctsrd.libnames.mk
+++ ctsrd/ctsrd.libnames.mk
@@ -1,6 +1,5 @@
-LOCAL_LIBRARIES+=	terasic_mtl vuln_magic vuln_png
+LOCAL_LIBRARIES+=	terasic_mtl vuln_magic
 
-# Depends on png, but don't include to all png or vuln_png to be used.
 LIBTERASIC_MTLDIR=	ctsrd/lib/libterasic_mtl
 LIBTERASIC_MTL?=	${DESTDIR}${LIBDIR}/libterasic_mtl.a
 _DP_terasic_mtl=	png
@@ -8,7 +7,3 @@ _DP_terasic_mtl=	png
 LIBVULN_MAGICDIR=	ctsrd/lib/libvuln_magic
 LIBVULN_MAGIC?=		${DESTDIR}${LIBDIR}/libvuln_magic.a
 _DP_vuln_magic=		z
-
-LIBVULN_PNGDIR=		ctsrd/lib/libvuln_png
-LIBVULN_PNG?=		${DESTDIR}${LIBDIR}/libvuln_png.a
-_DP_vuln_png=		z
