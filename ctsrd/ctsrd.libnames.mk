36dc406f080eaa50b3d0db0685eb9edfc5bef4a7
Brooks Davis <brooks@one-eyed-alien.net>
2019-11-21 23:30:59 +0000

    Remove unused libvuln_magic.

--- ctsrd/ctsrd.libnames.mk
+++ ctsrd/ctsrd.libnames.mk
@@ -1,9 +1,5 @@
-LOCAL_LIBRARIES+=	terasic_mtl vuln_magic
+LOCAL_LIBRARIES+=	terasic_mtl
 
 LIBTERASIC_MTLDIR=	ctsrd/lib/libterasic_mtl
 LIBTERASIC_MTL?=	${DESTDIR}${LIBDIR}/libterasic_mtl.a
 _DP_terasic_mtl=	png
-
-LIBVULN_MAGICDIR=	ctsrd/lib/libvuln_magic
-LIBVULN_MAGIC?=		${DESTDIR}${LIBDIR}/libvuln_magic.a
-_DP_vuln_magic=		z
