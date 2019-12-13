b2fceb355b7396685c9a7987cf9bc157e76931b8
Brooks Davis <brooks@one-eyed-alien.net>
2019-12-13 21:55:58 +0000

    When unmapping pages, replace them with guard-like pages.
    Introduce a new MAP_ENTRY_UNMAPPED flag for vm map entries with semantics
    nearly identical to MAP_ENTRY_GUARD.  The differences are that it can't
    be used to guard unallocated stack space and mappings are only created during
    unmap.  (They will be used to pad oddly sized mappings in future
    commits.)
    
    In this implementation, don't actually unmap any portion of a
    reservation (except temporarily with the map lock held) until the whole
    thing has been unmapped.

--- sys/vm/vm_map.c
+++ sys/vm/vm_map.c
@@ -1691,6 +1691,8 @@ vm_map_insert(vm_map_t map, vm_object_t object, vm_ooffset_t offset,
 		protoeflags |= MAP_ENTRY_VN_EXEC;
 	if ((cow & MAP_CREATE_GUARD) != 0)
 		protoeflags |= MAP_ENTRY_GUARD;
+	if ((cow & MAP_CREATE_UNMAPPED) != 0)
+		protoeflags |= MAP_ENTRY_UNMAPPED;
 	if ((cow & MAP_CREATE_STACK_GAP_DN) != 0)
 		protoeflags |= MAP_ENTRY_STACK_GAP_DN;
 	if ((cow & MAP_CREATE_STACK_GAP_UP) != 0)
@@ -1757,7 +1759,8 @@ charged:
 			KASSERT((prev_entry->eflags & MAP_ENTRY_USER_WIRED) ==
 			    0, ("prev_entry %p has incoherent wiring",
 			    prev_entry));
-			if ((prev_entry->eflags & MAP_ENTRY_GUARD) == 0)
+			if ((prev_entry->eflags & (MAP_ENTRY_GUARD |
+			    MAP_ENTRY_UNMAPPED)) == 0)
 				map->size += end - prev_entry->end;
 			vm_map_entry_resize(map, prev_entry,
 			    end - prev_entry->end);
@@ -1814,7 +1817,7 @@ charged:
 	 * Insert the new entry into the list
 	 */
 	vm_map_entry_link(map, new_entry);
-	if ((new_entry->eflags & MAP_ENTRY_GUARD) == 0)
+	if ((new_entry->eflags & (MAP_ENTRY_GUARD | MAP_ENTRY_UNMAPPED)) == 0)
 		map->size += new_entry->end - new_entry->start;
 
 	vm_map_log("insert", new_entry);
@@ -2365,7 +2368,7 @@ vm_map_entry_charge_object(vm_map_t map, vm_map_entry_t entry)
 	KASSERT((entry->eflags & MAP_ENTRY_IS_SUB_MAP) == 0,
 	    ("map entry %p is a submap", entry));
 	if (entry->object.vm_object == NULL && !map->system_map &&
-	    (entry->eflags & MAP_ENTRY_GUARD) == 0)
+	    (entry->eflags & (MAP_ENTRY_GUARD | MAP_ENTRY_UNMAPPED)) == 0)
 		vm_map_entry_back(entry);
 	else if (entry->object.vm_object != NULL &&
 	    ((entry->eflags & MAP_ENTRY_NEEDS_COPY) == 0) &&
@@ -2707,7 +2710,8 @@ again:
 	 */
 	for (entry = first_entry; entry->start < end;
 	    entry = vm_map_entry_succ(entry)) {
-		if ((entry->eflags & MAP_ENTRY_GUARD) != 0)
+		if ((entry->eflags &
+		    (MAP_ENTRY_GUARD | MAP_ENTRY_UNMAPPED)) != 0)
 			continue;
 		if ((entry->eflags & MAP_ENTRY_IS_SUB_MAP) != 0) {
 			vm_map_unlock(map);
@@ -2750,7 +2754,8 @@ again:
 		if (set_max ||
 		    ((new_prot & ~entry->protection) & VM_PROT_WRITE) == 0 ||
 		    ENTRY_CHARGED(entry) ||
-		    (entry->eflags & MAP_ENTRY_GUARD) != 0) {
+		    (entry->eflags &
+		    (MAP_ENTRY_GUARD | MAP_ENTRY_UNMAPPED)) != 0) {
 			continue;
 		}
 
@@ -2808,7 +2813,8 @@ again:
 	    vm_map_try_merge_entries(map, prev_entry, entry),
 	    prev_entry = entry, entry = vm_map_entry_succ(entry)) {
 		if (rv != KERN_SUCCESS ||
-		    (entry->eflags & MAP_ENTRY_GUARD) != 0)
+		    (entry->eflags &
+		    (MAP_ENTRY_GUARD | MAP_ENTRY_UNMAPPED)) != 0)
 			continue;
 
 		old_prot = entry->protection;
@@ -3070,7 +3076,8 @@ vm_map_inherit(vm_map_t map, vm_offset_t start, vm_offset_t end,
 	    entry->start < end;
 	    prev_entry = entry, entry = vm_map_entry_succ(entry)) {
 		vm_map_clip_end(map, entry, end);
-		if ((entry->eflags & MAP_ENTRY_GUARD) == 0 ||
+		if ((entry->eflags &
+		    (MAP_ENTRY_GUARD | MAP_ENTRY_UNMAPPED)) == 0 ||
 		    new_inheritance != VM_INHERIT_ZERO)
 			entry->inheritance = new_inheritance;
 		vm_map_try_merge_entries(map, prev_entry, entry);
@@ -3824,7 +3831,7 @@ vm_map_entry_delete(vm_map_t map, vm_map_entry_t entry)
 	vm_map_entry_unlink(map, entry, UNLINK_MERGE_NONE);
 	object = entry->object.vm_object;
 
-	if ((entry->eflags & MAP_ENTRY_GUARD) != 0) {
+	if ((entry->eflags & (MAP_ENTRY_GUARD | MAP_ENTRY_UNMAPPED)) != 0) {
 		MPASS(entry->cred == NULL);
 		MPASS((entry->eflags & MAP_ENTRY_IS_SUB_MAP) == 0);
 		MPASS(object == NULL);
@@ -4205,7 +4212,7 @@ vmspace_map_entry_forked(const struct vmspace *vm1, struct vmspace *vm2,
 	vm_size_t entrysize;
 	vm_offset_t newend;
 
-	if ((entry->eflags & MAP_ENTRY_GUARD) != 0)
+	if ((entry->eflags & (MAP_ENTRY_GUARD | MAP_ENTRY_UNMAPPED)) != 0)
 		return;
 	entrysize = entry->end - entry->start;
 	vm2->vm_map.size += entrysize;
@@ -4278,7 +4285,8 @@ vmspace_fork(struct vmspace *vm1, vm_ooffset_t *fork_charge)
 			panic("vm_map_fork: encountered a submap");
 
 		inh = old_entry->inheritance;
-		if ((old_entry->eflags & MAP_ENTRY_GUARD) != 0 &&
+		if ((old_entry->eflags &
+		    (MAP_ENTRY_GUARD | MAP_ENTRY_UNMAPPED)) != 0 &&
 		    inh != VM_INHERIT_NONE)
 			inh = VM_INHERIT_COPY;
 
@@ -5177,6 +5185,33 @@ vm_map_pmap_KBI(vm_map_t map)
 {
 
 	return (map->pmap);
+}
+
+int
+vm_map_reservation_delete(vm_map_t map, vm_offset_t reservation)
+{
+	vm_map_entry_t	entry, next_entry;
+
+	if (!vm_map_lookup_entry(map, reservation, &entry))
+		return (KERN_FAILURE);
+
+	while (entry->reservation == reservation) {
+		next_entry = vm_map_entry_succ(entry);
+		vm_map_entry_delete(map, entry);
+	}
+	return (KERN_SUCCESS);
+}
+
+bool
+vm_map_reservation_is_unmapped(vm_map_t map, vm_offset_t reservation)
+{
+	vm_map_entry_t	entry;
+
+	if (vm_map_lookup_entry(map, reservation, &entry) &&
+	    entry->reservation == entry->start &&
+	    entry->reservation != vm_map_entry_succ(entry)->reservation)
+		return (true);
+	return (false);
 }
 
 #ifdef INVARIANTS
