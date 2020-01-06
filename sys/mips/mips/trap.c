6ee9b7f6889666ad78452d54675e6d9d9000f9f6
John Baldwin <jhb@FreeBSD.org>
2020-01-06 12:46:34 -0800

    Fix emulation of CHERI branch instructions.
    The break only exited one level of switch and fell through to the
    default case.

--- sys/mips/mips/trap.c
+++ sys/mips/mips/trap.c
@@ -1637,7 +1637,7 @@ MipsEmulateBranch(struct trapframe *framePtr, trapf_pc_t _instPC, int fpcCSR,
 				retAddr = GetBranchDest(instPC, inst);
 			else
 				retAddr = instPC + 8;
-			break;
+			return (trapf_pc_t)(retAddr);
 		}
 		/* FALLTHROUGH */
 #endif
