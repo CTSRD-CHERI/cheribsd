CTSRD-CHERI/cheribsd

commit ff61324a8b54ff418ffefefdbe39736120d98f2a

Use hybridabi_newthread_setregs() to set $pcc in the CHERI case.
This fixes a panic in the kernel when hybrid processes create threads.

sys/mips/mips/vm_machdep.c
@@ -516,8 +516,19 @@ cpu_set_upcall(struct thread *td, void (*entry)(void *), void *arg,
 	tf = td->td_frame;
 	bzero(tf, sizeof(struct trapframe));
 	tf->sp = sp;
-	// FIXME: this is wrong for the purecap kernel
+
+	/* XXXRW: With CNMIPS moved, does this still belong here? */
+#if __has_feature(capabilities)
+	/*
+	 * For the MIPS ABI, we can derive any required CHERI state from
+	 * the completed MIPS trapframe and existing process state.
+	 */
+	tf->sr |= MIPS_SR_COP_2_BIT;
+	hybridabi_newthread_setregs(td, (uintptr_t)entry);
+#else
+	/* For CHERI $pcc is set by hybridabi_newthread_setregs() */
 	TRAPF_PC_SET_ADDR(tf, (vaddr_t)(intptr_t)entry);
+#endif
 	/* 
 	 * MIPS ABI requires T9 to be the same as PC 
 	 * in subroutine entry point
@@ -539,15 +550,6 @@ cpu_set_upcall(struct thread *td, void (*entry)(void *), void *arg,
 	td->td_frame->sr |= MIPS_SR_PX | MIPS_SR_UX | MIPS_SR_KX;
 #endif
 
-	/* XXXRW: With CNMIPS moved, does this still belong here? */
-#ifdef CPU_CHERI
-	/*
-	 * For the MIPS ABI, we can derive any required CHERI state from
-	 * the completed MIPS trapframe and existing process state.
-	 */
-	tf->sr |= MIPS_SR_COP_2_BIT;
-	hybridabi_newthread_setregs(td, (uintptr_t)entry);
-#endif
 /*	tf->sr |= (ALL_INT_MASK & idle_mask) | SR_INT_ENAB; */
 	/**XXX the above may now be wrong -- mips2 implements this as panic */
 	/*

commit d8d8de4035faeadb945be39c31b3a93761de88af

Fix a regression in ff61324a8b that broke thread creation.
Enable COP2 in SR after SR is re-derived.

sys/mips/mips/vm_machdep.c
@@ -517,13 +517,11 @@ cpu_set_upcall(struct thread *td, void (*entry)(void *), void *arg,
 	bzero(tf, sizeof(struct trapframe));
 	tf->sp = sp;
 
-	/* XXXRW: With CNMIPS moved, does this still belong here? */
 #if __has_feature(capabilities)
 	/*
 	 * For the MIPS ABI, we can derive any required CHERI state from
 	 * the completed MIPS trapframe and existing process state.
 	 */
-	tf->sr |= MIPS_SR_COP_2_BIT;
 	hybridabi_newthread_setregs(td, (uintptr_t)entry);
 #else
 	/* For CHERI $pcc is set by hybridabi_newthread_setregs() */
@@ -548,6 +546,10 @@ cpu_set_upcall(struct thread *td, void (*entry)(void *), void *arg,
 	td->td_frame->sr |= MIPS_SR_PX;
 #elif  defined(__mips_n64)
 	td->td_frame->sr |= MIPS_SR_PX | MIPS_SR_UX | MIPS_SR_KX;
+#endif
+
+#if __has_feature(capabilities)
+	tf->sr |= MIPS_SR_COP_2_BIT;
 #endif
 
 /*	tf->sr |= (ALL_INT_MASK & idle_mask) | SR_INT_ENAB; */