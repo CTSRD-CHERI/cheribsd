4b8d3b10280832de1483ec9764550b5bc23d42e5
John Baldwin <jhb@FreeBSD.org>
2020-01-16 10:10:42 -0800

    Add skeleton version of freebsd64_machdep.c to hold MD freebsd64 bits.

---
+++ sys/riscv/riscv/freebsd64_machdep.c
@@ -0,0 +1,188 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright 1996-1998 John D. Polstra.
+ * All rights reserved.
+ * Copyright (c) 2015 Ruslan Bukin <br@bsdpad.com>
+ * All rights reserved.
+ * Copyright (c) 2016 Yukishige Shibata <y-shibat@mtd.biglobe.ne.jp>
+ * All rights reserved.
+ * Copyright (c) 2020 John Baldwin
+ *
+ * Portions of this software were developed by SRI International and the
+ * University of Cambridge Computer Laboratory under DARPA/AFRL contract
+ * FA8750-10-C-0237 ("CTSRD"), as part of the DARPA CRASH research programme.
+ *
+ * Portions of this software were developed by the University of Cambridge
+ * Computer Laboratory as part of the CTSRD Project, with support from the
+ * UK Higher Education Innovation Fund (HEIF).
+ *
+ * Portions of this software were developed by SRI International and the
+ * University of Cambridge Computer Laboratory (Department of Computer
+ * Science and Technology) under DARPA contract HR0011-18-C-0016 ("ECATS"),
+ * as part of the DARPA SSITH research programme.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#define __ELF_WORD_SIZE 64
+#define	EXPLICIT_USER_ACCESS
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/imgact.h>
+#include <sys/imgact_elf.h>
+#include <sys/kernel.h>
+#include <sys/proc.h>
+#include <sys/syscallsubr.h>
+#include <sys/sysent.h>
+#include <sys/ucontext.h>
+
+#include <machine/md_var.h>
+
+#include <compat/freebsd64/freebsd64_proto.h>
+#include <compat/freebsd64/freebsd64_syscall.h>
+#include <compat/freebsd64/freebsd64_util.h>
+
+static void	freebsd64_sendsig(sig_t, ksiginfo_t *, sigset_t *);
+
+extern const char *freebsd64_syscallnames[];
+
+struct sysentvec elf_freebsd_freebsd64_sysvec = {
+	.sv_size	= FREEBSD64_SYS_MAXSYSCALL,
+	.sv_table	= freebsd64_sysent,
+	.sv_errsize	= 0,
+	.sv_errtbl	= NULL,
+	.sv_transtrap	= NULL,
+	.sv_fixup	= __elfN(freebsd_fixup),
+	.sv_sendsig	= freebsd64_sendsig,
+	.sv_sigcode	= freebsd64_sigcode,
+	.sv_szsigcode	= &freebsd64_szsigcode,
+	.sv_name	= "FreeBSD ELF64",
+	.sv_coredump	= __elfN(coredump),
+	.sv_imgact_try	= NULL,
+	.sv_minsigstksz	= MINSIGSTKSZ,
+	.sv_minuser	= VM_MIN_ADDRESS,
+	.sv_maxuser	= VM_MAXUSER_ADDRESS,
+	.sv_usrstack	= USRSTACK,
+	.sv_psstrings	= FREEBSD64_PS_STRINGS,
+	.sv_stackprot	= VM_PROT_READ | VM_PROT_WRITE,
+	.sv_copyout_auxargs = __elfN(freebsd_copyout_auxargs),
+	.sv_copyout_strings = freebsd64_copyout_strings,
+	.sv_setregs	= exec_setregs,
+	.sv_fixlimit	= NULL,
+	.sv_maxssiz	= NULL,
+	.sv_flags	= SV_ABI_FREEBSD | SV_LP64 | SV_SHP | SV_ASLR,
+	.sv_set_syscall_retval = cpu_set_syscall_retval,
+	.sv_fetch_syscall_args = cpu_fetch_syscall_args,
+	.sv_syscallnames = freebsd64_syscallnames,
+	.sv_shared_page_base = SHAREDPAGE,
+	.sv_shared_page_len = PAGE_SIZE,
+	.sv_schedtail	= NULL,
+	.sv_thread_detach = NULL,
+	.sv_trap	= NULL,
+	.sv_hwcap	= &elf_hwcap,
+};
+INIT_SYSENTVEC(freebsd64_sysent, &elf_freebsd_freebsd64_sysvec);
+
+static Elf64_Brandinfo freebsd_freebsd64_brand_info = {
+	.brand		= ELFOSABI_FREEBSD,
+	.machine	= EM_RISCV,
+	.compat_3_brand	= "FreeBSD",
+	.emul_path	= NULL,
+	.interp_path	= "/libexec/ld-elf.so.1",
+	.sysvec		= &elf_freebsd_freebsd64_sysvec,
+	.interp_newpath = "/libexec/ld-elf64.so.1",
+	.brand_note	= &elf64_freebsd_brandnote,
+	.flags		= BI_CAN_EXEC_DYN | BI_BRAND_NOTE,
+};
+
+SYSINIT(freebsd64, SI_SUB_EXEC, SI_ORDER_ANY,
+    (sysinit_cfunc_t) elf64_insert_brand_entry,
+    &freebsd_freebsd64_brand_info);
+
+int
+freebsd64_get_mcontext(struct thread *td, mcontext64_t *mcp, int flags)
+{
+	mcontext_t mc;
+	int error;
+
+	error = get_mcontext(td, &mc, flags);
+	if (error != 0)
+		return (error);
+
+	panic("TODO");
+
+	return (0);
+}
+
+int
+freebsd64_set_mcontext(struct thread *td, mcontext64_t *mcp)
+{
+	mcontext_t mc;
+
+	panic("TODO");
+
+	return (set_mcontext(td, &mc));
+}
+
+static void
+freebsd64_sendsig(sig_t catcher, ksiginfo_t *ksi, sigset_t *mask)
+{
+
+	panic("TODO");
+}
+
+int
+freebsd64_sigreturn(struct thread *td, struct freebsd64_sigreturn_args *uap)
+{
+	ucontext64_t uc;
+	int error;
+
+	error = copyin(__USER_CAP_OBJ(uap->sigcntxp), &uc, sizeof(uc));
+	if (error != 0)
+		return (error);
+
+	error = freebsd64_set_mcontext(td, &uc.uc_mcontext);
+	if (error != 0)
+		return (error);
+
+	kern_sigprocmask(td, SIG_SETMASK, &uc.uc_sigmask, NULL, 0);
+
+	return (EJUSTRETURN);
+}
+
+int
+freebsd64_sysarch(struct thread *td, struct freebsd64_sysarch_args *uap)
+{
+
+	return (ENOTSUP);
+}
+
+void
+elf64_dump_thread(struct thread *td __unused, void *dst __unused,
+    size_t *off __unused)
+{
+}
