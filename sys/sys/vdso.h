c54cfdcd215ad86d677fe7ac7ba0233db6052e12
Brooks Davis <brooks@one-eyed-alien.net>
2020-01-22 17:18:37 +0000

    Limit alignment workaround to 64-bit systems.

--- sys/sys/vdso.h
+++ sys/sys/vdso.h
@@ -39,9 +39,15 @@ struct vdso_timehands {
 	uint64_t	th_scale;
 	uint32_t 	th_offset_count;
 	uint32_t	th_counter_mask;
+#ifdef __LP64__
 	/* XXXAR: temporary workaround to avoid memcpy */
-	_Alignas(sizeof(void*)) struct bintime	th_offset;
-	_Alignas(sizeof(void*)) struct bintime	th_boottime;
+	_Alignas(sizeof(void*))
+#endif
+	struct bintime	th_offset;
+#ifdef __LP64__
+	_Alignas(sizeof(void*))
+#endif
+	struct bintime	th_boottime;
 	VDSO_TIMEHANDS_MD
 };
 
