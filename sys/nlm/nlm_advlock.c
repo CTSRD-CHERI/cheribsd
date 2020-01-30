783017f31a51f3159682816db2aa6eb6ab9bad01
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2020-01-30 15:08:40 +0000

    Fix missing parentheses leftover from previous changes.

--- sys/nlm/nlm_advlock.c
+++ sys/nlm/nlm_advlock.c
@@ -1155,7 +1155,7 @@ nlm_find_svid(void *id)
 	struct nlm_file_svid *ns, *newns;
 	int h;
 
-	h = (((uintptr_t) id)) >> 7) % NLM_SVID_HASH_SIZE;
+	h = (((uintptr_t) id) >> 7) % NLM_SVID_HASH_SIZE;
 
 	mtx_lock(&nlm_svid_lock);
 	LIST_FOREACH(ns, &nlm_file_svids[h], ns_link) {
