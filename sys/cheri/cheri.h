2a87d0a0f0507676316d03db6b1e1b89469430f4
John Baldwin <jhb@FreeBSD.org>
2020-02-06 13:18:50 -0800

    Move cheri_frame and related functions to the MIPS <machine/cheri.h>.
    RISC-V doesn't define a cheri_frame and instead modifies the trapframe.

--- sys/cheri/cheri.h
+++ sys/cheri/cheri.h
@@ -111,25 +111,12 @@ void * __capability cheri_sigcode_capability(struct thread *td);
 /*
  * CHERI context management functions.
  */
-struct cheri_frame;
-struct trapframe;
 const char	*cheri_exccode_string(uint8_t exccode);
 void	cheri_exec_setregs(struct thread *td, u_long entry_addr);
-void	cheri_log_cheri_frame(struct trapframe *frame);
-void	cheri_log_exception(struct trapframe *frame, int trap_type);
-void	cheri_log_exception_registers(struct trapframe *frame);
 void	cheri_newthread_setregs(struct thread *td, u_long entry_addr);
 int	cheri_syscall_authorize(struct thread *td, u_int code,
 	    int nargs, syscallarg_t *args);
 int	cheri_signal_sandboxed(struct thread *td);
-void	cheri_trapframe_from_cheriframe(struct trapframe *frame,
-	    struct cheri_frame *cfp);
-void	_cheri_trapframe_to_cheriframe(struct trapframe *frame,
-	    struct cheri_frame *cfp, bool strip_tags);
-#define	cheri_trapframe_to_cheriframe(tf, cf)			\
-	_cheri_trapframe_to_cheriframe((tf), (cf), false)
-#define	cheri_trapframe_to_cheriframe_strip(tf, cf)		\
-	_cheri_trapframe_to_cheriframe((tf), (cf), true)
 void	hybridabi_sendsig(struct thread *td);
 
 /*
@@ -171,6 +158,7 @@ struct ktr_ccall;
 struct ktr_creturn;
 struct ktr_cexception;
 struct thr_param_c;
+struct trapframe;
 void	ktrccall_mdfill(struct pcb *pcb, struct ktr_ccall *kc);
 void	ktrcreturn_mdfill(struct pcb *pcb, struct ktr_creturn *kr);
 void	ktrcexception_mdfill(struct trapframe *frame,
