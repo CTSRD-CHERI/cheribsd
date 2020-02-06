3bdacbd505ad4ae230ad3159f7744fab73177c93
John Baldwin <jhb@FreeBSD.org>
2020-02-06 15:35:46 -0800

    Copy p_md.md_sigcode to child processes on fork.

--- sys/riscv/riscv/vm_machdep.c
+++ sys/riscv/riscv/vm_machdep.c
@@ -85,6 +85,10 @@ cpu_fork(struct thread *td1, struct proc *p2, struct thread *td2, int flags)
 	td2->td_pcb = pcb2;
 	bcopy(td1->td_pcb, pcb2, sizeof(*pcb2));
 
+#if __has_feature(capabilities)
+	p2->p_md.md_sigcode = td1->td_proc->p_md.md_sigcode;
+#endif
+
 	/*
 	 * The "top-most" trapframe uses a "hole" between the pcb and
 	 * trapframe to save the kernel's tp register used for per-CPU
