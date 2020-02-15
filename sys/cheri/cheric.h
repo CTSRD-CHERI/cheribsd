e3b1b98dd62a492ed54b905a5dd019dfa16ae97e
John Baldwin <jhb@FreeBSD.org>
2020-02-15 11:54:33 -0800

    Print flags in _CHERI_PRINTF_CAP_*.

--- sys/cheri/cheric.h
+++ sys/cheri/cheric.h
@@ -232,11 +232,12 @@ cheri_bytes_remaining(const void * __capability cap)
 #define cheri_cap_to_typed_ptr(cap, type)				\
 	(type *)cheri_cap_to_ptr(cap, sizeof(type))
 
-#define _CHERI_PRINTF_CAP_FMT  "v:%lu s:%lu p:%08lx b:%016jx l:%016zx o:%jx t:%ld"
+#define _CHERI_PRINTF_CAP_FMT  "v:%lu s:%lu p:%08lx f:%01x b:%016jx l:%016zx o:%jx t:%ld"
 #define _CHERI_PRINTF_CAP_ARG(ptr)					\
 	    (unsigned long)cheri_gettag((const void * __capability)(ptr)),		\
 	    (unsigned long)cheri_getsealed((const void * __capability)(ptr)),		\
 	    cheri_getperm((const void * __capability)(ptr)),		\
+	    cheri_getflags((const void * __capability)(ptr)),		\
 	    cheri_getbase((const void * __capability)(ptr)),		\
 	    cheri_getlen((const void * __capability)(ptr)),		\
 	    cheri_getoffset((const void * __capability)(ptr)),		\
