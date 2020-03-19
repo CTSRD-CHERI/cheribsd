b759163c60ed1e05e4fa4d06d90b5bafdd52aeba
nicomazz <nicomazz97@gmail.com>
2020-03-19 15:29:37 +0000

    Change intcap_t definition

--- sys/sys/_stdint.h
+++ sys/sys/_stdint.h
@@ -96,23 +96,21 @@ typedef	__intmax_t		intmax_t;
 typedef	__uintmax_t		uintmax_t;
 #define	_UINTMAX_T_DECLARED
 #endif
-
 #ifndef _KINTCAP_T_DECLARED
-#ifndef __kintcap_t
+#ifdef _KERNEL
+typedef __intcap_t	kintcap_t;
+#else
 typedef __intptr_t	kintcap_t;
-#else
-typedef __kintcap_t	kintcap_t;
 #endif
-#define _KINTCAP_T_DECLARED
+#define	_KINTCAP_T_DECLARED
 #endif
-
 #ifndef _KUINTCAP_T_DECLARED
-#ifndef __kuintcap_t
-typedef __uintptr_t	kuintcap_t;
+#ifdef _KERNEL
+typedef __uintcap_t	kuintcap_t;
 #else
-typedef __kuintcap_t	kuintcap_t;
+typedef __uintptr_t	kuintcap_t;
 #endif
-#define _KUINTCAP_T_DECLARED
+#define	_KUINTCAP_T_DECLARED
 #endif
 
 #ifndef _VADDR_T_DECLARED
