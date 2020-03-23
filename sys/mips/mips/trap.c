90fe426bbb113f344baab1dd2d4ced285aa2918f
Nathaniel Filardo <nwf20@cl.cam.ac.uk>
2020-03-23 15:06:55 +0000

    capdirty: mips support for capdirty tracking

--- sys/mips/mips/trap.c
+++ sys/mips/mips/trap.c
@@ -685,6 +685,40 @@ cpu_fetch_syscall_args(struct thread *td)
 #define __FBSDID(x)
 #include "../../kern/subr_syscall.c"
 
+#ifdef CPU_CHERI
+static inline int
+try_emulate_capdirty(struct trapframe *trapframe, pmap_t kpmap, pmap_t upmap)
+{
+	register_t cause = trapframe->capcause;
+
+	cause &= CHERI_CAPCAUSE_EXCCODE_MASK;
+	cause >>= CHERI_CAPCAUSE_EXCCODE_SHIFT;
+
+	if (cause == CHERI_EXCCODE_TLBSTORE) {
+		/*
+		 * This could just need capdirty emulation; we cannot get
+		 * here unless the TLB entry is permitted to store, just not
+		 * store capabilities.  That is, all CoW will have already
+		 * been handled for us, thank goodness.
+		 */
+
+		vm_offset_t va = trapframe->badvaddr;
+
+		/*
+		 * Capdirty of kernel va requires kernel pmap, which is only
+		 * given to us if the trapping PC was in the kernel.  For user
+		 * va, the dirtying store could come from either kernel or user
+		 * PC, but both go through the user pmap.
+		 */
+		if (KERNLAND(va) && kpmap) {
+			return pmap_emulate_capdirty(kpmap, va);
+		} else {
+			return pmap_emulate_capdirty(upmap, va);
+		}
+	}
+	return 1;
+}
+#endif
 
 /*
  * Handle an exception.
@@ -1146,6 +1180,11 @@ dofault:
 		break;
 #ifdef CPU_CHERI
 	case T_C2E:
+		if (try_emulate_capdirty(trapframe, kernel_pmap,
+					&p->p_vmspace->vm_pmap) == 0) {
+			return trapframe->pc;
+		}
+
 		if (td->td_pcb->pcb_onfault != NULL) {
 			pc = trapf_pc_from_kernel_code_ptr(td->td_pcb->pcb_onfault);
 			td->td_pcb->pcb_onfault = NULL;
@@ -1161,6 +1200,12 @@ dofault:
 
 	case T_C2E + T_USER:
 		msg = "USER_CHERI_EXCEPTION";
+
+		if (try_emulate_capdirty(trapframe, NULL,
+					 &p->p_vmspace->vm_pmap) == 0) {
+			goto out;
+		}
+
 		fetch_bad_instr(trapframe);
 		if (log_user_cheri_exceptions)
 			log_c2e_exception(msg, trapframe, type);
