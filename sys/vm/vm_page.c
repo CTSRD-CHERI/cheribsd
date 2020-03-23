f2a4f276d3dc5f95571e3f5ccde96e96e84841b0
Nathaniel Filardo <nwf20@cl.cam.ac.uk>
2020-03-23 13:27:21 +0000

    capdirty: introduce MI bits

--- sys/vm/vm_page.c
+++ sys/vm/vm_page.c
@@ -1820,6 +1820,15 @@ vm_page_replace_hold(vm_page_t mnew, vm_object_t object, vm_pindex_t pindex,
 	 */
 	if (pmap_page_is_write_mapped(mnew))
 		vm_object_set_writeable_dirty(object);
+
+	/*
+	 * Copy across capability tracking flags
+	 */
+	if (vm_page_astate_load(mold).flags & PGA_CAPDIRTY)
+		vm_page_capdirty(mnew);
+	if (mold->oflags & VPO_CAPSTORE)
+		mnew->oflags |= VPO_CAPSTORE;
+
 	dropped = vm_page_drop(mold, VPRC_OBJREF) == VPRC_OBJREF;
 	vm_page_xunbusy(mold);
 
