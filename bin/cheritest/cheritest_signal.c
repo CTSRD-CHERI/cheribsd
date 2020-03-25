032cee906c534da6cc7924712352326436f75fb3
Jessica Clarke <jrtc27@jrtc27.com>
2020-03-25 04:50:11 +0000

    Update my name in copyright headers

--- bin/cheritest/cheritest_signal.c
+++ bin/cheritest/cheritest_signal.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2017, 2019 James Clarke
+ * Copyright (c) 2017, 2019 Jessica Clarke
  * All rights reserved.
  *
  * This software was developed by SRI International and the University of
