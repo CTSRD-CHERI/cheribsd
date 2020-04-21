f04c576534d2fe09cd11c06961bf49db6a033e38
nicomazz <nicomazz97@gmail.com>
2020-04-21 19:06:38 +0100

    Enable CTF generation for kernel
    CDDL was set as a broken option for mips. It is not so broken anymore.

--- sys/conf/kern.opts.mk
+++ sys/conf/kern.opts.mk
@@ -71,7 +71,7 @@ BROKEN_OPTIONS+= CDDL ZFS
 .endif
 
 .if ${MACHINE_CPUARCH} == "mips"
-BROKEN_OPTIONS+= CDDL ZFS SSP
+BROKEN_OPTIONS+= ZFS SSP
 .endif
 
 .if ${MACHINE_CPUARCH} == "powerpc" && ${MACHINE_ARCH} == "powerpc"
