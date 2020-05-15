cdcd1c6c1c9175023ef23d493456cbe4530d5ab6
John Baldwin <jhb@FreeBSD.org>
2020-05-15 15:37:03 -0700

    Update linuxkpi for explicit user capabilities.
    I choose to plumb user capabilities all the way down such that things
    like copy_from_user() take capabilities and not just flat user
    addresses in case the GPU driver for Morello ends up using linuxkpi.

--- sys/compat/linuxkpi/common/include/linux/kernel.h
+++ sys/compat/linuxkpi/common/include/linux/kernel.h
@@ -433,7 +433,7 @@ kstrtobool(const char *s, bool *res)
 }
 
 static inline int
-kstrtobool_from_user(const char __user *s, size_t count, bool *res)
+kstrtobool_from_user(const char __user * __capability s, size_t count, bool *res)
 {
 	char buf[8] = {};
 
