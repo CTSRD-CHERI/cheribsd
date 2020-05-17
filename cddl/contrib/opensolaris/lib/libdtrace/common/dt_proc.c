ead7bad1ddc99f8662a2d3b0aaf11b0907f5672b
nicomazz <nicomazz97@gmail.com>
2020-05-18 00:16:30 +0100

    Disable breakpoints while probes are created in PID provider

--- cddl/contrib/opensolaris/lib/libdtrace/common/dt_proc.c
+++ cddl/contrib/opensolaris/lib/libdtrace/common/dt_proc.c
@@ -311,10 +311,11 @@ dt_proc_rdevent(dtrace_hdl_t *dtp, dt_proc_t *dpr, const char *evname)
 			break;
 
 		Pupdate_syms(dpr->dpr_proc);
+		dt_proc_bpdisable(dpr);
 		if (dt_pid_create_probes_module(dtp, dpr) != 0)
 			dt_proc_notify(dtp, dtp->dt_procs, dpr,
 			    dpr->dpr_errmsg);
-
+		dt_proc_bpenable(dpr);
 		break;
 	case RD_PREINIT:
 		Pupdate_syms(dpr->dpr_proc);
