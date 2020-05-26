9a2ffc9ad9c6e4f318d568220c4dbc6a83462617
Edward Napierala <en322@cl.cam.ac.uk>
2020-05-26 23:40:31 +0100

    Fix build.

--- sys/cheri/cheri_colocation.c
+++ sys/cheri/cheri_colocation.c
@@ -350,7 +350,7 @@ setup_scb(struct thread *td)
 
 	rv = vm_map_insert(map, NULL, 0, addr, addr + PAGE_SIZE,
 	    VM_PROT_READ | VM_PROT_WRITE, VM_PROT_READ | VM_PROT_WRITE,
-	    MAP_DISABLE_COREDUMP);
+	    MAP_DISABLE_COREDUMP, addr);
 	if (rv != KERN_SUCCESS) {
 		COLOCATION_DEBUG("vm_map_insert() failed with rv %d", rv);
 		vm_map_unlock(map);
