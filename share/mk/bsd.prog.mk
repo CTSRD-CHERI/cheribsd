c1617bc34a4da89cffd55eab7e212d8911d49c5b
Brooks Davis <brooks@one-eyed-alien.net>
2020-06-10 19:40:38 +0100

    Add support for initializing stack variables on entry.
    There are two options:
     - WITH_INIT_ALL_ZERO: Zero all variables on the stack.
     - WITH_INIT_ALL_PATTERN: Initialize variables with well-defined patterns.
       The exact pattern are a compiler implementation detail and vary by type.
       They are somewhat documented in the LLVM commit message:
       https://reviews.llvm.org/rL349442
    
    I've used WITH_INIT_ALL_* to matches Microsoft's InitAll feature rather
    than naming them after the LLVM specific compiler flags.

--- share/mk/bsd.prog.mk
+++ share/mk/bsd.prog.mk
@@ -66,6 +66,25 @@ LDFLAGS+= -Wl,-zretpolineplt
 .endif
 .endif
 
+# Initialize stack variables on function entry
+.if ${MK_INIT_ALL_ZERO} == "yes"
+.if ${COMPILER_FEATURES:Minit-all}
+CFLAGS+= -ftrivial-auto-var-init=zero \
+    -enable-trivial-auto-var-init-zero-knowing-it-will-be-removed-from-clang
+CXXFLAGS+= -ftrivial-auto-var-init=zero \
+    -enable-trivial-auto-var-init-zero-knowing-it-will-be-removed-from-clang
+.else
+.warning InitAll (zeros) requested but not support by compiler
+.endif
+.elif ${MK_INIT_ALL_PATTERN} == "yes"
+.if ${COMPILER_FEATURES:Minit-all}
+CFLAGS+= -ftrivial-auto-var-init=pattern
+CXXFLAGS+= -ftrivial-auto-var-init=pattern
+.else
+.warning InitAll (pattern) requested but not support by compiler
+.endif
+.endif
+
 # macOS linker doesn't understand the --fatal-warnings flag
 .if ${LINKER_TYPE} != "mac"
 .if defined(LD_FATAL_WARNINGS) && ${LD_FATAL_WARNINGS} == "no"
