509386ca195410c31c1fe53c59a745d42b8eb3cf
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-06-23 17:53:01 +0100

    Relax a cheritest check for hybrid mode
    RISC-V does not bound stack capabilities in hybrid mode.

--- bin/cheritest/cheritest_bounds_stack.c
+++ bin/cheritest/cheritest_bounds_stack.c
@@ -91,9 +91,11 @@ test_bounds_precise(void * __capability c, size_t expected_len)
 	CHERITEST_VERIFY2(len >= expected_len,
 	    "length (%jd) smaller than expected lower bound %jd: " _CHERI_PRINTF_CAP_FMT,
 	    len, expected_len, _CHERI_PRINTF_CAP_ARG(c));
+#ifndef __riscv  /* RISC-V does not bound stack capabilities in hybrid mode */
 	CHERITEST_VERIFY2(len <= 2 * expected_len,
 	    "length (%jd) greater than expected upper bound %jd: " _CHERI_PRINTF_CAP_FMT,
 	    len, 2 * expected_len, _CHERI_PRINTF_CAP_ARG(c));
+#endif
 #endif
 	cheritest_success();
 }
