0ceed24d2aca90a6a8677d5ec9d1b687b898aa3e
Andrew Turner <at718@cam.ac.uk>
2020-07-02 10:59:43 +0100

    Teach arm64 about MACHINE_ARCH64
    Even though I believe it will cause us more issues in the long run use
    aarch64c for MACHINE_ARCH on Morello.
    
    It would be nice to use morello here, and aarch64c for any future
    production extensions as this would allow:
     * Build systems to know which they are building for when in a native
       build as the two extensions will have different C flags - the Morello
       toolchain uses morello in a flag.
     * Will allow for both to be built from the same source without having to
       manage multiple obj dirs. This would be useful with things like running
       'make tinderbox TARGETS=arm64'.
     * Will cause confusion as to which is being run. As both will use the same
       MACHINE_ARCH another means will be needed to find which a user is running.
     * This value is mostly useful for putting into 'uname -p'. Linux only uses
       this on a few architectures and arm64 isn't one of them so this is not to
       be compatible with them.
    
    Respect other peoples wishes and set MACHINE_ARCH to aarch64c.

--- sys/arm64/include/param.h
+++ sys/arm64/include/param.h
@@ -47,11 +47,21 @@
 #ifndef MACHINE
 #define	MACHINE		"arm64"
 #endif
-#ifndef MACHINE_ARCH
-#define	MACHINE_ARCH	"aarch64"
-#endif
-#ifndef MACHINE_ARCH32
-#define	MACHINE_ARCH32	"armv7"
+#if defined(__CHERI_PURE_CAPABILITY__) || (defined(_KERNEL) && \
+    __has_feature(capabilities))
+# ifndef MACHINE_ARCH
+#  define	MACHINE_ARCH	"aarch64c"
+# endif
+# ifndef MACHINE_ARCH64
+#  define	MACHINE_ARCH64	"aarch64"
+# endif
+#else
+# ifndef MACHINE_ARCH
+#  define	MACHINE_ARCH	"aarch64"
+# endif
+# ifndef MACHINE_ARCH32
+#  define	MACHINE_ARCH32	"armv7"
+# endif
 #endif
 
 #ifdef SMP
