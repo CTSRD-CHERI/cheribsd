7b0858e8d23624129a7993060fc13fddf4bce73e
Brooks Davis <brooks@one-eyed-alien.net>
2020-07-08 23:30:02 +0100

    Add an is_aligned wrapper for __builtin_is_aligned.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -298,6 +298,7 @@
 #define	howmany(x, y)	(((x)+((y)-1))/(y))
 #endif
 #define	nitems(x)	(sizeof((x)) / sizeof((x)[0]))
+#define	is_aligned(x, y) __builtin_is_aligned(x, y)
 #define	rounddown(x, y)	(((x)/(y))*(y))
 #define	rounddown2(x, y) __builtin_align_down((x), (y))
 #define	roundup(x, y)	((((x)+((y)-1))/(y))*(y))  /* to any y */
