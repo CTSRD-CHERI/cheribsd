1cf9bb56f3bc6f064f003b05a330e7a4bc674836
John Baldwin <jhb@FreeBSD.org>
2020-07-09 10:50:56 -0700

    Use 'ctp' for the per-CPU pointer for purecap.

--- sys/riscv/riscv/machdep.c
+++ sys/riscv/riscv/machdep.c
@@ -1064,7 +1064,11 @@ initriscv(struct riscv_bootparams *rvbp)
 	pcpup->pc_hart = boot_hart;
 
 	/* Set the pcpu pointer */
+#ifdef __CHERI_PURE_CAPABILITY__
+	__asm __volatile("cmove ctp, %0" :: "C"(pcpup));
+#else
 	__asm __volatile("mv tp, %0" :: "r"(pcpup));
+#endif
 
 	PCPU_SET(curthread, &thread0);
 
--- sys/riscv/riscv/mp_machdep.c
+++ sys/riscv/riscv/mp_machdep.c
@@ -233,7 +233,11 @@ init_secondary(uint64_t hart)
 
 	/* Setup the pcpu pointer */
 	pcpup = &__pcpu[cpuid];
+#ifdef __CHERI_PURE_CAPABILITY__
+	__asm __volatile("cmove ctp, %0" :: "C"(pcpup));
+#else
 	__asm __volatile("mv tp, %0" :: "r"(pcpup));
+#endif
 
 	/* Workaround: make sure wfi doesn't halt the hart */
 	csr_set(sie, SIE_SSIE);
@@ -548,3 +552,13 @@ cpu_mp_setmaxid(void)
 	mp_ncpus = 1;
 	mp_maxid = 0;
 }
+
+// CHERI CHANGES START
+// {
+//   "updated": 20200803,
+//   "target_type": "kernel",
+//   "changes_purecap": [
+//     "support"
+//   ]
+// }
+// CHERI CHANGES END
