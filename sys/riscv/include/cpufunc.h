bd45de50eca95e236df6f902517d1cc4390a040d
John Baldwin <jhb@FreeBSD.org>
2020-07-13 13:45:16 -0700

    Pass a virtual address to sfence_vma_page().
    The instruction takes an address, not a capability.

--- sys/riscv/include/cpufunc.h
+++ sys/riscv/include/cpufunc.h
@@ -98,7 +98,7 @@ sfence_vma(void)
 }
 
 static __inline void
-sfence_vma_page(uintptr_t addr)
+sfence_vma_page(register_t addr)
 {
 
 	__asm __volatile("sfence.vma %0" :: "r" (addr) : "memory");
@@ -133,3 +133,12 @@ void riscv_nullop(void);
 
 #endif	/* _KERNEL */
 #endif	/* _MACHINE_CPUFUNC_H_ */
+// CHERI CHANGES START
+// {
+//   "updated": 20200804,
+//   "target_type": "kernel",
+//   "changes_purecap": [
+//     "pointer_as_integer"
+//   ]
+// }
+// CHERI CHANGES END
