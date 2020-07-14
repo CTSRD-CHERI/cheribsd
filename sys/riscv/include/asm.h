c43dfab65aec237f3179ce7026586affe5151e33
John Baldwin <jhb@FreeBSD.org>
2020-07-14 14:08:56 -0700

    Provide symbol type and size information for alternate entry points.
    This fixes captable linker warnings for fueword64() and suword64().

--- sys/riscv/include/asm.h
+++ sys/riscv/include/asm.h
@@ -51,8 +51,8 @@
 #define	END(sym) .size sym, . - sym
 
 #define	EENTRY(sym)						\
-	.globl	sym; sym:
-#define	EEND(sym)
+	.globl	sym; .type sym,@function; sym:
+#define	EEND(sym) .size sym, . - sym
 
 #define	WEAK_REFERENCE(sym, alias)				\
 	.weak alias;						\
