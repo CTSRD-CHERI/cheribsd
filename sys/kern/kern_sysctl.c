76bf6e7ae189fd8bd189d767e9d715a10adcc7fb
John Baldwin <jhb@FreeBSD.org>
2020-07-15 13:47:11 -0700

    Adjust for VNET_START before deriving from vnet_data_base.

--- sys/kern/kern_sysctl.c
+++ sys/kern/kern_sysctl.c
@@ -2197,10 +2197,10 @@ sysctl_root(SYSCTL_HANDLER_ARGS)
 #endif
 #ifdef VIMAGE
 	if ((oid->oid_kind & CTLFLAG_VNET) && arg1 != NULL) {
-		arg1 = (void *)(curvnet->vnet_data_base + (vaddr_t)arg1);
 #ifdef __CHERI_PURE_CAPABILITY__
 		arg1 = (char *)arg1 - (vaddr_t)VNET_START;
 #endif
+		arg1 = (void *)(curvnet->vnet_data_base + (vaddr_t)arg1);
 	}
 #endif
 	error = sysctl_root_handler_locked(oid, arg1, arg2, req, &tracker);
