a1fb8b58281e440909224a859ca12553f396757e
John Baldwin <jhb@FreeBSD.org>
2020-07-28 13:34:18 -0700

    Switch NDINIT_AT to NDINIT_ALL_C.
    This is a no-op since the kernel currently only uses NDINIT_AT_C.

--- sys/sys/namei.h
+++ sys/sys/namei.h
@@ -186,7 +186,7 @@ struct nameidata {
 #define	NDINIT_C(ndp, op, flags, segflg, namep, td)			\
 	NDINIT_ALL_C(ndp, op, flags, segflg, namep, AT_FDCWD, NULL, 0, td)
 #define	NDINIT_AT(ndp, op, flags, segflg, namep, dirfd, td)		\
-	NDINIT_ALL(ndp, op, flags, segflg, namep, dirfd, NULL, 0, td)
+	NDINIT_ALL_C(ndp, op, flags, segflg, namep, dirfd, NULL, 0, td)
 #define	NDINIT_AT_C(ndp, op, flags, segflg, namep, dirfd, td)		\
 	NDINIT_ALL_C(ndp, op, flags, segflg, namep, dirfd, NULL, 0, td)
 #define	NDINIT_ATRIGHTS(ndp, op, flags, segflg, namep, dirfd, rightsp, td) \
