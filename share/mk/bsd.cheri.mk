54c4cd9338cb1dbfe2bc69ba23743eb9dc3deab1
John Baldwin <jhb@FreeBSD.org>
2020-08-06 14:43:00 -0700

    --target and -msoft-float are redundant.
    Makefile.inc1 ensures --target is set in CFLAGS, and bsd.cpu.mk handles
    -msoft-float.

--- share/mk/bsd.cheri.mk
+++ share/mk/bsd.cheri.mk
@@ -51,9 +51,7 @@ WANT_CHERI:= ${NEED_CHERI}
 .endif
 
 .if ${MK_CHERI} != "no" && defined(WANT_CHERI) && ${WANT_CHERI} != "none"
-_CHERI_COMMON_FLAGS=	--target=mips64-unknown-freebsd \
-			-msoft-float \
-			-cheri-uintcap=${CHERI_UINTCAP_MODE:Uaddr}
+_CHERI_COMMON_FLAGS=	-cheri-uintcap=${CHERI_UINTCAP_MODE:Uaddr}
 .ifdef WANT_AFL_FUZZ
 # Build binaries static when fuzzing
 .if defined(__BSD_PROG_MK)
