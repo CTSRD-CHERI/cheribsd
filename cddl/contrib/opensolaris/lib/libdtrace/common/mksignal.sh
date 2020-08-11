cd38673452270f3f7fa462c7516fd8878276ecd1
arichardson <arichardson@FreeBSD.org>
2020-08-11 16:46:54 +0000

    Fix libdtrace build with zsh as /bin/sh
    When zsh runs in POSIX sh mode it does not support the -e flag to echo.
    Use printf instead of echo to avoid the "-e" characters being printed.
    
    Obtained from:	CheriBSD
    Reviewed By:	markj
    Differential Revision: https://reviews.freebsd.org/D26026

--- cddl/contrib/opensolaris/lib/libdtrace/common/mksignal.sh
+++ cddl/contrib/opensolaris/lib/libdtrace/common/mksignal.sh
@@ -24,7 +24,6 @@
 # Copyright 2003 Sun Microsystems, Inc.  All rights reserved.
 # Use is subject to license terms.
 #
-#ident	"%Z%%M%	%I%	%E% SMI"
 set -e
 
 printf "%s" "
@@ -33,7 +32,6 @@ printf "%s" "
  * Use is subject to license terms.
  */
 
-#pragma ident\t\"%Z%%M%\t%I%\t%E% SMI\"
 "
 
 pattern='^#define[	 ]*_*\(SIG[A-Z0-9]*\)[	 ]\{1,\}\([A-Z0-9]*\).*$'
