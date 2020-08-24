3dfc76b6ba65f1dbaf4fa23deb35f4f7a2dcaff2
arichardson <arichardson@FreeBSD.org>
2020-08-24 09:20:18 +0000

    Re-indent crunched_main.c in preparation for D25998

--- usr.sbin/crunch/crunchgen/crunched_main.c
+++ usr.sbin/crunch/crunchgen/crunched_main.c
@@ -38,13 +38,10 @@ __FBSDID("$FreeBSD$");
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/param.h>
-#include <sys/auxv.h>
-#include <sys/sysctl.h>
 
 struct stub {
-    char *name;
-    int (*f)();
+	char *name;
+	int (*f)();
 };
 
 extern char *__progname;
@@ -52,156 +49,67 @@ extern struct stub entry_points[];
 
 static void crunched_usage(void);
 
-
-static struct stub* find_entry_point(const char* basename)
-{
-    struct stub *ep;
-
-    for(ep=entry_points; ep->name != NULL; ep++)
-	if(!strcmp(basename, ep->name)) break;
-
-    return ep;
-}
-
-static const char* get_basename(const char* exe_path)
-{
-    const char *slash;
-
-    slash = strrchr(exe_path, '/');
-    return (slash ? slash+1 : exe_path);
-}
-
 int
 main(int argc, char **argv, char **envp)
 {
-    struct stub *ep;
-    const char *basename = NULL;
-    char exe_buf[MAXPATHLEN];
+	char *slash, *basename;
+	struct stub *ep;
 
-    /*
-     * Look at __progname first (this will be set if the crunched binary is
-     * invoked directly).
-     */
-    if (__progname) {
-	basename = get_basename(__progname);
-	ep = find_entry_point(basename);
-    }
+	if (argv[0] == NULL || *argv[0] == '\0')
+		crunched_usage();
 
-    /*
-     * Otherwise try to find entry point based on argv[0] (this works for both
-     * symlinks as well as hardlinks).
-     * However, it does not work when su invokes a crunched shell because it
-     * sets argv[0] to _su when invoking the shell. In that case we look at
-     * KERN_PROC_PATHNAME. This will only work for hard links to the
-     * crunched binary but that should be fine.
-     * TODO: is there a way to get the exepath without resolving symlinks?
-     */
-    if (ep->name == NULL) {
-	basename = get_basename(argv[0]);
-	ep = find_entry_point(basename);
-    }
+	slash = strrchr(argv[0], '/');
+	basename = slash ? slash + 1 : argv[0];
 
-#ifdef AT_EXECPATH
-    /*
-     * Try AT_EXECPATH to get the actual binary that was executed.
-     * This is needed since su will set argv[0] to -su instead of the shell.
-     */
-    if (ep->name == NULL) {
-	int error = elf_aux_info(AT_EXECPATH, &exe_buf, sizeof(exe_buf));
-	if (error == 0) {
-	    const char *exe_name = get_basename(exe_buf);
-	    /*
-	     * Keep using argv[0] if AT_EXECPATH is the crunched binary
-	     * so that symlinks to the crunched binary report "not compiled in"
-	     * instead of invoking crunched_main().
-	     */
-	    if (strcmp(exe_name, EXECNAME) != 0) {
-		basename = exe_name;
-		ep = find_entry_point(basename);
-	    }
-	} else {
-		fprintf(stderr, "elf_aux_info(AT_EXECPATH) got error %d: %s\n", error, strerror(error));
+	for (ep = entry_points; ep->name != NULL; ep++)
+		if (!strcmp(basename, ep->name))
+			break;
+
+	if (ep->name)
+		return ep->f(argc, argv, envp);
+	else {
+		fprintf(stderr, "%s: %s not compiled in\n", EXECNAME, basename);
+		crunched_usage();
 	}
-    }
-#else
-#error "EXPECTED AT_EXECPATH to exist!"
-#endif
-
-    /* Finally fall back to using KERN_PROC_PATHNAME */
-    /*
-     * XXXAR: this does not seem to work correctly since it appears to return
-     * the last resolved path for a hardlink rather than the one that was
-     * actually used to execute the command.
-     */
-#if 0
-    if (ep->name == NULL) {
-	size_t len = sizeof(exe_buf);
-	int name[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, -1 };
-	if (sysctl(name, 4, exe_buf, &len, NULL, 0) == 0) {
-	    const char *exe_name = get_basename(exe_buf);
-	    /*
-	     * Keep using argv[0] if KERN_PROC_PATHNAME is the crunched binary
-	     * so that symlinks to the crunched binary report "not compiled in"
-	     * instead of invoking crunched_main().
-	     */
-	    if (strcmp(exe_name, EXECNAME) != 0) {
-		basename = exe_name;
-		ep = find_entry_point(basename);
-	    }
-	}
-    }
-#endif
-
-    if (basename == NULL || *basename == '\0')
-	crunched_usage();
-
-    if (ep->name)
-	return ep->f(argc, argv, envp);
-    else {
-	fprintf(stderr, "%s: %s not compiled in\n", EXECNAME, basename);
-	crunched_usage();
-    }
 }
-
 
 int
 crunched_main(int argc, char **argv, char **envp)
 {
-    char *slash;
-    struct stub *ep;
-    int columns, len;
+	char *slash;
+	struct stub *ep;
+	int columns, len;
 
-    if(argc <= 1)
-	crunched_usage();
+	if (argc <= 1)
+		crunched_usage();
 
-    slash = strrchr(argv[1], '/');
-    __progname = slash? slash+1 : argv[1];
+	slash = strrchr(argv[1], '/');
+	__progname = slash ? slash + 1 : argv[1];
 
-    return main(--argc, ++argv, envp);
+	return main(--argc, ++argv, envp);
 }
-
 
 static void
 crunched_usage()
 {
-    int columns, len;
-    struct stub *ep;
+	int columns, len;
+	struct stub *ep;
 
-    fprintf(stderr, "usage: %s <prog> <args> ..., where <prog> is one of:\n",
-	    EXECNAME);
-    columns = 0;
-    for(ep=entry_points; ep->name != NULL; ep++) {
-	len = strlen(ep->name) + 1;
-	if(columns+len < 80)
-	    columns += len;
-	else {
-	    fprintf(stderr, "\n");
-	    columns = len;
+	fprintf(stderr,
+	    "usage: %s <prog> <args> ..., where <prog> is one of:\n", EXECNAME);
+	columns = 0;
+	for (ep = entry_points; ep->name != NULL; ep++) {
+		len = strlen(ep->name) + 1;
+		if (columns + len < 80)
+			columns += len;
+		else {
+			fprintf(stderr, "\n");
+			columns = len;
+		}
+		fprintf(stderr, " %s", ep->name);
 	}
-	fprintf(stderr, " %s", ep->name);
-    }
-    fprintf(stderr, "\n");
-    exit(1);
+	fprintf(stderr, "\n");
+	exit(1);
 }
 
 /* end of crunched_main.c */
