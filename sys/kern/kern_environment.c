185891ada310b93061d367bc748b43ee48578fd0
John Baldwin <jhb@FreeBSD.org>
2020-09-23 09:05:41 -0700

    Narrow bounds on strings returned by getenv().

--- sys/kern/kern_environment.c
+++ sys/kern/kern_environment.c
@@ -306,6 +306,11 @@ init_static_kenv(char *buf, size_t len)
 	if (!getenv_is_true("loader_env.disabled")) {
 		md_envp = buf;
 		md_env_len = len;
+#ifdef __CHERI_PURE_CAPABILITY__
+		if (len != 0)
+			KASSERT(cheri_getlen(md_envp) == len,
+			    ("unbounded static kenv"));
+#endif
 		md_env_pos = 0;
 
 		if (getenv_is_true("static_env.disabled")) {
@@ -454,7 +459,11 @@ _getenv_static_from(char *chkenv, const char *name)
 		len = ep - cp;
 		ep++;
 		if (!strncmp(name, cp, len) && name[len] == 0)
+#ifdef __CHERI_PURE_CAPABILITY__
+			return (cheri_setbounds(ep, strlen(ep) + 1));
+#else
 			return (ep);
+#endif
 	}
 	return (NULL);
 }
