c7c378729f1aa70c285b0fa909181a63fd9c9fba
John Baldwin <jhb@FreeBSD.org>
2020-10-27 19:33:38 -0700

    Use trapframe_set_elr for the thread start routine for new threads.

--- sys/arm64/arm64/vm_machdep.c
+++ sys/arm64/arm64/vm_machdep.c
@@ -55,6 +55,10 @@ __FBSDID("$FreeBSD$");
 #include <machine/vfp.h>
 #endif
 
+#if __has_feature(capabilities)
+#include <cheri/cheric.h>
+#endif
+
 #include <dev/psci/psci.h>
 
 /*
@@ -215,7 +219,11 @@ cpu_set_upcall(struct thread *td, void (* __capability entry)(void *),
 #endif
 	else {
 		tf->tf_sp = STACKALIGN((uintcap_t)stack->ss_sp + stack->ss_size);
+#if __has_feature(capabilities)
+		trapframe_set_elr(tf, (uintcap_t)entry);
+#else
 		tf->tf_elr = (uintcap_t)entry;
+#endif
 	}
 	tf->tf_x[0] = (uintcap_t)arg;
 }
