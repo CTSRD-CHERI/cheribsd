f7c23d772ade8ee989daf2bd0550e2cb9661f861
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-11-26 12:49:38 +0000

    git subrepo pull contrib/subrepo-cheri-libc++
    subrepo:
      subdir:   "contrib/subrepo-cheri-libc++"
      merged:   "8869f3cc5444"
    upstream:
      origin:   "https://github.com/CTSRD-CHERI/libcxx.git"
      branch:   "monorepo-mirror"
      commit:   "309c48cd5228"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "ca6a98f"

--- contrib/subrepo-cheri-libc++/test/std/thread/thread.threads/thread.thread.this/sleep_until.pass.cpp
+++ contrib/subrepo-cheri-libc++/test/std/thread/thread.threads/thread.thread.this/sleep_until.pass.cpp
@@ -7,7 +7,6 @@
 //===----------------------------------------------------------------------===//
 //
 // UNSUPPORTED: libcpp-has-no-threads
-// FLAKY_TEST.
 
 // <thread>
 
@@ -22,21 +21,15 @@
 
 int main(int, char**)
 {
-    typedef std::chrono::system_clock Clock;
-    typedef Clock::time_point time_point;
-    std::chrono::milliseconds ms(500);
-    time_point t0 = Clock::now();
-    std::this_thread::sleep_until(t0 + ms);
-    time_point t1 = Clock::now();
-    std::chrono::nanoseconds ns = (t1 - t0) - ms;
-    std::chrono::nanoseconds err = 5 * ms / 100;
-#if TEST_SLOW_HOST()
-    // 25ms is sometimes not enough tolerance on a busy jenkins slave running
-    // multiple QEMU instances. 100ms should hopefully be enough
-    err *= 4;
-#endif
-    // The time slept is within 5% of 500ms
-    assert(std::abs(ns.count()) < err.count());
+  typedef std::chrono::system_clock Clock;
+  typedef Clock::time_point time_point;
+  std::chrono::milliseconds ms(500);
+  time_point t0 = Clock::now();
+  std::this_thread::sleep_until(t0 + ms);
+  time_point t1 = Clock::now();
+  // NOTE: Operating systems are (by default) best effort and therefore we may
+  // have slept longer, perhaps much longer than we requested.
+  assert(t1 - t0 >= ms);
 
   return 0;
 }
