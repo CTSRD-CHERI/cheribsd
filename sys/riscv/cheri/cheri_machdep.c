b29ecaac4d9ded08a414d9458bca4a703be7c9bb
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2020-12-03 11:12:15 +0000

    purecap-kernel: Introduce kernel_root_sealcap global capability

--- sys/riscv/cheri/cheri_machdep.c
+++ sys/riscv/cheri/cheri_machdep.c
@@ -53,7 +53,7 @@ cheri_init_capabilities(void * __capability kroot)
 	ctemp = cheri_setaddress(kroot, CHERI_SEALCAP_KERNEL_BASE);
 	ctemp = cheri_setbounds(kroot, CHERI_SEALCAP_KERNEL_LENGTH);
 	ctemp = cheri_andperm(kroot, CHERI_SEALCAP_KERNEL_PERMS);
-	kernel_sealcap = ctemp;
+	kernel_root_sealcap = ctemp;
 
 	ctemp = cheri_setaddress(kroot, CHERI_CAP_USER_DATA_BASE);
 	ctemp = cheri_setbounds(ctemp, CHERI_CAP_USER_DATA_LENGTH);
