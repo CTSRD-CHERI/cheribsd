ce49578bb4f150643185d02dfcf7b93a920642d0
Nathaniel Wesley Filardo <nfilardo@microsoft.com>
2020-12-04 14:26:21 +0000

    tls_malloc: use load-side, don't copy out stats

--- lib/libc/gen/tls_malloc.c
+++ lib/libc/gen/tls_malloc.c
@@ -513,8 +513,8 @@ do_revoke(void)
 
 	caprevoke_epoch start_epoch = cri->epoch_enqueue;
 	while (!caprevoke_epoch_clears(cri->epoch_dequeue, start_epoch)) {
-		struct caprevoke_stats crst;
-		error = caprevoke(CAPREVOKE_LAST_PASS, start_epoch, &crst);
+		error = caprevoke(CAPREVOKE_LAST_PASS|CAPREVOKE_LOAD_SIDE,
+		    start_epoch, NULL);
 		assert(error == 0);
 	}
 }
