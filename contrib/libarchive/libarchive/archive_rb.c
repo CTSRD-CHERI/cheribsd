CTSRD-CHERI/cheribsd

commit 44b466f3d38132462eaf60621eb32aca6fe86ab1

Fix more -Wcheri-bitwise warnings in dtrace and libarchive

contrib/libarchive/libarchive/archive_rb.c
@@ -35,6 +35,8 @@
 
 #include "archive_rb.h"
 
+#include <cheri/cheric.h>
+
 /* Keep in sync with archive_rb.h */
 #define	RB_DIR_LEFT		0
 #define	RB_DIR_RIGHT		1
@@ -46,9 +48,9 @@
 #define	RB_FLAG_RED		(uintptr_t)0x1
 #define	RB_FLAG_MASK		(RB_FLAG_POSITION|RB_FLAG_RED)
 #define	RB_FATHER(rb) \
-    ((struct archive_rb_node *)((rb)->rb_info & ~RB_FLAG_MASK))
+    ((struct archive_rb_node *)cheri_clear_low_ptr_bits((rb)->rb_info, RB_FLAG_MASK))
 #define	RB_SET_FATHER(rb, father) \
-    ((void)((rb)->rb_info = (uintptr_t)(father)|((rb)->rb_info & RB_FLAG_MASK)))
+    ((void)((rb)->rb_info = (uintptr_t)(father)|cheri_get_low_ptr_bits((rb)->rb_info, RB_FLAG_MASK)))
 
 #define	RB_SENTINEL_P(rb)	((rb) == NULL)
 #define	RB_LEFT_SENTINEL_P(rb)	RB_SENTINEL_P((rb)->rb_left)
@@ -60,19 +62,22 @@
     (!RB_SENTINEL_P(rb) && !RB_LEFT_SENTINEL_P(rb) && !RB_RIGHT_SENTINEL_P(rb))
 
 #define	RB_POSITION(rb)	\
-    (((rb)->rb_info & RB_FLAG_POSITION) ? RB_DIR_RIGHT : RB_DIR_LEFT)
+    (cheri_get_low_ptr_bits((rb)->rb_info, RB_FLAG_POSITION) ? RB_DIR_RIGHT : RB_DIR_LEFT)
 #define	RB_RIGHT_P(rb)		(RB_POSITION(rb) == RB_DIR_RIGHT)
 #define	RB_LEFT_P(rb)		(RB_POSITION(rb) == RB_DIR_LEFT)
-#define	RB_RED_P(rb) 		(!RB_SENTINEL_P(rb) && (vaddr_t)((rb)->rb_info & RB_FLAG_RED) != 0)
-#define	RB_BLACK_P(rb) 		(RB_SENTINEL_P(rb) || (vaddr_t)((rb)->rb_info & RB_FLAG_RED) == 0)
+#define	RB_RED_P(rb) 		(!RB_SENTINEL_P(rb) && cheri_get_low_ptr_bits((rb)->rb_info, RB_FLAG_RED) != 0)
+#define	RB_BLACK_P(rb) 		(RB_SENTINEL_P(rb) || cheri_get_low_ptr_bits((rb)->rb_info, RB_FLAG_RED) == 0)
 #define	RB_MARK_RED(rb) 	((void)((rb)->rb_info |= RB_FLAG_RED))
-#define	RB_MARK_BLACK(rb) 	((void)((rb)->rb_info &= ~RB_FLAG_RED))
+#define	RB_MARK_BLACK(rb) 	((void)(cheri_clear_low_ptr_bits((rb)->rb_info, RB_FLAG_RED)))
+#ifndef __CHERI_PURE_CAPABILITY__
 #define	RB_INVERT_COLOR(rb) 	((void)((rb)->rb_info ^= RB_FLAG_RED))
+#endif
 #define	RB_ROOT_P(rbt, rb)	((rbt)->rbt_root == (rb))
 #define	RB_SET_POSITION(rb, position) \
     ((void)((position) ? ((rb)->rb_info |= RB_FLAG_POSITION) : \
-    ((rb)->rb_info &= ~RB_FLAG_POSITION)))
-#define	RB_ZERO_PROPERTIES(rb)	((void)((rb)->rb_info &= ~RB_FLAG_MASK))
+    ((rb)->rb_info = cheri_clear_low_ptr_bits((rb)->rb_info, RB_FLAG_POSITION))))
+#define	RB_ZERO_PROPERTIES(rb)	((void)((rb)->rb_info = cheri_clear_low_ptr_bits((rb)->rb_info, RB_FLAG_MASK)))
+#ifndef __CHERI_PURE_CAPABILITY__
 #define	RB_COPY_PROPERTIES(dst, src) \
     ((void)((dst)->rb_info ^= ((dst)->rb_info ^ (src)->rb_info) & RB_FLAG_MASK))
 #define RB_SWAP_PROPERTIES(a, b) do { \
@@ -80,6 +85,27 @@
     (a)->rb_info ^= xorinfo; \
     (b)->rb_info ^= xorinfo; \
   } while (/*CONSTCOND*/ 0)
+#else
+/*
+ * XXXAR: for CHERI purecap replace the uintcap_t xors with a standard swap
+ * This silences compiler warnings and probably won't be much worse than the xor
+ * version since we can avoid some int -> cap conversions.
+ */
+static inline void
+RB_COPY_PROPERTIES(struct archive_rb_node * const dst, struct archive_rb_node * const src)
+{
+	vaddr_t src_bits = cheri_get_low_ptr_bits(src->rb_info, RB_FLAG_MASK);
+	dst->rb_info = cheri_clear_low_ptr_bits(dst->rb_info, RB_FLAG_MASK) | src_bits;
+}
+static inline void
+RB_SWAP_PROPERTIES(struct archive_rb_node * const a, struct archive_rb_node * const b)
+{
+	vaddr_t a_bits = cheri_get_low_ptr_bits(a->rb_info, RB_FLAG_MASK);
+	vaddr_t b_bits = cheri_get_low_ptr_bits(b->rb_info, RB_FLAG_MASK);
+	a->rb_info = cheri_clear_low_ptr_bits(a->rb_info, RB_FLAG_MASK) | b_bits;
+	b->rb_info = cheri_clear_low_ptr_bits(b->rb_info, RB_FLAG_MASK) | a_bits;
+}
+#endif
 
 static void __archive_rb_tree_insert_rebalance(struct archive_rb_tree *,
     struct archive_rb_node *);

cddl/contrib/opensolaris/lib/libdtrace/common/dt_module.c
@@ -1159,7 +1159,10 @@ dt_module_update(dtrace_hdl_t *dtp, struct kld_file_stat *k_stat)
 	GElf_Ehdr ehdr;
 	GElf_Phdr ph;
 	char name[MAXPATHLEN];
-	uintptr_t mapbase, alignmask;
+	uintptr_t mapbase;
+#ifndef __CHERI__
+	uintptr_t alignmask;
+#endif
 	int i = 0;
 	int is_elf_obj;
 
@@ -1232,9 +1235,13 @@ dt_module_update(dtrace_hdl_t *dtp, struct kld_file_stat *k_stat)
 		if (sh.sh_size == 0)
 			continue;
 		if (sh.sh_type == SHT_PROGBITS || sh.sh_type == SHT_NOBITS) {
+#if __has_builtin(__builtin_align_up)
+			mapbase = __builtin_align_up(mapbase, sh.sh_addralign);
+#else
 			alignmask = sh.sh_addralign - 1;
 			mapbase += alignmask;
 			mapbase &= ~alignmask;
+#endif
 			sh.sh_addr = mapbase;
 			if (is_elf_obj)
 				dmp->dm_sec_offsets[elf_ndxscn(sp)] = sh.sh_addr;

cddl/contrib/opensolaris/lib/libdtrace/common/dt_printf.c
@@ -1262,8 +1262,7 @@ dt_printf_getint(dtrace_hdl_t *dtp, const dtrace_recdesc_t *recp,
 
 	if (addr + sizeof (int) > (uintptr_t)buf + len)
 		return (dt_set_errno(dtp, EDT_DOFFSET));
-
-	if (addr & (uintptr_t)(recp->dtrd_alignment - 1))
+	if (!__builtin_is_aligned(addr, recp->dtrd_alignment))
 		return (dt_set_errno(dtp, EDT_DALIGN));
 
 	switch (recp->dtrd_size) {
@@ -1507,7 +1506,7 @@ dt_printf_format(dtrace_hdl_t *dtp, FILE *fp, const dt_pfargv_t *pfv,
 		}
 
 		if (rec->dtrd_alignment != 0 &&
-		    ((uintptr_t)addr & (uintptr_t)(rec->dtrd_alignment - 1)) != 0) {
+		    !__builtin_is_aligned(addr, rec->dtrd_alignment)) {
 			dt_dprintf("bad align: addr=%p size=0x%x align=0x%x\n",
 			    (void *)addr, rec->dtrd_size, rec->dtrd_alignment);
 			return (dt_set_errno(dtp, EDT_DALIGN));

cddl/contrib/opensolaris/lib/libzpool/common/sys/zfs_context.h
@@ -389,7 +389,7 @@ typedef struct taskq_ent {
 	struct taskq_ent	*tqent_prev;
 	task_func_t		*tqent_func;
 	void			*tqent_arg;
-	uintptr_t		tqent_flags;
+	vaddr_t			tqent_flags;
 } taskq_ent_t;
 
 #define	TQENT_FLAG_PREALLOC	0x1	/* taskq_dispatch_ent used */

contrib/libarchive/libarchive/archive_read_support_format_lha.c
@@ -1702,7 +1702,11 @@ lha_crc16(uint16_t crc, const void *pp, size_t len)
 		return crc;
 
 	/* Process unaligned address. */
-	if (((uintptr_t)p) & (uintptr_t)0x1) {
+#if __has_builtin(__builtin_is_aligned)
+	if (!__builtin_is_aligned(p, sizeof(uint16_t))) {
+#else
+	if (((vaddr_t)p) & (vaddr_t)0x1) {
+#endif
 		crc = (crc >> 8) ^ crc16tbl[0][(crc ^ *p++) & 0xff];
 		len--;
 	}

commit 9cf8b8cb13426ca125303850c1f8fac0ed66a85a

libarchive: fix missing assignment in RB_MARK_BLACK
Looks like I broke this in 44b466f3d38132462eaf60621eb32aca6fe86ab1.
This was causing crashes in many of the mtree tests.

contrib/libarchive/libarchive/archive_rb.c
@@ -88,7 +88,7 @@
 #define	RB_RED_P(rb) 		(!RB_SENTINEL_P(rb) && __get_bits((rb)->rb_info, RB_FLAG_RED) != 0)
 #define	RB_BLACK_P(rb) 		(RB_SENTINEL_P(rb) || __get_bits((rb)->rb_info, RB_FLAG_RED) == 0)
 #define	RB_MARK_RED(rb) 	((void)((rb)->rb_info |= RB_FLAG_RED))
-#define	RB_MARK_BLACK(rb) 	((void)(__clear_bits((rb)->rb_info, RB_FLAG_RED)))
+#define	RB_MARK_BLACK(rb) 	((void)((rb)->rb_info = __clear_bits((rb)->rb_info, RB_FLAG_RED)))
 #ifndef __CHERI_PURE_CAPABILITY__
 #define	RB_INVERT_COLOR(rb) 	((void)((rb)->rb_info ^= RB_FLAG_RED))
 #endif