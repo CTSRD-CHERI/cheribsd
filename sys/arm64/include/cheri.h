39d8ae15e386044ac5137ede2228ed7bc8a4b74e
Jessica Clarke <jrtc27@jrtc27.com>
2020-12-15 18:21:44 +0000

    morello: Initialise userspace and kernel root capabilities on boot

--- sys/arm64/include/cheri.h
+++ sys/arm64/include/cheri.h
@@ -40,6 +40,7 @@
 /*
  * Morello specific kernel utility functions.
  */
+void		cheri_init_capabilities(void * __capability kroot);
 void		hybridabi_thread_setregs(struct thread *td, unsigned long entry_addr);
 int		cheri_esr_to_sicode(uint64_t esr);
 const char	*cheri_fsc_string(uint8_t fsc);
