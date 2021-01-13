fff69c1738dc5370488d8b3a069e6434f2afdc5f
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2021-01-13 17:20:57 +0000

    purecap-kernel: Add purecap-only variant of cheri_andperm.

--- sys/cheri/cheric.h
+++ sys/cheri/cheric.h
@@ -302,6 +302,7 @@ cheri_bytes_remaining(const void * __capability cap)
 #define	cheri_kern_setaddress(x, y)	cheri_setaddress(x, y)
 #define	cheri_kern_getaddress(x)	cheri_setaddress(x)
 #define	cheri_kern_getbase(x)		cheri_getbase(x)
+#define	cheri_kern_andperm(x, y)	cheri_andperm(x, y)
 #else
 #define	cheri_kern_gettag(x)					\
 	(((x) == NULL || (vm_offset_t)(x) < 4096) ? 0 : 1)
@@ -310,6 +311,7 @@ cheri_bytes_remaining(const void * __capability cap)
 #define	cheri_kern_setaddress(x, y)	((__typeof__(x))(y))
 #define	cheri_kern_getaddress(x)	((uintptr_t)(x))
 #define	cheri_kern_getbase(x)		((uintptr_t)(x))
+#define	cheri_kern_andperm(x, y)	(x)
 #endif
 
 /*
