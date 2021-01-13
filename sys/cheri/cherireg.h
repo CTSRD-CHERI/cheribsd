0d97b709fc19a4b73b98a2b9b27c95cd332cf34f
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2021-01-13 16:20:13 +0000

    purecap-kernel: Move CHERI_REPRESENTABLE_* macros to cheric.h
    There is no reason they should live in cherireg and cheric.h is more appropriate
    as they are always defined.

--- sys/cheri/cherireg.h
+++ sys/cheri/cherireg.h
@@ -115,41 +115,4 @@
     (CHERI_OTYPE_KERN_MAX - CHERI_OTYPE_KERN_MIN + 1)
 #define	CHERI_SEALCAP_KERNEL_OFFSET	0x0
 
-#define	CHERI_REPRESENTABLE_LENGTH(len) \
-	__builtin_cheri_round_representable_length(len)
-#define	CHERI_REPRESENTABLE_ALIGNMENT_MASK(len) \
-	__builtin_cheri_representable_alignment_mask(len)
-
-/* Provide macros to make it easier to work with the raw CRAM/CRRL results: */
-#define	CHERI_REPRESENTABLE_ALIGNMENT(len) \
-	(~CHERI_REPRESENTABLE_ALIGNMENT_MASK(len) + 1)
-#define	CHERI_REPRESENTABLE_BASE(base, len) \
-	((base) & CHERI_REPRESENTABLE_ALIGNMENT_MASK(len))
-
-/*
- * In the current encoding sealed and unsealed capabilities have the same
- * alignment constraints.
- */
-#define	CHERI_SEALABLE_LENGTH(len)	\
-	CHERI_REPRESENTABLE_LENGTH(len)
-#define	CHERI_SEALABLE_ALIGNMENT_MASK(len)	\
-	CHERI_REPRESENTABLE_ALIGNMENT_MASK(len)
-#define	CHERI_SEALABLE_ALIGNMENT(len)	\
-	CHERI_REPRESENTABLE_ALIGNMENT(len)
-#define	CHERI_SEALABLE_BASE(base, len)	\
-	CHERI_REPRESENTABLE_BASE(base, len)
-
-/* A mask for the lower bits, i.e. the negated alignment mask */
-#define	CHERI_SEAL_ALIGN_MASK(l)	~(CHERI_SEALABLE_ALIGNMENT_MASK(l))
-#define	CHERI_ALIGN_MASK(l)		~(CHERI_REPRESENTABLE_ALIGNMENT_MASK(l))
-
-/*
- * TODO: avoid using these since count leading/trailing zeroes is expensive on
- * BERI/CHERI
- */
-#define	CHERI_ALIGN_SHIFT(l)	\
-	__builtin_ctzll(CHERI_REPRESENTABLE_ALIGNMENT_MASK(l))
-#define	CHERI_SEAL_ALIGN_SHIFT(l)	\
-	__builtin_ctzll(CHERI_SEALABLE_ALIGNMENT_MASK(l))
-
 #endif /* !__SYS_CHERIREG_H__ */
