096d522c84856192c9b853058dbc4c80deb27502
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2021-01-19 15:07:53 +0000

    purecap-kernel: Drop purecap-only version of cheri_getbase.
    This does not make sense in hybrid mode and has no sensible no-op that can
    be substituted with.

--- sys/cheri/cheric.h
+++ sys/cheri/cheric.h
@@ -208,7 +208,6 @@ cheri_bytes_remaining(const void * __capability cap)
 #define	cheri_kern_setboundsexact(x, y)	cheri_setboundsexact(x, y)
 #define	cheri_kern_setaddress(x, y)	cheri_setaddress(x, y)
 #define	cheri_kern_getaddress(x)	cheri_setaddress(x)
-#define	cheri_kern_getbase(x)		cheri_getbase(x)
 #define	cheri_kern_andperm(x, y)	cheri_andperm(x, y)
 #else
 #define	cheri_kern_gettag(x)					\
@@ -217,7 +216,6 @@ cheri_bytes_remaining(const void * __capability cap)
 #define	cheri_kern_setboundsexact(x, y)	(x)
 #define	cheri_kern_setaddress(x, y)	((__typeof__(x))(y))
 #define	cheri_kern_getaddress(x)	((uintptr_t)(x))
-#define	cheri_kern_getbase(x)		((uintptr_t)(x))
 #define	cheri_kern_andperm(x, y)	(x)
 #endif
 
