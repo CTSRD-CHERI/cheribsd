209a592c53a398445d7b8aa5c58e12ca5e4e6a25
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2021-01-26 18:08:40 +0000

    Update kmap allocation functions to use reservations.
    This will enforce representability in the kernel allocations.

--- sys/vm/vm_kern.c
+++ sys/vm/vm_kern.c
@@ -633,11 +633,17 @@ kmem_free(vm_offset_t addr, vm_size_t size)
 vm_offset_t
 kmap_alloc_wait(vm_map_t map, vm_size_t size)
 {
+	int error;
+	vm_size_t padded_size;
+	vm_offset_t alignment;
 	vm_offset_t addr;
+	vm_pointer_t mapped;
 
 	size = round_page(size);
 	if (!swap_reserve(size))
 		return (0);
+	padded_size = CHERI_REPRESENTABLE_LENGTH(size);
+	alignment = CHERI_REPRESENTABLE_ALIGNMENT(size);
 
 	for (;;) {
 		/*
@@ -645,9 +651,16 @@ kmap_alloc_wait(vm_map_t map, vm_size_t size)
 		 * to lock out sleepers/wakers.
 		 */
 		vm_map_lock(map);
-		addr = vm_map_findspace(map, vm_map_min(map), size);
-		if (addr + size <= vm_map_max(map))
+		addr = vm_map_findspace(map, vm_map_min(map), padded_size);
+		if (addr + padded_size <= vm_map_max(map) && alignment == 0)
 			break;
+		if (alignment > 0) {
+			error = vm_map_alignspace(map, NULL, 0, &addr,
+			    padded_size, vm_map_max(map), alignment);
+			if (error == KERN_SUCCESS)
+				break;
+		}
+
 		/* no space now; see if we can ever get space */
 		if (vm_map_max(map) - vm_map_min(map) < size) {
 			vm_map_unlock(map);
@@ -657,10 +670,19 @@ kmap_alloc_wait(vm_map_t map, vm_size_t size)
 		map->needs_wakeup = TRUE;
 		vm_map_unlock_and_wait(map, 0);
 	}
-	vm_map_insert(map, NULL, 0, addr, addr + size, VM_PROT_RW_CAP,
-	    VM_PROT_RW_CAP, MAP_ACC_CHARGED, VM_MIN_KERNEL_ADDRESS);
+
+	mapped = addr;
+	if (vm_map_reservation_create_locked(map, &mapped, padded_size,
+	    VM_PROT_RW_CAP)) {
+		vm_map_unlock(map);
+		swap_release(size);
+		return (0);
+	}
+	vm_map_insert(map, NULL, 0, mapped, mapped + size, VM_PROT_RW_CAP,
+	    VM_PROT_RW_CAP, MAP_ACC_CHARGED, mapped);
 	vm_map_unlock(map);
-	return (addr);
+
+	return (mapped);
 }
 
 /*
@@ -674,7 +696,9 @@ kmap_free_wakeup(vm_map_t map, vm_offset_t addr, vm_size_t size)
 {
 
 	vm_map_lock(map);
-	(void) vm_map_delete(map, trunc_page(addr), round_page(addr + size));
+	(void) vm_map_remove_locked(map, trunc_page(addr),
+	    round_page(addr + size));
+
 	if (map->needs_wakeup) {
 		map->needs_wakeup = FALSE;
 		vm_map_wakeup(map);
