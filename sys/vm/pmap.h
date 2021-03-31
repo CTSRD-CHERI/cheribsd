f91dc1993b4284b4e89dde5615a7bb2972cc24cd
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2021-03-31 11:56:40 +0100

    purecap-kernel: Update pmap signatures.

--- sys/vm/pmap.h
+++ sys/vm/pmap.h
@@ -147,8 +147,9 @@ boolean_t	 pmap_is_modified(vm_page_t m);
 boolean_t	 pmap_is_prefaultable(pmap_t pmap, vm_offset_t va);
 boolean_t	 pmap_is_referenced(vm_page_t m);
 boolean_t	 pmap_is_valid_memattr(pmap_t, vm_memattr_t);
-vm_offset_t	 pmap_map(vm_offset_t *, vm_paddr_t, vm_paddr_t, int);
-int		 pmap_mincore(pmap_t pmap, vm_offset_t addr, vm_paddr_t *pap);
+vm_pointer_t	 pmap_map(vm_pointer_t *, vm_paddr_t, vm_paddr_t, int);
+int		 pmap_mincore(pmap_t pmap, vm_offset_t addr,
+		    vm_paddr_t *pap);
 void		 pmap_object_init_pt(pmap_t pmap, vm_offset_t addr,
 		    vm_object_t object, vm_pindex_t pindex, vm_size_t size);
 boolean_t	 pmap_page_exists_quick(pmap_t pmap, vm_page_t m);
@@ -159,7 +160,7 @@ void		 pmap_pinit0(pmap_t);
 void		 pmap_protect(pmap_t, vm_offset_t, vm_offset_t, vm_prot_t);
 void		 pmap_qenter(vm_offset_t, vm_page_t *, int);
 void		 pmap_qremove(vm_offset_t, int);
-vm_offset_t	 pmap_quick_enter_page(vm_page_t);
+vm_pointer_t	 pmap_quick_enter_page(vm_page_t);
 void		 pmap_quick_remove_page(vm_offset_t);
 void		 pmap_release(pmap_t);
 void		 pmap_remove(pmap_t, vm_offset_t, vm_offset_t);
@@ -192,6 +193,9 @@ void		 pmap_zero_page_area(vm_page_t, int off, int size);
 //   "target_type": "header",
 //   "changes": [
 //     "support"
+//   ],
+//   "changes_purecap": [
+//     "pointer_as_integer"
 //   ],
 //   "change_comment": ""
 // }
