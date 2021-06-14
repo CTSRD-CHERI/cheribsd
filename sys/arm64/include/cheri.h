836aec7f716270c9dc999893268cf560f88caf6b
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2021-06-14 23:00:27 +0100

    Add Morello macros to start stop CHERI tracing on the ARM FVP.

--- sys/arm64/include/cheri.h
+++ sys/arm64/include/cheri.h
@@ -38,6 +38,17 @@
 #define	__USER_PCC	((void * __capability)curthread->td_frame->tf_elr)
 
 /*
+ * Special marker NOPs for the Morello FVP to start / stop region of interest
+ * in trace.
+ */
+#define	CHERI_START_TRACE	do {					\
+	__asm__ __volatile__("hlt 0xbeef");				\
+} while(0)
+#define	CHERI_STOP_TRACE	do {					\
+	__asm__ __volatile__("hlt 0xbeef");				\
+} while(0)
+
+/*
  * Morello specific kernel utility functions.
  */
 void		cheri_init_capabilities(void * __capability kroot);
