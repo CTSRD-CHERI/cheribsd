dcee4e56cdd843f7654daf46eda4c9cbc160baeb
Mark Johnston <markj@FreeBSD.org>
2021-08-26 09:46:50 -0400

    Assert that vm_map_entry reservation IDs are non-decreasing
    This helps catch inconsistencies if debug.vmmap_check is set to 1 or 2
    during tests.

--- sys/vm/vm_map.c
+++ sys/vm/vm_map.c
@@ -6074,6 +6074,10 @@ _vm_map_assert_consistent(vm_map_t map, int check)
 		KASSERT(entry->start < entry->end,
 		    ("map %p start = %jx, end = %jx", map,
 		    (uintmax_t)entry->start, (uintmax_t)entry->end));
+		KASSERT(prev->reservation <= entry->reservation,
+		    ("map %p prev->reservation = %jx, reservation = %jx", map,
+		    (uintmax_t)prev->reservation,
+		    (uintmax_t)entry->reservation));
 		KASSERT(entry->left == header ||
 		    entry->left->start < entry->start,
 		    ("map %p left->start = %jx, start = %jx", map,
