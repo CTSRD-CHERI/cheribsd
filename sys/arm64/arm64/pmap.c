d7007ddf7b89c1cfaea3c887e8548e80a16c5583
Brett Gutstein <brett.gutstein@cst.cam.ac.uk>
2021-09-15 12:52:38 +0100

    capdirty: morello: prevent promotion of pages with CDBM !SC
    This is to avoid synchronization errors as a result of faults and
    also because the revoker creates PTEs with CDBM !SC without doing a
    TLB shootdown and we don't want to lose capability stores.

--- sys/arm64/arm64/pmap.c
+++ sys/arm64/arm64/pmap.c
@@ -3917,6 +3917,22 @@ pmap_promote_l2(pmap_t pmap, pd_entry_t *l2, vm_offset_t va,
 	}
 
 setl2:
+#if __has_feature(capabilities)
+		/*
+		 * Prohibit superpages involving CDBM-set SC-clear PTEs.  The
+		 * revoker creates these without TLB shootdown, and so there
+		 * may be a SC-set TLBE still in the system.  Thankfully,
+		 * these are ephemera: either they'll transition to CD-set
+		 * or CW-clear in the next revocation epoch.
+		 */
+		if ((newl2 & (ATTR_CDBM | ATTR_SC)) == ATTR_CDBM) {
+			atomic_add_long(&pmap_l2_p_failures, 1);
+			CTR2(KTR_PMAP, "pmap_promote_l2: CDBM failure for va "
+			    "%#lx in pmap %p", va, pmap);
+			return;
+		}
+#endif
+
 	if ((newl2 & (ATTR_S1_AP_RW_BIT | ATTR_SW_DBM)) ==
 	    (ATTR_S1_AP(ATTR_S1_AP_RO) | ATTR_SW_DBM)) {
 		/*
@@ -3950,6 +3966,21 @@ setl3:
 			    " in pmap %p", va, pmap);
 			return;
 		}
+#if __has_feature(capabilities)
+		/*
+		 * Prohibit superpages involving CDBM-set SC-clear PTEs.  The
+		 * revoker creates these without TLB shootdown, and so there
+		 * may be a SC-set TLBE still in the system.  Thankfully,
+		 * these are ephemera: either they'll transition to CD-set
+		 * or CW-clear in the next revocation epoch.
+		 */
+		if ((oldl3 & (ATTR_CDBM | ATTR_SC)) == ATTR_CDBM) {
+			atomic_add_long(&pmap_l2_p_failures, 1);
+			CTR2(KTR_PMAP, "pmap_promote_l2: CDBM failure for va "
+			    "%#lx in pmap %p", va, pmap);
+			return;
+		}
+#endif
 		pa -= PAGE_SIZE;
 	}
 
