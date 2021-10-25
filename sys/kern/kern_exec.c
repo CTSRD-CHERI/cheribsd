a15e56b105953f6cab496c6ecae7e3dadf1c3a3b
John Baldwin <jhb@FreeBSD.org>
2021-10-25 13:56:26 -0700

    Defer exec_free_args() to support the opportunistic case.
    The args were previously being freed before both of the fallback cases
    (in do_execve and in kern_execve) resulting in a use after free when
    the fallback ran.
    
    Possibly the better solution is to move exec_free_args out to the same
    function that copies in the args (except in the internal exit case).

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -453,6 +453,7 @@ kern_execve(struct thread *td, struct image_args *args,
 		case ENOEXEC:
 		case ENOENT:
 		case ELOOP:
+			exec_free_args(args);
 			return (error);
 		default:
 			break;
@@ -1053,7 +1054,6 @@ exec_fail:
 	mac_execve_exit(imgp);
 	mac_execve_interpreter_exit(interpvplabel);
 #endif
-	exec_free_args(args);
 
 	/*
 	 * Handle deferred decrement of ref counts.
@@ -1080,6 +1080,8 @@ exec_fail:
 			if (error == EJUSTRETURN)
 				return (error);
 		}
+		exec_free_args(args);
+
 		/* sorry, no more process anymore. exit gracefully */
 		if (cop != NULL)
 			PRELE(cop);
@@ -1087,6 +1089,8 @@ exec_fail:
 		exit1(td, 0, SIGABRT);
 		/* NOT REACHED */
 	}
+	if (!opportunistic)
+		exec_free_args(args);
 
 #ifdef KTRACE
 	if (error == 0)
