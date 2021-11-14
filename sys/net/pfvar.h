df1d3b2a95b2b1f3d1e1dfce62f76f103071ee63
Kristof Provost <kp@FreeBSD.org>
2021-11-14 04:57:17 +0100

    pf: renumber DIOCKEEPCOUNTERS
    We accidentally had two ioctls use the same base number
    (DIOCKEEPCOUNTERS and DIOCGIFSPEEDV{0,1}). We get away with that on most
    platforms because the size of the argument structures is different.
    This does break CHERI, and is generally a bad idea anyway.
    Renumber to avoid this collision.
    
    Reported by:	jhb
    
    (cherry picked from commit 047c4e365d15c4561fdffd9be650ac1245ce422c)

--- sys/net/pfvar.h
+++ sys/net/pfvar.h
@@ -1305,7 +1305,9 @@ struct pfioc_iface {
 #define	DIOCSETIFFLAG	_IOWR('D', 89, struct pfioc_iface)
 #define	DIOCCLRIFFLAG	_IOWR('D', 90, struct pfioc_iface)
 #define	DIOCKILLSRCNODES	_IOWR('D', 91, struct pfioc_src_node_kill)
-#define	DIOCKEEPCOUNTERS	_IOWR('D', 92, struct pfioc_nv)
+#define	DIOCGIFSPEEDV0	_IOWR('D', 92, struct pf_ifspeed_v0)
+#define	DIOCGIFSPEEDV1	_IOWR('D', 92, struct pf_ifspeed_v1)
+#define	DIOCKEEPCOUNTERS	_IOWR('D', 96, struct pfioc_nv)
 
 struct pf_ifspeed_v0 {
 	char			ifname[IFNAMSIZ];
@@ -1321,9 +1323,6 @@ struct pf_ifspeed_v1 {
 
 /* Latest version of struct pf_ifspeed_vX */
 #define PF_IFSPEED_VERSION	1
-
-#define	DIOCGIFSPEEDV0	_IOWR('D', 92, struct pf_ifspeed_v0)
-#define	DIOCGIFSPEEDV1	_IOWR('D', 92, struct pf_ifspeed_v1)
 
 /*
  * Compatibility and convenience macros
