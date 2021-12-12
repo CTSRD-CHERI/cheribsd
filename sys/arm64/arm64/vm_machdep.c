aea7acf854235a7a1c0a71865dce8a9cf3cb654f
Edward Tomasz Napierala <trasz@FreeBSD.org>
2021-12-12 23:54:58 +0000

    Add missing calls to colocation_cleanup()

--- sys/arm64/arm64/vm_machdep.c
+++ sys/arm64/arm64/vm_machdep.c
@@ -111,6 +111,7 @@ cpu_fork(struct thread *td1, struct proc *p2, struct thread *td2, int flags)
 
 #if __has_feature(capabilities)
 	p2->p_md.md_sigcode = td1->td_proc->p_md.md_sigcode;
+	colocation_cleanup(td2);
 #endif
 
 #ifdef PAC
@@ -307,6 +308,9 @@ cpu_thread_alloc(struct thread *td)
 	    (struct trapframe *)td->td_pcb - 1);
 #ifdef PAC
 	ptrauth_thread_alloc(td);
+#endif
+#if __has_feature(capabilities)
+	colocation_cleanup(td);
 #endif
 }
 
