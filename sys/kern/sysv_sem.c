41131757df7b76be9de91653d09f94fb4aeb93a6
John Baldwin <jhb@FreeBSD.org>
2021-12-27 10:34:08 -0800

    kern_semctl: Use PTR2CAP for kernel pointers passed to bcopy_c.
    These are currently converted implicitly, but a future change to
    implement bcopy_c as a macro around memmove_c adds extra parentheses
    that breaks the implicit conversion.

--- sys/kern/sysv_sem.c
+++ sys/kern/sysv_sem.c
@@ -841,7 +841,7 @@ kern_semctl(struct thread *td, int semid, int semnum, int cmd,
 		if (error != 0)
 			goto done2;
 #endif
-		bcopy_c(&semakptr->u, arg->buf, sizeof(struct semid_ds));
+		bcopy_c(PTR2CAP(&semakptr->u), arg->buf, sizeof(struct semid_ds));
 		if (cred->cr_prison != semakptr->cred->cr_prison)
 			arg->buf->sem_perm.key = IPC_PRIVATE;
 		*rval = IXSEQ_TO_IPCID(semid, semakptr->u.sem_perm);
@@ -897,7 +897,7 @@ kern_semctl(struct thread *td, int semid, int semnum, int cmd,
 			goto done2;
 		if ((error = ipcperm(td, &semakptr->u.sem_perm, IPC_R)))
 			goto done2;
-		bcopy_c(&semakptr->u, arg->buf, sizeof(struct semid_ds));
+		bcopy_c(PTR2CAP(&semakptr->u), arg->buf, sizeof(struct semid_ds));
 		if (cred->cr_prison != semakptr->cred->cr_prison)
 			arg->buf->sem_perm.key = IPC_PRIVATE;
 
