f8420811ebd1487e803404a6a8ce693ac32a8100
John Baldwin <jhb@FreeBSD.org>
2022-01-13 09:51:35 -0800

    i386 copyout: Update for vm_fault_quick_hold_pages API change.

--- sys/i386/i386/copyout.c
+++ sys/i386/i386/copyout.c
@@ -103,7 +103,7 @@ cp_slow0(vm_offset_t uva, size_t len, bool write,
 	plen = howmany(uva - trunc_page(uva) + len, PAGE_SIZE);
 	MPASS(plen <= nitems(m));
 	error = 0;
-	i = vm_fault_quick_hold_pages(&curproc->p_vmspace->vm_map, uva, len,
+	i = vm_fault_quick_hold_pages(&curproc->p_vmspace->vm_map, (void *)uva, len,
 	    (write ? VM_PROT_WRITE : VM_PROT_READ) | VM_PROT_QUICK_NOFAULT,
 	    m, nitems(m));
 	if (i != plen)
