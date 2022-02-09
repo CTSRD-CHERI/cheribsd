eee9d2a221b7133ad4533396bcb083e04cfa036b
John Baldwin <jhb@FreeBSD.org>
2022-02-08 16:42:19 -0800

    aarch64: Add more sanity checks in unwind_frame for purecap.
    If unwind_frame faults, things go off the rails fairly quickly, hence
    the addition of kstack_contains in an earlier commit.  For the purecap
    kernel, add additional checks that the frame pointer can be
    dereferenced to read the next frame pointer and PC.  Check bounds,
    tag, and permissions explicitly and fail to unwind if it would fault.

--- sys/arm64/arm64/unwind.c
+++ sys/arm64/arm64/unwind.c
@@ -44,6 +44,15 @@ unwind_frame(struct thread *td, struct unwind_state *frame)
 	if (!kstack_contains(td, fp, sizeof(uintptr_t) * 2))
 		return (false);
 
+#ifdef __CHERI_PURE_CAPABILITY__
+	if ((ptraddr_t)fp < cheri_getbase(fp) ||
+	    (ptraddr_t)(fp + sizeof(uintptr_t) * 2) >= cheri_gettop(fp) ||
+	    cheri_gettag(fp) == 0 ||
+	    (cheri_getperm(fp) & (CHERI_PERM_LOAD | CHERI_PERM_LOAD_CAP)) !=
+	    (CHERI_PERM_LOAD | CHERI_PERM_LOAD_CAP))
+		return (false);
+#endif
+
 	/* FP to previous frame (X29) */
 	frame->fp = ((uintptr_t *)fp)[0];
 	/* LR (X30) */
