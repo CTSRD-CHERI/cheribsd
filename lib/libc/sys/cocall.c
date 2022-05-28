40190bc688c1f0012ce785c345bd43de532627eb
Edward Tomasz Napierala <trasz@FreeBSD.org>
2022-05-28 00:09:23 +0000

    cocall(2): Transparently fall back to cocall_slow(2)
    This takes care of handling of EAGAIN and EPROTOTYPE
    returns from _cocall(2).

--- lib/libc/sys/cocall.c
+++ lib/libc/sys/cocall.c
@@ -32,17 +32,67 @@
 __FBSDID("$FreeBSD$");
 
 #include "namespace.h"
+#include <errno.h>
 #include <unistd.h>
 #include "cheri_private.h"
 #include "un-namespace.h"
+
+int __usleep(useconds_t);	// XXX
 
 int
 cocall(void * __capability target,
     const void * __capability outbuf, size_t outlen,
     void * __capability inbuf, size_t inlen)
 {
+	int error;
 
 	_trace_cocall((ptraddr_t)target, outlen, inlen);
-	return (_cocall(_cocall_code, _cocall_data, target,
-	    outbuf, outlen, inbuf, inlen));
+
+	/* XXX This loop is like this for no particular reason. */
+	for (;;) {
+		error = _cocall(_cocall_code, _cocall_data, target,
+		    outbuf, outlen, inbuf, inlen);
+
+		if (__predict_true(error == 0))
+			return (error);
+
+		switch (errno) {
+		case EAGAIN:
+			/*
+			 * So, originally this part looked like this:
+			 */
+#if 0
+			/*
+			 * EAGAIN means the caller hasn't entered coaccept(2)
+			 * yet.  We will need to enter anyhow, if only to wait,
+			 * so why not just retry the whole thing.
+			 */
+			error = cocall_slow(target, outbuf, outlen, inbuf, inlen);
+			break;
+#endif
+			/*
+			 * BUT: Suppose we got EAGAIN from _cocall(2);
+			 * if we fall back to cocall_slow(2) we will block
+			 * there, and if the callee calls _coaccept(2),
+			 * there is no code path to wake us up.  If the callee
+			 * chooses cocall_slow(2) instead, then we will receive
+			 * an EPROTOTYPE, handled below.
+			 */
+			__usleep(1000);
+			continue;
+
+		case EPROTOTYPE:
+			/*
+			 * EPROTOTYPE means the callee is waiting on
+			 * coaccept_slow(2), not _coaccept(2).  It's a syscall,
+			 * so it's slow anyway; this whole switch is the slow
+			 * path.
+			 */
+			return (cocall_slow(target, outbuf, outlen, inbuf, inlen));
+
+		default:
+			return (error);
+		}
+	}
+
 }
