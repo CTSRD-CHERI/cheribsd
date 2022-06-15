357ebfa2f9a40f766bfb0ad38338ec6f7c734112
Brooks Davis <brooks@one-eyed-alien.net>
2022-06-15 17:26:14 +0100

    kldstat: vaddr_t -> ptraddr_t

--- sbin/kldstat/kldstat.c
+++ sbin/kldstat/kldstat.c
@@ -53,7 +53,7 @@ __FBSDID("$FreeBSD$");
 #include <strings.h>
 #include <unistd.h>
 
-#define	PTR_WIDTH ((int)(sizeof(vaddr_t) * 2 + 2))
+#define	PTR_WIDTH ((int)(sizeof(ptraddr_t) * 2 + 2))
 
 static void printmod(int);
 static void printfile(int, int, int);
