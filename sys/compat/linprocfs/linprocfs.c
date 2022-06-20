5a26f0c87e36b9398a1cf800a6b415cb31f47e8e
Brooks Davis <brooks@one-eyed-alien.net>
2022-06-21 00:24:58 +0100

    Chase move of vm_stacktop

--- sys/compat/linprocfs/linprocfs.c
+++ sys/compat/linprocfs/linprocfs.c
@@ -1350,7 +1350,7 @@ linprocfs_doprocmaps(PFS_FILL_ARGS)
 				if (e_start == p->p_sysent->sv_shared_page_base +
 				    PAGE_SIZE)
 					name = vdso_str;
-				if (e_end == p->p_vmspace->vm_stacktop)
+				if (e_end == p->p_vm_stacktop)
 					name = stack_str;
 			}
 		}
