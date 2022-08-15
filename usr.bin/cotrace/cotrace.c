777b4aa3ac061ecc604f5a366da197a2603db6ae
Edward Tomasz Napierala <trasz@FreeBSD.org>
2022-08-15 14:39:39 +0000

    cotrace(1): Trace all the services, not a single one
    It's using pthreads now.

--- usr.bin/cotrace/cotrace.c
+++ usr.bin/cotrace/cotrace.c
@@ -28,10 +28,6 @@
  * SUCH DAMAGE.
  */
 
-/*
- * XXX: Make -i optional; would probably need pthreads.
- */
-
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
@@ -44,6 +40,8 @@ __FBSDID("$FreeBSD$");
 #include <ctype.h>
 #include <err.h>
 #include <errno.h>
+#include <pthread.h>
+#include <pthread_np.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -52,13 +50,23 @@ __FBSDID("$FreeBSD$");
 #include <unistd.h>
 #include <vis.h>
 
+struct interposer {
+	int			i_index;
+	void * __capability	i_target;
+	void * __capability	i_public;
+	pthread_t		i_pthread;
+};
+
 static bool Cflag = false, kflag = false, vflag = false;
+static pthread_mutex_t mtx;
+static pthread_cond_t donecv;
+static int pending;
 
 static void
 usage(void)
 {
 
-	fprintf(stderr, "usage: cotrace [-Ckv] -i entry command [args ...]\n");
+	fprintf(stderr, "usage: cotrace [-Ckv] command [args ...]\n");
 	exit(0);
 }
 
@@ -70,7 +78,7 @@ sigchld_handler(int dummy __unused)
 }
 
 static void
-answerback(capv_answerback_t *out, void * __capability target)
+answerback(capv_answerback_t *out, const void * __capability target)
 {
 	int error, mode;
 	char *tmp;
@@ -89,13 +97,13 @@ answerback(capv_answerback_t *out, void * __capability target)
 	out->len = sizeof(*out);
 	out->op = 0;
 	snprintf(out->answerback, sizeof(out->answerback),
-	    "%s -- via cotrace(1), pid %d, tracing %#lp%s%s",
-	    tmp, getpid(), target, kflag ? " (slow)" : "",
+	    "%s -- via cotrace(1), pid %d, threadid %d, tracing %#lp%s%s",
+	    tmp, getpid(), pthread_getthreadid_np(), target, kflag ? " (slow)" : "",
 	    mode ? "" : " (capsicum disabled)");
 }
 
-int
-main(int argc, char **argv)
+static void *
+interpose(void *tmp)
 {
 	union {
 		capv_t		cap;
@@ -104,58 +112,14 @@ main(int argc, char **argv)
 	} inbuf, outbuf;
 	capv_t *in = &inbuf.cap;
 	capv_t *out = &outbuf.cap;
-	struct sigaction sa;
-	void * __capability target = NULL;
-	void * __capability public;
 	void * __capability cookie;
-	void * __capability *capv;
-	char *tmp = NULL;
 	char dumpbuf[MAXBSIZE * 4 + 1];
 	ssize_t received;
 	pid_t pid;
-	int chosen = -1;
-	int capc, ch, error, len;
+	int error, len;
+	struct interposer *interposer;
 
-	while ((ch = getopt(argc, argv, "Ci:kv")) != -1) {
-		switch (ch) {
-		case 'C':
-			Cflag = true;
-			break;
-		case 'i':
-			if (chosen >= 0)
-				errx(-1, "-i specified more than once");
-			chosen = strtol(optarg, &tmp, 10);
-			if (*tmp != '\0')
-				errx(1, "argument to -i must be a number");
-			if (chosen < 0)
-				usage();
-			break;
-		case 'k':
-			kflag = true;
-			break;
-		case 'v':
-			vflag = true;
-			break;
-		case '?':
-		default:
-			usage();
-		}
-	}
-
-	argc -= optind;
-	argv += optind;
-	if (argc < 1 || chosen < 0)
-		usage();
-
-	memset(&sa, 0, sizeof(sa));
-	sa.sa_handler = sigchld_handler;
-	sa.sa_flags = SA_NOCLDSTOP;
-	sigfillset(&sa.sa_mask);
-
-	error = sigaction(SIGCHLD, &sa, NULL);
-	if (error != 0)
-		err(1, "sigaction");
-
+	interposer = (struct interposer *)tmp;
 	error = cosetup(COSETUP_COACCEPT);
 	if (error != 0)
 		err(1, "cosetup");
@@ -164,50 +128,18 @@ main(int argc, char **argv)
 	if (error != 0)
 		err(1, "2nd cosetup");
 
-	error = coregister(NULL, &public);
+	error = coregister(NULL, &interposer->i_public);
 	if (error != 0)
 		err(1, "coregister");
 
-	capvfetch(&capc, &capv);
-	if (chosen >= capc || capv[chosen] == NULL)
-		errx(1, "capv[%d] is NULL", chosen);
-	target = capv[chosen];
-
-	error = capvset(&capc, &capv, chosen, public);
-	if (error != 0)
-		err(1, "capvset");
-	/*
-	 * We can't explicitely pass capv into another address space,
-	 * so we need vfork(2) here, not fork(2).
-	 */
-	pid = vfork();
-	if (pid < 0)
-		err(1, "vfork");
-
-	if (pid == 0) {
-		/*
-		 * Child, will coexecvec(2) the new command.
-		 */
-		coexecvpc(getppid(), argv[0], argv, capv, capc);
-
-		/*
-		 * Shouldn't have returned.
-		 */
-		err(1, "%s", argv[0]);
-	}
-
-	/*
-	 * Parent, will loop on coaccept(2) until SIGCHLD.
-	 */
-
-	if (!Cflag) {
-		error = cap_enter();
-		if (error != 0)
-			err(1, "cap_enter");
-	}
-
 	memset(out, 0, sizeof(*out));
 	received = 0; /* Nothing to send back at this point. */
+
+	pthread_mutex_lock(&mtx);
+	pending--;
+	if (pending == 0)
+		pthread_cond_signal(&donecv);
+	pthread_mutex_unlock(&mtx);
 
 	for (;;) {
 		/*
@@ -235,20 +167,21 @@ main(int argc, char **argv)
 			err(1, "strvisx");
 
 		if (vflag) {
+			pthread_mutex_lock(&mtx);
 			error = cocachedpid(&pid, cookie);
+			pthread_mutex_unlock(&mtx);
 			if (error != 0)
-				warn("cogetpid");
-			printf("%s: cocall len %zd, op %d, received %zd, from pid %d -> pid %d%s: \"%s\"\n",
-			    getprogname(), in->len, in->op, received, pid, getpid(), kflag ? " (slow)" : "",
-			    dumpbuf);
+				warn("cocachedpid");
+			printf("%s: cocall from pid %d[%d] -> pid %d, received %zd, len %zd, op %d: \"%s\"%s\n",
+			    getprogname(), pid, interposer->i_index, getpid(), received, in->len, in->op,
+			    kflag ? " (slow)" : "", dumpbuf);
 		} else {
-			dumpbuf[56] = '.';
-			dumpbuf[57] = '.';
-			dumpbuf[58] = '.';
-			dumpbuf[59] = '\0';
+			dumpbuf[66] = '.';
+			dumpbuf[67] = '.';
+			dumpbuf[68] = '.';
+			dumpbuf[69] = '\0';
 
-			printf("len %4zd, op %2d%s -> \"%s\"\n",
-			    in->len, in->op, kflag ? " (slow)" : "", dumpbuf);
+			printf("-> %d: \"%s\"%s\n", interposer->i_index, dumpbuf, kflag ? " (slow)" : "");
 		}
 
 		if ((size_t)received != in->len) {
@@ -268,11 +201,11 @@ main(int argc, char **argv)
 		 * Forward the call.  Note the reversed directions.
 		 */
 		if (kflag)
-			received = cocall_slow(target, in, received, out, sizeof(outbuf));
+			received = cocall_slow(interposer->i_target, in, received, out, sizeof(outbuf));
 		else
-			received = cocall(target, in, received, out, sizeof(outbuf));
+			received = cocall(interposer->i_target, in, received, out, sizeof(outbuf));
 		if (received < 0) {
-			warn("%s", kflag ? "coaccept_slow" : "coaccept");
+			warn("%s", kflag ? "cocall_slow" : "cocall");
 			out->len = received = 0;
 			// XXX we should send back error response
 		}
@@ -285,28 +218,27 @@ main(int argc, char **argv)
 			err(1, "strvisx");
 
 		if (vflag) {
-			printf("%s: returning len %zd, op %d to pid %d <- pid %d%s: \"%s\"\n",
-			    getprogname(), out->len, out->op, pid, getpid(), kflag ? " (slow)" : "",
-			    dumpbuf);
+			printf("%s: returning to pid %d[%d] <- pid %d, len %zd, op %d: \"%s\"%s\n",
+			    getprogname(), pid, interposer->i_index, getpid(), out->len, out->op,
+			    dumpbuf, kflag ? " (slow)" : "");
 		} else {
-			dumpbuf[56] = '.';
-			dumpbuf[57] = '.';
-			dumpbuf[58] = '.';
-			dumpbuf[59] = '\0';
+			dumpbuf[66] = '.';
+			dumpbuf[67] = '.';
+			dumpbuf[68] = '.';
+			dumpbuf[69] = '\0';
 
-			printf("len %4zd, op %2d%s <- \"%s\"\n",
-			    out->len, out->op, kflag ? " (slow)" : "", dumpbuf);
+			printf("<- %d: \"%s\"%s\n", interposer->i_index, dumpbuf, kflag ? " (slow)" : "");
 		}
 
 		/*
 		 * If this was an answerback request, modify the answer to add our own info.
 		 */
 		if ((size_t)received >= sizeof(*in) && in->op == 0) {
-			answerback(&outbuf.answerback, target);
+			answerback(&outbuf.answerback, interposer->i_target);
 			received = out->len;
 			if (vflag) {
-				printf("%s: returning answerback to pid %d <- pid %d%s\n",
-				    getprogname(), pid, getpid(), kflag ? " (slow)" : "");
+				printf("%s: returning answerback to pid[%d] %d <- pid %d%s\n",
+				    getprogname(), pid, interposer->i_index, getpid(), kflag ? " (slow)" : "");
 			}
 		}
 
@@ -314,4 +246,139 @@ main(int argc, char **argv)
 		 * Now loop; coaccept(2) will send the response back and then wait for the next one.
 		 */
 	}
+}
+
+int
+main(int argc, char **argv)
+{
+	struct sigaction sa;
+	void * __capability *capv;
+	struct interposer *interposers;
+	pid_t pid;
+	int capc, ch, error, i;
+
+	while ((ch = getopt(argc, argv, "Ckv")) != -1) {
+		switch (ch) {
+		case 'C':
+			Cflag = true;
+			break;
+		case 'k':
+			kflag = true;
+			break;
+		case 'v':
+			vflag = true;
+			break;
+		case '?':
+		default:
+			usage();
+		}
+	}
+
+	argc -= optind;
+	argv += optind;
+	if (argc < 1)
+		usage();
+
+	capvfetch(&capc, &capv);
+
+	interposers = calloc(capc, sizeof(*interposers));
+	if (interposers == NULL)
+		err(1, "calloc");
+
+	error = pthread_mutex_init(&mtx, NULL);
+	if (error != 0)
+		err(1, "pthread_mutex_init");
+	error = pthread_cond_init(&donecv, NULL);
+	if (error != 0)
+		err(1, "pthread_cond_init");
+
+	/*
+	 * Start the threads, one for each service found in capv.
+	 *
+	 * XXX: I wonder if we should be using processes here instead.  Threads
+	 *      within a single process are bit of a security risk, and I'm not
+	 *      sure they are really more efficient here.
+	 */
+	for (i = 0; i < capc; i++) {
+		if (capv[i] == NULL)
+			continue;
+		pthread_mutex_lock(&mtx);
+		pending++;
+		pthread_mutex_unlock(&mtx);
+
+		interposers[i].i_index = i;
+		interposers[i].i_target = capv[i];
+		error = pthread_create(&interposers[i].i_pthread, NULL, interpose, (void *)&interposers[i]);
+		if (error != 0)
+			err(1, "pthread_create");
+	}
+
+	/*
+	 * Wait until the threads are ready.
+	 *
+	 * XXX: There's still a slight race there, in that we're still signalling
+	 *      we're ready before calling coaccept(2).
+	 */
+	pthread_mutex_lock(&mtx);
+	while (pending > 0)
+		pthread_cond_wait(&donecv, &mtx);
+	pthread_mutex_unlock(&mtx);
+
+	for (i = 0; i < capc; i++) {
+		if (capv[i] == NULL)
+			continue;
+		capv[i] = interposers[i].i_public;
+	}
+
+	memset(&sa, 0, sizeof(sa));
+	sa.sa_handler = sigchld_handler;
+	sa.sa_flags = SA_NOCLDSTOP;
+	sigfillset(&sa.sa_mask);
+	error = sigaction(SIGCHLD, &sa, NULL);
+	if (error != 0)
+		err(1, "sigaction");
+
+	/*
+	 * We can't explicitely pass capv into another address space,
+	 * so we need vfork(2) here, not fork(2).
+	 *
+	 * XXX: vforking a threaded process #yolo
+	 */
+	pid = vfork();
+	if (pid < 0)
+		err(1, "vfork");
+
+	if (pid == 0) {
+		/*
+		 * Child, will coexecvec(2) the new command.
+		 */
+		coexecvpc(getppid(), argv[0], argv, capv, capc);
+
+		/*
+		 * Shouldn't have returned.
+		 */
+		err(1, "%s", argv[0]);
+	}
+
+	/*
+	 * Parent.  Nothing to do until SIGCHLD.
+	 *
+	 * XXX: There's another race here: another thread could have already started
+	 *      making cocalls before we call cap_enter(2).
+	 */
+	if (!Cflag) {
+		error = cap_enter();
+		if (error != 0)
+			err(1, "cap_enter");
+	}
+
+	for (i = 0; i < capc; i++) {
+		if (capv[i] == NULL)
+			continue;
+		error = pthread_join(interposers[i].i_pthread, NULL);
+		if (error != 0)
+			err(1, "pthread_join");
+	}
+
+	return (0);
 }
