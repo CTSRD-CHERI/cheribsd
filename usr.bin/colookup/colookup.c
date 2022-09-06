0dc62b785382ba707a2cd96c38aa8049e26a015e
Edward Tomasz Napierala <trasz@FreeBSD.org>
2022-09-06 13:07:28 +0000

    colookup(1): Add -f
    This makes it possible to use SCM_CAPS instead of colookup(2) syscall.

--- usr.bin/colookup/colookup.c
+++ usr.bin/colookup/colookup.c
@@ -32,7 +32,10 @@
 __FBSDID("$FreeBSD$");
 
 #include <sys/auxv.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 #include <err.h>
+#include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -45,29 +48,104 @@ usage(void)
 
 	fprintf(stderr,
 	    "usage: colookup -c name\n"
-	    "       colookup -i index -n name [-i index -n name ...] command [args ...]\n"
+	    "       colookup -f path -i index [-f path -i index ...] command [args ...]\n"
+	    "       colookup -n name -i index [-n name -i index ...] command [args ...]\n"
 	    "       colookup -s\n");
 	exit(0);
 }
 
+static void * __capability
+receive_cap(char *filename)
+{
+	void * __capability target;
+	struct msghdr msg;
+	union {
+		struct cmsghdr	hdr;
+		unsigned char	buf[CMSG_SPACE(sizeof(target))];
+	} cmsgbuf;
+	struct cmsghdr *cmsg;
+	struct sockaddr_un sun;
+	ssize_t received;
+	int fd, error;
+
+	fd = socket(PF_UNIX, SOCK_STREAM, 0);
+	if (fd < 0)
+		err(1, "socket");
+
+	memset(&sun, 0, sizeof(sun));
+	sun.sun_family = AF_UNIX;
+	strlcpy(sun.sun_path, filename, sizeof(sun.sun_path));
+	sun.sun_len = SUN_LEN(&sun);
+
+	error = connect(fd, (struct sockaddr *)&sun, sizeof(sun));
+	if (error != 0)
+		err(1, "%s", filename);
+
+	memset(&msg, 0, sizeof(msg));
+	msg.msg_control = &cmsgbuf.buf;
+	msg.msg_controllen = sizeof(cmsgbuf.buf);
+	msg.msg_iov = NULL;
+	msg.msg_iovlen = 0;
+
+	received = recvmsg(fd, &msg, MSG_WAITALL);
+	if (received != 0) {
+		if (errno == EPROT)
+			warnx("likely not colocated with coregister(1)");
+		err(1, "recvmsg");
+	}
+	if (msg.msg_flags & MSG_TRUNC)
+		errx(1, "message truncated");
+	if (msg.msg_flags & MSG_CTRUNC)
+		errx(1, "control message truncated");
+
+	cmsg = CMSG_FIRSTHDR(&msg);
+	if (cmsg == NULL)
+		errx(1, "CMSG_FIRSTHDR returned NULL");
+	if (cmsg->cmsg_len != CMSG_LEN(sizeof(target)))
+		errx(1, "received wrong cmsg_len %d, expected %lu\n", cmsg->cmsg_len, CMSG_LEN(sizeof(target)));
+	if (cmsg->cmsg_level != SOL_SOCKET)
+		errx(1, "received wrong cmsg_level %d, expected %d\n", cmsg->cmsg_level, SOL_SOCKET);
+	if (cmsg->cmsg_type != SCM_CAPS)
+		errx(1, "received wrong cmsg_type %d, expected %d\n", cmsg->cmsg_type, SCM_CAPS);
+	memcpy(&target, CMSG_DATA(cmsg), sizeof(target));
+
+	error = close(fd);
+	if (error != 0)
+		warn("close");
+
+	return (target);
+}
+
 static void
-maybe_add(int *capcp, void * __capability **capvp, int *chosenp, char **registeredp)
+consider(int *capcp, void * __capability **capvp, int *chosenp,
+    char **namep, char **filenamep)
 {
 	void * __capability lookedup;
 	int error;
 
-	if (*chosenp < 0 || *registeredp == NULL)
+	if (*chosenp < 0)
 		return;
 
-	error = colookup(*registeredp, &lookedup);
-	if (error != 0)
-		err(1, "%s", *registeredp);
+	if (*filenamep != NULL && *namep != NULL)
+		errx(-1, "-f and -n are mutually exclusive");
+
+	if (*namep != NULL) {
+		error = colookup(*namep, &lookedup);
+		if (error != 0)
+			err(1, "%s", *namep);
+	} else if (*filenamep != NULL) {
+		lookedup = receive_cap(*filenamep);
+	} else {
+		return;
+	}
+
 	error = capvset(capcp, capvp, *chosenp, lookedup);
 	if (error != 0)
 		err(1, "capvset");
 
 	*chosenp = -1;
-	*registeredp = NULL;
+	*namep = NULL;
+	*filenamep = NULL;
 }
 
 int
@@ -77,12 +155,12 @@ main(int argc, char **argv)
 	void * __capability lookedup;
 	void * __capability code;
 	void * __capability data;
-	char *tmp = NULL, *registered = NULL;
+	char *tmp = NULL, *name = NULL, *filename = NULL;
 	int capc, ch, chosen = -1, error;
 
 	capvfetch(&capc, &capv);
 
-	while ((ch = getopt(argc, argv, "c:i:n:s")) != -1) {
+	while ((ch = getopt(argc, argv, "c:f:i:n:s")) != -1) {
 		switch (ch) {
 		case 'c':
 			error = colookup(optarg, &lookedup);
@@ -90,6 +168,12 @@ main(int argc, char **argv)
 				err(1, "%s", optarg);
 			printf("%s: %#lp\n", optarg, lookedup);
 			return (0);
+		case 'f':
+			if (filename != NULL)
+				errx(-1, "-f specified more than once");
+			filename = optarg;
+			consider(&capc, &capv, &chosen, &name, &filename);
+			break;
 		case 'i':
 			if (chosen >= 0)
 				errx(-1, "-i specified more than once");
@@ -98,13 +182,13 @@ main(int argc, char **argv)
 				errx(1, "argument to -i must be a number");
 			if (chosen < 0)
 				usage();
-			maybe_add(&capc, &capv, &chosen, &registered);
+			consider(&capc, &capv, &chosen, &name, &filename);
 			break;
 		case 'n':
-			if (registered != NULL)
+			if (name != NULL)
 				errx(-1, "-n specified more than once");
-			registered = optarg;
-			maybe_add(&capc, &capv, &chosen, &registered);
+			name = optarg;
+			consider(&capc, &capv, &chosen, &name, &filename);
 			break;
 		case 's':
 			error = _cosetup(COSETUP_COCALL, &code, &data);
@@ -125,7 +209,7 @@ main(int argc, char **argv)
 	argc -= optind;
 	argv += optind;
 
-	if (argc < 1 || chosen >= 0 || registered != NULL)
+	if (argc < 1 || chosen >= 0 || name != NULL | filename != NULL)
 		usage();
 
 	coexecvpc(getppid(), argv[0], argv, capv, capc);
