030810f37a7e0b1df16f97dd7044b1f9b4c2fc45
Edward Tomasz Napierala <trasz@FreeBSD.org>
2022-10-16 09:44:13 +0000

    stevie(1): Simplify

--- usr.bin/stevie/stevie.c
+++ usr.bin/stevie/stevie.c
@@ -28,185 +28,108 @@
  * SUCH DAMAGE.
  */
 
-#include <sys/cdefs.h>
-__FBSDID("$FreeBSD$");
+/*
+ * This is a trivial example of using coaccept(3) and cocall(3) between
+ * two threads.  With processes it would work pretty much the same,
+ * except it would require a mechanism to transfer the target capability
+ * from service to the caller.
+ */
 
-#include <machine/param.h>
-#include <machine/sysarch.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <ctype.h>
 #include <err.h>
-#include <errno.h>
 #include <pthread.h>
-#include <pthread_np.h>
-#include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
-static pthread_t	service_thread;
-static char		*coname;
-
-#if 0
-#define SHARED_PAGE	0x7ffffff000
-
-static void
-show_8_hex(char *p)
-{
-	int i;
-
-	for (i = 0; i < 8; i++)
-		fprintf(stderr, " %02x", (uint8_t)*(p + i));
-}
-
-static void
-show_8_char(char *p)
-{
-	int i;
-
-	for (i = 0; i < 8; i++) {
-		if (isprint(p[i]))
-			fprintf(stderr, "%c", p[i]);
-		else
-			fprintf(stderr, ".");
-	}
-}
-
-static void
-show_16(char *p, off_t off)
-{
-	fprintf(stderr, "%08lx ", off);
-	show_8_hex(p);
-	fprintf(stderr, " ");
-	show_8_hex(p + 8);
-	fprintf(stderr, "  |");
-	show_8_char(p);
-	show_8_char(p + 8);
-	fprintf(stderr, "|\n");
-}
-
-static void
-show_chunk(void *ptr, off_t off, int len)
-{
-	char *p;
-	int i, j;
-	bool just_zeroes, just_zeroes_last_time;
-
-	p = ptr;
-	just_zeroes_last_time = false;
-	for (i = 0; i < len; i += 16) {
-		just_zeroes = true;
-		for (j = 0; j < 16; j++) {
-			if (p[i + j] != 0) {
-				just_zeroes = false;
-				break;
-			}
-		}
-		if (just_zeroes) {
-			if (!just_zeroes_last_time)
-				fprintf(stderr, "*\n");
-		} else {
-			show_16(p + i, off + i);
-		}
-		just_zeroes_last_time = just_zeroes;
-	}
-}
-#endif
-
-static void
-call(void)
-{
-	void * __capability lookedup;
-	void * __capability buf[8];
-	ssize_t received;
-	int error, i;
-
-	fprintf(stderr, "%s: setting up...\n", __func__);
-	error = cosetup(COSETUP_COCALL);
-	if (error != 0)
-		err(1, "cosetup");
-
-	fprintf(stderr, "%s: colookingup %s...\n", __func__, coname);
-	error = colookup(coname, &lookedup);
-	if (error != 0)
-		err(1, "colookup");
-
-	memset(buf, 0, sizeof(buf));
-	buf[0] = (void *)42;
-
-	for (i = 0; i < 2; i++) {
-		fprintf(stderr, "%s: calling %p, buf %p, we are thread %d...\n",
-		    __func__, (__cheri_fromcap void *)lookedup, buf, pthread_getthreadid_np());
-		received = cocall(lookedup, buf, sizeof(buf), buf, sizeof(buf));
-		if (received < 0)
-			fprintf(stderr, "%s: cocall: %s\n", __func__, strerror(errno));
-		fprintf(stderr, "%s: done, we are thread %d, buf %p contains %d\n",
-		    __func__, pthread_getthreadid_np(), buf, (int)(intcap_t)buf[0]);
-		buf[0] = (void *)((intcap_t)buf[0] + 1);
-	}
-}
+static pthread_t		service_thread;
+static void * __capability	target;
 
 static void *
-service_proc(void *dummy __unused)
+service_function(void *dummy __unused)
 {
-	void * __capability cookie;
-	void * __capability buf[8];
-	uint64_t *halfcookie;
+	/*
+	 * Can't use int here, because buffers need to be capability-aligned.
+	 * This will happen naturally when using malloc(3), but for now lets keep
+	 * it simple and use a capability-sized integer type instead.
+	 */
+	intcap_t buf = 0;
 	ssize_t received;
-	pid_t pid;
 	int error;
 
-	fprintf(stderr, "%s: setting up...\n", __func__);
+	/*
+	 * Every thread needs to do this once before calling coaccept(2).
+	 */
 	error = cosetup(COSETUP_COACCEPT);
 	if (error != 0)
 		err(1, "cosetup");
 
-	fprintf(stderr, "%s: coregistering as %s...\n", __func__, coname);
-	error = coregister(coname, NULL);
+	/*
+	 * Ask kernel for the target capability to call into this thread;
+	 * the `target` is a global variable, to be used by the calling thread.
+	 */
+	error = coregister(NULL, &target);
 	if (error != 0)
 		err(1, "coregister");
 
-	fprintf(stderr, "%s: buf %p, we are thread %d, accepting...\n",
-	    __func__, buf, pthread_getthreadid_np());
-
-	memset(buf, 0, sizeof(buf));
-
+	/*
+	 * Now loop until the process exits.
+	 */
 	for (;;) {
-		received = coaccept(&cookie, buf, sizeof(buf), buf, sizeof(buf));
+		/*
+		 * Send back the response, if any, then wait for next caller.
+		 */
+		received = coaccept(NULL, &buf, sizeof(buf), &buf, sizeof(buf));
 		if (received < 0)
-			fprintf(stderr, "%s: coaccept: %s\n", __func__, strerror(errno));
-		error = cogetpid(&pid);
-		if (error != 0)
-			warn("cogetpid");
-		halfcookie = (uint64_t *)&cookie;
-		fprintf(stderr, "%s: accepted, cookie %#lx%lx, pid %d, we are thread %d, buf %p contains %d, looping...\n",
-		    __func__, halfcookie[0], halfcookie[1], pid, pthread_getthreadid_np(), buf, (int)(intcap_t)buf[0]);
-		buf[0] = (void *)((intcap_t)buf[0] + 1);
+			err(1, "cocall");
+
+		/*
+		 * Got a call, bump the counter and loop.
+		 */
+		printf("accepted, counter is %d\n", (int)buf);
+		buf++;
 	}
 }
 
 int
 main(int argc __unused, char **argv __unused)
 {
-	int error;
+	intcap_t buf = 0;
+	ssize_t received;
+	int error, i;
 
-	coname = mktemp(strdup("stevie.XXXXXX"));
-	if (coname == NULL)
-		err(1, "mktemp");
-
-#if 0
-	fprintf(stderr, "memory at %p:\n", (void *)SHARED_PAGE);
-	show_chunk((void *)SHARED_PAGE, SHARED_PAGE, PAGE_SIZE);
-#endif
-
-	error = pthread_create(&service_thread, NULL, service_proc, NULL);
+	/*
+	 * Create the thread to wait on coaccept(2).
+	 */
+	error = pthread_create(&service_thread, NULL, service_function, NULL);
 	if (error != 0)
 		err(1, "pthread_create");
 
-	sleep(1);
-	call();
+	/*
+	 * Give the service thread a moment to start coaccepting before we proceed;
+	 * otherwise cocall(3) might fail with EAGAIN.
+	 */
+	usleep(1000);
 
+	/*
+	 * Every thread needs to do this once before calling cocall(2).
+	 * Call it twice if it needs both coaccept(2) and cocall(2).
+	 */
+	error = cosetup(COSETUP_COCALL);
+	if (error != 0)
+		err(1, "cosetup");
+
+	/*
+	 * Do the thing a couple of times.
+	 */
+	for (i = 3; i > 0; i--) {
+		printf("calling %lp...\n", target);
+		received = cocall(target, &buf, sizeof(buf), &buf, sizeof(buf));
+		if (received < 0)
+			err(1, "cocall");
+		printf("returned, counter is %d\n", (int)buf);
+	}
+
+	/*
+	 * Exit.
+	 */
 	return (0);
 }
