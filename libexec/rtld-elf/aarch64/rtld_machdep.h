1897d4458817eb2be1ae70bbc982bf32a136eea4
Dapeng Gao <dapeng@dpgao.cn>
2022-11-09 14:24:12 +0000

    c18n: Avoid creating trampolines for [rv]fork

--- libexec/rtld-elf/aarch64/rtld_machdep.h
+++ libexec/rtld-elf/aarch64/rtld_machdep.h
@@ -150,13 +150,13 @@ make_data_cap(const Elf_Sym *def, const struct Struct_Obj_Entry *defobj)
 
 /* TODO: Per-function captable/PLT/FNDESC support */
 #ifdef RTLD_SANDBOX
-#define make_rtld_function_pointer(target_func)	(tramp_pgs_append((target_func), NULL))
+#define make_rtld_function_pointer(target_func)	(tramp_pgs_append((target_func), NULL, NULL))
 
 #define call_init_array_pointer(obj, target)				\
-	(((InitArrFunc)tramp_pgs_append((const void *)(target).value, obj))(main_argc, main_argv, environ))
+	(((InitArrFunc)tramp_pgs_append((void *)(target).value, obj, NULL))(main_argc, main_argv, environ))
 
 #define call_fini_array_pointer(obj, target)				\
-	(((InitFunc)tramp_pgs_append((const void *)(target).value, obj))())
+	(((InitFunc)tramp_pgs_append((void *)(target).value, obj, NULL))())
 #else
 #define call_init_array_pointer(obj, target)				\
 	(((InitArrFunc)(target).value)(main_argc, main_argv, environ))
@@ -211,7 +211,7 @@ extern void *__tls_get_addr(tls_index *ti);
 #define md_abi_variant_hook(x)
 
 #if defined(__CHERI_PURE_CAPABILITY__) && defined(RTLD_SANDBOX)
-void *tramp_pgs_append(const void *target, const Obj_Entry *dst);
+void *tramp_pgs_append(void *, const Obj_Entry *, const Elf_Sym *);
 #endif
 
 #ifdef __CHERI_PURE_CAPABILITY__
