6b6854334ae407cd4bd0f88bd9c62d38975f4229
John Baldwin <jhb@FreeBSD.org>
2023-01-04 16:32:54 -0800

    git subrepo pull contrib/subrepo-cheri-libc++
    subrepo:
      subdir:   "contrib/subrepo-cheri-libc++"
      merged:   "dfb276fd548c"
    upstream:
      origin:   "https://github.com/CTSRD-CHERI/libcxx.git"
      branch:   "monorepo-mirror"
      commit:   "dfb276fd548c"
    git-subrepo:
      version:  "0.4.3"
      origin:   "???"
      commit:   "???"

--- contrib/subrepo-cheri-libc++/test/support/poisoned_hash_helper.h
+++ contrib/subrepo-cheri-libc++/test/support/poisoned_hash_helper.h
@@ -57,7 +57,9 @@ using LibraryHashTypes = TypeList<
       char,
       signed char,
       unsigned char,
+#ifndef TEST_HAS_NO_WIDE_CHARACTERS
       wchar_t,
+#endif
 #ifndef _LIBCPP_HAS_NO_UNICODE_CHARS
       char16_t,
       char32_t,
@@ -70,7 +72,7 @@ using LibraryHashTypes = TypeList<
       unsigned long,
       long long,
       unsigned long long,
-#ifndef _LIBCPP_HAS_NO_INT128
+#ifndef TEST_HAS_NO_INT128
       __int128_t,
       __uint128_t,
 #endif
