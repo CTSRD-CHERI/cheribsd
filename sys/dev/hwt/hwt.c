f7a3c7d125745c6bb4f2984169b2ae0c29f57d93
Ruslan Bukin <br@bsdpad.com>
2023-01-24 17:05:53 +0000

    HWT stubs

---
+++ sys/dev/hwt/hwt.c
@@ -0,0 +1,157 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2023 Ruslan Bukin <br@bsdpad.com>
+ *
+ * This work was supported by Innovate UK project 105694, "Digital Security
+ * by Design (DSbD) Technology Platform Prototype".
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/* Hardware Trace (HWT) module. */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/ioccom.h>
+#include <sys/conf.h>
+#include <sys/kernel.h>
+#include <sys/malloc.h>
+#include <sys/module.h>
+#include <sys/mutex.h>
+
+#include <dev/hwt/hwtvar.h>
+
+#define	HWT_DEBUG
+//#undef	HWT_DEBUG
+
+#ifdef	HWT_DEBUG
+#define	dprintf(fmt, ...)	printf(fmt, ##__VA_ARGS__)
+#else
+#define	dprintf(fmt, ...)
+#endif
+
+struct hwt_softc hwt_sc;
+
+
+static int
+hwt_ioctl(struct cdev *dev, u_long cmd, caddr_t addr, int flags,
+    struct thread *td)
+{
+	//struct hwt_softc *sc;
+	int error;
+	int len;
+
+	error = 0;
+
+	//sc = dev->si_drv1;
+
+	len = IOCPARM_LEN(cmd);
+
+	dprintf("%s: cmd %lx, addr %lx, len %d\n", __func__, cmd,
+	    (uint64_t)addr, len);
+
+	switch (cmd) { };
+
+	return (error);
+}
+
+static struct cdevsw hwt_cdevsw = {
+	.d_version	= D_VERSION,
+	.d_name		= "hwt",
+	.d_ioctl	= hwt_ioctl
+};
+
+static int
+hwt_load(void)
+{
+	struct make_dev_args args;
+	struct hwt_softc *sc;
+	int error;
+
+	sc = &hwt_sc;
+
+	dprintf("%s\n", __func__);
+
+	mtx_init(&sc->mtx, "HWT driver", NULL, MTX_DEF);
+
+	make_dev_args_init(&args);
+	args.mda_devsw = &hwt_cdevsw;
+	args.mda_flags = MAKEDEV_CHECKNAME | MAKEDEV_WAITOK;
+	args.mda_uid = UID_ROOT;
+	args.mda_gid = GID_WHEEL;
+	args.mda_mode = 0660;
+	args.mda_si_drv1 = sc;
+
+	error = make_dev_s(&args, &sc->hwt_cdev, "hwt");
+	if (error != 0)
+		return (error);
+
+	return (0);
+}
+
+static int
+hwt_unload(void)
+{
+	struct hwt_softc *sc;
+
+	dprintf("%s\n", __func__);
+
+	sc = &hwt_sc;
+
+	destroy_dev(sc->hwt_cdev);
+	mtx_destroy(&sc->mtx);
+
+	return (0);
+}
+
+static int
+hwt_modevent(module_t mod, int type, void *data)
+{
+	int error;
+
+	switch (type) {
+	case MOD_LOAD:
+		error = hwt_load();
+		break;
+	case MOD_UNLOAD:
+		error = hwt_unload();
+		break;
+	default:
+		error = 0;
+		break;
+	}
+
+	return (error);
+}
+
+static moduledata_t hwt_mod = {
+	"hwt",
+	hwt_modevent,
+	NULL
+};
+
+DECLARE_MODULE(hwt, hwt_mod, SI_SUB_LAST, SI_ORDER_ANY);
+MODULE_VERSION(hwt, 1);
