904994a6a7b100aefebb13299bea886e503dc9b5
Jessica Clarke <jrtc27@FreeBSD.org>
2023-05-31 00:20:36 +0100

    pmc: Rework PROCEXEC event to support PIEs
    Currently the PROCEXEC event only reports a single address, entryaddr,
    which is the entry point of the interpreter in the typical dynamic case,
    and used solely to calculate the base address of the interpreter. For
    PDEs this is fine, since the base address is known from the program
    headers, but for PIEs the base address varies at run time based on where
    the kernel chooses to load it, and so pmcstat has no way of knowing the
    real address ranges for the executable. This was less of an issue in the
    past since PIEs were rare, but now they're on by default on 64-bit
    architectures it's more of a problem.
    
    To solve this, pass through what was picked for et_dyn_addr by the
    kernel, and use that as the offset for the executable's start address
    just as is done for everything in the kernel. Since we're changing this
    interface, sanitise the way we determine the interpreter's base address
    by passing it through directly rather than indirectly via the entry
    point and having to subtract off whatever the ELF header's e_entry is
    (and anything that wants the entry point in future can still add that
    back on as needed; this merely changes the interface to directly provide
    the underlying variables involved).
    
    This will be followed up by a bump to the pmc major version.
    
    Reviewed by:	jhb
    Differential Revision:	https://reviews.freebsd.org/D39595
    
    (cherry picked from commit 94426d21bf62f2b36dc9b556ab27c401a412a026)
    (cherry picked from commit 0a1ab8d43ca5c80c5aded64fb55918be11b930f0)

--- usr.sbin/pmcstat/pmcstat_log.c
+++ usr.sbin/pmcstat/pmcstat_log.c
@@ -470,10 +470,11 @@ pmcstat_print_log(void)
 			    ev.pl_u.pl_pc.pl_pcomm);
 			break;
 		case PMCLOG_TYPE_PROCEXEC:
-			PMCSTAT_PRINT_ENTRY("exec","0x%x %d %p \"%s\"",
+			PMCSTAT_PRINT_ENTRY("exec","0x%x %d %p %p \"%s\"",
 			    ev.pl_u.pl_x.pl_pmcid,
 			    ev.pl_u.pl_x.pl_pid,
-			    (void *)(intptr_t) ev.pl_u.pl_x.pl_entryaddr,
+			    (void *)(uintptr_t)ev.pl_u.pl_x.pl_baseaddr,
+			    (void *)(uintptr_t)ev.pl_u.pl_x.pl_dynaddr,
 			    ev.pl_u.pl_x.pl_pathname);
 			break;
 		case PMCLOG_TYPE_PROCEXIT:
