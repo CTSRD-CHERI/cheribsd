ed2e114543ea25ff0f241f9d3e4462dbf5eb7da8
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2023-06-22 14:52:13 +0100

    Implement a test for a compartment entry
    The sysctl security.compartment.test.entry.call can be used to call
    compartment_function() from the compartment function kernel module.

--- sys/modules/compartment_test/compartment_test.c
+++ sys/modules/compartment_test/compartment_test.c
@@ -36,15 +36,20 @@
 #include <sys/module.h>
 #include <sys/sysctl.h>
 
+#include <modules/compartment_function/compartment_function.h>
+
 SYSCTL_NODE(_security_compartment, OID_AUTO, test, CTLFLAG_RD, 0,
     "Compartment test cases");
-SYSCTL_NODE(_security_compartment_test, OID_AUTO, compressor, CTLFLAG_RD, 0,
-    "Compartment test cases for compressor");
 
-#define	COMPRESSOR_FUNCTION_INIT	0
-#define	COMPRESSOR_FUNCTION_WRITE	1
-#define	COMPRESSOR_FUNCTION_FLUSH	2
-#define	COMPRESSOR_FUNCTION_FINI	3
+static uintptr_t compartment_entry_funptr;
+
+#define	SYSCTL_ENTRY_INIT	0
+#define	SYSCTL_ENTRY_CALL	1
+#define	SYSCTL_ENTRY_FINI	2
+#define	SYSCTL_COMPRESSOR_INIT	3
+#define	SYSCTL_COMPRESSOR_WRITE	4
+#define	SYSCTL_COMPRESSOR_FLUSH	5
+#define	SYSCTL_COMPRESSOR_FINI	6
 
 static uint8_t compressor_data[PAGE_SIZE];
 static struct compressor *compressor_stream;
@@ -58,7 +63,7 @@ compartment_test_compressor_cb(void *base __unused, size_t length __unused,
 }
 
 static int
-compartment_test_compressor_function(int function, SYSCTL_HANDLER_ARGS)
+compartment_test_sysctl(int function, SYSCTL_HANDLER_ARGS)
 {
 	int error, ii;
 	unsigned int value;
@@ -72,7 +77,25 @@ compartment_test_compressor_function(int function, SYSCTL_HANDLER_ARGS)
 	}
 
 	switch (function) {
-	case COMPRESSOR_FUNCTION_INIT:
+	case SYSCTL_ENTRY_INIT:
+		if (compartment_entry_funptr != (uintptr_t)NULL)
+			return (EINVAL);
+		compartment_entry_funptr =
+		    (uintptr_t)compartment_entry((uintptr_t)compartment_function);
+		break;
+	case SYSCTL_ENTRY_CALL:
+		if (compartment_entry_funptr == (uintptr_t)NULL)
+			return (EINVAL);
+		for (ii = 0; ii < value; ii++)
+			((void (*)(void))compartment_entry_funptr)();
+		break;
+	case SYSCTL_ENTRY_FINI:
+		if (compartment_entry_funptr == (uintptr_t)NULL)
+			return (EINVAL);
+		compartment_trampoline_destroy(compartment_entry_funptr);
+		compartment_entry_funptr = (uintptr_t)NULL;
+		break;
+	case SYSCTL_COMPRESSOR_INIT:
 		if (compressor_stream != NULL)
 			return (EINVAL);
 		compressor_stream = compressor_init(
@@ -82,7 +105,7 @@ compartment_test_compressor_function(int function, SYSCTL_HANDLER_ARGS)
 			error = ENOMEM;
 		}
 		break;
-	case COMPRESSOR_FUNCTION_WRITE:
+	case SYSCTL_COMPRESSOR_WRITE:
 		if (compressor_stream == NULL)
 			return (EINVAL);
 		for (ii = 0; ii < value; !error && ii++) {
@@ -90,12 +113,12 @@ compartment_test_compressor_function(int function, SYSCTL_HANDLER_ARGS)
 			    &compressor_data, sizeof(compressor_data));
 		}
 		break;
-	case COMPRESSOR_FUNCTION_FLUSH:
+	case SYSCTL_COMPRESSOR_FLUSH:
 		if (compressor_stream == NULL)
 			return (EINVAL);
 		error = compressor_flush(compressor_stream);
 		break;
-	case COMPRESSOR_FUNCTION_FINI:
+	case SYSCTL_COMPRESSOR_FINI:
 		if (compressor_stream == NULL)
 			return (EINVAL);
 		compressor_fini(compressor_stream);
@@ -112,10 +135,34 @@ out:
 }
 
 static int
+compartment_test_entry_init(SYSCTL_HANDLER_ARGS)
+{
+
+	return (compartment_test_sysctl(SYSCTL_ENTRY_INIT,
+	    oidp, arg1, arg2, req));
+}
+
+static int
+compartment_test_entry_call(SYSCTL_HANDLER_ARGS)
+{
+
+	return (compartment_test_sysctl(SYSCTL_ENTRY_CALL,
+	    oidp, arg1, arg2, req));
+}
+
+static int
+compartment_test_entry_fini(SYSCTL_HANDLER_ARGS)
+{
+
+	return (compartment_test_sysctl(SYSCTL_ENTRY_FINI,
+	    oidp, arg1, arg2, req));
+}
+
+static int
 compartment_test_compressor_init(SYSCTL_HANDLER_ARGS)
 {
 
-	return (compartment_test_compressor_function(COMPRESSOR_FUNCTION_INIT,
+	return (compartment_test_sysctl(SYSCTL_COMPRESSOR_INIT,
 	    oidp, arg1, arg2, req));
 }
 
@@ -123,7 +170,7 @@ static int
 compartment_test_compressor_write(SYSCTL_HANDLER_ARGS)
 {
 
-	return (compartment_test_compressor_function(COMPRESSOR_FUNCTION_WRITE,
+	return (compartment_test_sysctl(SYSCTL_COMPRESSOR_WRITE,
 	    oidp, arg1, arg2, req));
 }
 
@@ -131,7 +178,7 @@ static int
 compartment_test_compressor_flush(SYSCTL_HANDLER_ARGS)
 {
 
-	return (compartment_test_compressor_function(COMPRESSOR_FUNCTION_FLUSH,
+	return (compartment_test_sysctl(SYSCTL_COMPRESSOR_FLUSH,
 	    oidp, arg1, arg2, req));
 }
 
@@ -139,10 +186,24 @@ static int
 compartment_test_compressor_fini(SYSCTL_HANDLER_ARGS)
 {
 
-	return (compartment_test_compressor_function(COMPRESSOR_FUNCTION_FINI,
+	return (compartment_test_sysctl(SYSCTL_COMPRESSOR_FINI,
 	    oidp, arg1, arg2, req));
 }
 
+SYSCTL_NODE(_security_compartment_test, OID_AUTO, entry, CTLFLAG_RD, 0,
+    "Compartment test cases for entries");
+SYSCTL_PROC(_security_compartment_test_entry, OID_AUTO, init,
+    CTLTYPE_UINT | CTLFLAG_RWTUN, 0, 0, compartment_test_entry_init, "IU",
+    "Initialize a compartment entry");
+SYSCTL_PROC(_security_compartment_test_entry, OID_AUTO, call,
+    CTLTYPE_UINT | CTLFLAG_RWTUN, 0, 0, compartment_test_entry_call, "IU",
+    "Call a compartment entry");
+SYSCTL_PROC(_security_compartment_test_entry, OID_AUTO, fini,
+    CTLTYPE_UINT | CTLFLAG_RWTUN, 0, 0, compartment_test_entry_fini, "IU",
+    "Finalize a compartment entry");
+
+SYSCTL_NODE(_security_compartment_test, OID_AUTO, compressor, CTLFLAG_RD, 0,
+    "Compartment test cases for compressor");
 SYSCTL_PROC(_security_compartment_test_compressor, OID_AUTO, init,
     CTLTYPE_UINT | CTLFLAG_RWTUN, 0, 0, compartment_test_compressor_init, "IU",
     "Call a compartment entry for a compressor init function");
@@ -177,5 +238,6 @@ static moduledata_t compartment_test_mod = {
 };
 
 MODULE_VERSION(compartment_test, 1);
+MODULE_DEPEND(compartment_test, compartment_function, 1, 1, 1);
 DECLARE_MODULE(compartment_test, compartment_test_mod, SI_SUB_PSEUDO,
     SI_ORDER_ANY);
