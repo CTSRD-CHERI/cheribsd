cb783c26e49df3ce7c98e077e0d6b408a69af1e5
Jessica Clarke <jrtc27@FreeBSD.org>
2023-06-28 00:10:49 +0100

    Generalise libcompat to be a list rather than a single option
    Whilst the kernel can support any number of COMPAT_FOO, world can only
    build a single libfoo. Upstream this isn't such an issue, since the only
    option is lib32 anyway, but downstreams, such as CheriBSD, may wish to
    support multiple at the same time. Thus, adjust the top-level Makefiles
    to turn _LIBCOMPAT into a _LIBCOMPATS list that gets iterated over, and
    adjust bsd.compat.mk to support this use-case.
    
    For the normal NEED_COMPAT/WANT_COMPAT case, LIBCOMPATFOO remain set and
    refer to the requested compat's, preserving the current interface. For
    the top-level Makefiles those variables are no longer set (since there
    is no longer "the" compat) and only the per-compat ones are available.
    
    Reviewed by:	brooks, jhb, imp
    Differential Revision:	https://reviews.freebsd.org/D40571
    
    (cherry picked from commit 91d7edd549f52d09dbb12ecf5f0cf2298287f1c8)

--- share/mk/bsd.compat.mk
+++ share/mk/bsd.compat.mk
@@ -8,9 +8,13 @@ __<${_this:T}>__:
 
 .include <src.opts.mk>
 
-.if defined(_LIBCOMPAT)
+_ALL_LIBCOMPATS:=	32 64 64C
+
+.if defined(_LIBCOMPATS)
 COMPAT_ARCH?=	${TARGET_ARCH}
-COMPAT_CPUTYPE?= ${CPUTYPE_${_LIBCOMPAT}}
+.for _LIBCOMPAT in ${_ALL_LIBCOMPATS}
+LIB${_LIBCOMPAT}CPUTYPE?=	${CPUTYPE_${_LIBCOMPAT}}
+.endfor
 .if (defined(WANT_COMPILER_TYPE) && ${WANT_COMPILER_TYPE} == gcc) || \
     (defined(X_COMPILER_TYPE) && ${X_COMPILER_TYPE} == gcc)
 COMPAT_COMPILER_TYPE=	gcc
@@ -19,7 +23,9 @@ COMPAT_COMPILER_TYPE=	clang
 .endif
 .else
 COMPAT_ARCH=	${MACHINE_ARCH}
-COMPAT_CPUTYPE=	${CPUTYPE}
+.for _LIBCOMPAT in ${_ALL_LIBCOMPATS}
+LIB${_LIBCOMPAT}CPUTYPE=	${CPUTYPE}
+.endfor
 .include <bsd.compiler.mk>
 COMPAT_COMPILER_TYPE=${COMPILER_TYPE}
 .endif
@@ -28,11 +34,11 @@ COMPAT_COMPILER_TYPE=${COMPILER_TYPE}
 # 32 bit world
 .if ${MK_LIB32} != "no"
 .if ${COMPAT_ARCH} == "amd64"
-HAS_COMPAT=32
-.if empty(COMPAT_CPUTYPE)
+HAS_COMPAT+=	32
+.if empty(LIB32CPUTYPE)
 LIB32CPUFLAGS=	-march=i686 -mmmx -msse -msse2
 .else
-LIB32CPUFLAGS=	-march=${COMPAT_CPUTYPE}
+LIB32CPUFLAGS=	-march=${LIB32CPUTYPE}
 .endif
 .if ${COMPAT_COMPILER_TYPE} == gcc
 .else
@@ -47,11 +53,11 @@ LIB32WMAKEFLAGS=	\
 		LD="${XLD} -m elf_i386_fbsd"
 
 .elif ${COMPAT_ARCH} == "powerpc64"
-HAS_COMPAT=32
-.if empty(COMPAT_CPUTYPE)
+HAS_COMPAT+=	32
+.if empty(LIB32CPUTYPE)
 LIB32CPUFLAGS=	-mcpu=powerpc
 .else
-LIB32CPUFLAGS=	-mcpu=${COMPAT_CPUTYPE}
+LIB32CPUFLAGS=	-mcpu=${LIB32CPUTYPE}
 .endif
 
 .if ${COMPAT_COMPILER_TYPE} == "gcc"
@@ -84,7 +90,7 @@ LIB32_MACHINE_ABI+=	time64
 # 64 bit world
 .if ${MK_LIB64} != "no"
 .if ${COMPAT_ARCH:Maarch64*c*}
-HAS_COMPAT=64
+HAS_COMPAT+=	64
 LIB64_MACHINE=	arm64
 LIB64_MACHINE_ARCH=aarch64
 LIB64WMAKEENV=	MACHINE_CPU="arm64 cheri"
@@ -95,7 +101,7 @@ LIB64CPUFLAGS+=	-march=morello -mabi=aapcs
 .endif
 
 .if ${COMPAT_ARCH:Mriscv*c*}
-HAS_COMPAT=64
+HAS_COMPAT+=	64
 LIB64_RISCV_ABI=	lp64
 .if !${COMPAT_ARCH:Mriscv*sf}
 LIB64_RISCV_ABI:=	${LIB64_RISCV_ABI}d
@@ -121,13 +127,13 @@ LIB64_MACHINE_ABI=	${MACHINE_ABI:Npurecap:Nptr*} ptr64
 # CHERI world
 .if ${MK_LIB64C} != "no"
 .if ${COMPAT_ARCH} == "aarch64"
-HAS_COMPAT+=64C
+HAS_COMPAT+=	64C
 LIB64C_MACHINE=	arm64
 LIB64C_MACHINE_ARCH=	aarch64c
 LIB64CCPUFLAGS=	-target aarch64-unknown-freebsd13.0
 LIB64CCPUFLAGS+=	-march=morello -mabi=purecap
 .elif ${COMPAT_ARCH:Mriscv64*} && !${COMPAT_ARCH:Mriscv64*c*}
-HAS_COMPAT+=64C
+HAS_COMPAT+=	64C
 LIB64C_MACHINE=	riscv
 LIB64C_MACHINE_ARCH=	${COMPAT_ARCH}c
 LIB64CWMAKEFLAGS=	CPUTYPE=cheri
@@ -202,56 +208,63 @@ WANT_COMPAT:=	${NEED_COMPAT}
 
 .if defined(HAS_COMPAT) && defined(WANT_COMPAT)
 .if ${WANT_COMPAT} == "any"
-_LIBCOMPAT:=	${HAS_COMPAT:[1]}
+USE_COMPAT:=	${HAS_COMPAT:[1]}
 .elif !${HAS_COMPAT:M${WANT_COMPAT}}
 .warning WANT_COMPAT (${WANT_COMPAT}) defined, but not in HAS_COMPAT (${HAS_COMPAT})
 .undef WANT_COMPAT
 .else
-_LIBCOMPAT:=	${WANT_COMPAT}
+USE_COMPAT:=	${WANT_COMPAT}
 .endif
+
+_LIBCOMPATS=	${USE_COMPAT}
 .else # defined(HAS_COMPAT) && defined(WANT_COMPAT)
 .undef WANT_COMPAT
 .endif
 
+libcompats=	${_LIBCOMPATS:tl}
+
 # -------------------------------------------------------------------
 # Generic code for each type.
 # Set defaults based on type.
-libcompat=	${_LIBCOMPAT:tl}
+.for _LIBCOMPAT _libcompat in ${_LIBCOMPATS:@v@${v} ${v:tl}@}
+WORLDTMP?=		${SYSROOT}
+
+# Shared flags
+LIB${_LIBCOMPAT}_OBJTOP?=	${OBJTOP}/obj-lib${_libcompat}
+
+LIB${_LIBCOMPAT}CFLAGS+=	${LIB${_LIBCOMPAT}CPUFLAGS} \
+			--sysroot=${WORLDTMP} \
+			${BFLAGS}
+
+LIB${_LIBCOMPAT}LDFLAGS+=	-L${WORLDTMP}/usr/lib${_libcompat}
+
+LIB${_LIBCOMPAT}WMAKEENV+=	MACHINE=${LIB${_LIBCOMPAT}_MACHINE}
+LIB${_LIBCOMPAT}WMAKEENV+=	MACHINE_ARCH=${LIB${_LIBCOMPAT}_MACHINE_ARCH}
+# Note: TARGET and TARGET_ARCH must be set on the command line to override
+# previous assignments
+LIB${_LIBCOMPAT}WMAKEFLAGS+=	TARGET=${LIB${_LIBCOMPAT}_MACHINE}
+LIB${_LIBCOMPAT}WMAKEFLAGS+=	TARGET_ARCH=${LIB${_LIBCOMPAT}_MACHINE_ARCH}
+# Forward the cross linker and binutils
+.for BINUTIL in ${XBINUTILS}
+LIB${_LIBCOMPAT}WMAKEENV+=	${BINUTIL}="${X${BINUTIL}}"
+.endfor
+
+# -B is needed to find /usr/lib32/crti.o for gcc.
+LIB${_LIBCOMPAT}CFLAGS+=	-B${WORLDTMP}/usr/lib${_libcompat}
+.endfor
+
+.if defined(USE_COMPAT)
+libcompat=	${USE_COMPAT:tl}
+
 _LIBCOMPAT_MAKEVARS=	_OBJTOP TMP CPUFLAGS CFLAGS CXXFLAGS LDFLAGS \
 			_MACHINE _MACHINE_ARCH _MACHINE_ABI \
 			WMAKEENV WMAKEFLAGS WMAKE WORLDTMP
 .for _var in ${_LIBCOMPAT_MAKEVARS}
-.if !empty(LIB${_LIBCOMPAT}${_var})
-LIBCOMPAT${_var}?=	${LIB${_LIBCOMPAT}${_var}}
+.if !empty(LIB${USE_COMPAT}${_var})
+LIBCOMPAT${_var}?=	${LIB${USE_COMPAT}${_var}}
 .endif
 .endfor
 
-WORLDTMP?=		${SYSROOT}
-
-# Shared flags
-LIBCOMPAT_OBJTOP?=	${OBJTOP}/obj-lib${libcompat}
-
-LIBCOMPATCFLAGS+=	${LIBCOMPATCPUFLAGS} \
-			--sysroot=${WORLDTMP} \
-			${BFLAGS}
-
-LIBCOMPATLDFLAGS+=	-L${WORLDTMP}/usr/lib${libcompat}
-
-LIBCOMPATWMAKEENV+=	MACHINE=${LIBCOMPAT_MACHINE}
-LIBCOMPATWMAKEENV+=	MACHINE_ARCH=${LIBCOMPAT_MACHINE_ARCH}
-# Note: TARGET and TARGET_ARCH must be set on the command line to override
-# previous assignments
-LIBCOMPATWMAKEFLAGS+=	TARGET=${LIBCOMPAT_MACHINE}
-LIBCOMPATWMAKEFLAGS+=	TARGET_ARCH=${LIBCOMPAT_MACHINE_ARCH}
-# Forward the cross linker and binutils
-.for BINUTIL in ${XBINUTILS}
-LIBCOMPATWMAKEENV+=	${BINUTIL}="${X${BINUTIL}}"
-.endfor
-
-# -B is needed to find /usr/lib32/crti.o for gcc.
-LIBCOMPATCFLAGS+=	-B${WORLDTMP}/usr/lib${libcompat}
-
-.if defined(WANT_COMPAT)
 LIBDIR_BASE:=	/usr/lib${libcompat}
 LIBDATADIR:=	/usr/lib${libcompat}
 _LIB_OBJTOP=	${LIBCOMPAT_OBJTOP}
