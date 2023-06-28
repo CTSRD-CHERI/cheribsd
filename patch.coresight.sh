d6876164e27edf9aa529c79a7ccc86826103e92b
Ruslan Bukin <br@bsdpad.com>
2023-06-28 11:36:15 +0100

    update scripts

--- patch.coresight.sh
+++ patch.coresight.sh
@@ -1,1 +1,1 @@
-git diff -U9999999 origin/dev sys/arm64/coresight sys/dts/arm64 > patch.coresight
+git diff -U9999999 origin/dev sys/arm64/coresight sys/dts/arm64 sys/conf/files.arm64 > patch.coresight
