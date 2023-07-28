e26441e341f12b124c79ad03e82e04b39f196741
Jessica Clarke <jrtc27@jrtc27.com>
2023-07-29 00:50:37 +0100

    Makefile: Add NO_CHERI_TARGETS and NO_CHERI_KERNELS
    These are intended to allow performing a non-CHERI universe build, since
    we don't have a single compiler that can do all of CHERI-RISC-V, Morello
    and non-CHERI.

--- share/mk/sys.machine.mk
+++ share/mk/sys.machine.mk
@@ -6,10 +6,13 @@ TARGET_MACHINE_LIST?= amd64 arm arm64 i386 powerpc riscv
 
 MACHINE_ARCH_host?= ${_HOST_ARCH}
 
+CHERI_ARCHES_arm64?= aarch64c
+CHERI_ARCHES_riscv?= riscv64c
+
 MACHINE_ARCH_LIST_arm?= armv6 armv7
-MACHINE_ARCH_LIST_arm64?= aarch64 aarch64c
+MACHINE_ARCH_LIST_arm64?= aarch64 ${CHERI_ARCHES_arm64}
 MACHINE_ARCH_LIST_powerpc?= powerpc powerpc64 powerpc64le ${EXTRA_ARCHES_powerpc}
-MACHINE_ARCH_LIST_riscv?= riscv64 riscv64c
+MACHINE_ARCH_LIST_riscv?= riscv64 ${CHERI_ARCHES_riscv}
 
 .for m in ${TARGET_MACHINE_LIST}
 MACHINE_ARCH_LIST_$m?= $m
