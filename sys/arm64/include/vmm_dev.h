45989460bf9b138fb9c54206cd45056143437af0
Mark Johnston <markj@FreeBSD.org>
2023-09-04 16:43:31 -0400

    arm64/vmm: Annotate userspace pointers in ioctl parameters
    Note that we do not bother providing COMPAT64 compatibility for the vmm
    device interface, as libvmmapi is the only consumer we care about and it
    belongs to the base system.

--- sys/arm64/include/vmm_dev.h
+++ sys/arm64/include/vmm_dev.h
@@ -63,15 +63,15 @@ struct vm_register {
 struct vm_register_set {
 	int		cpuid;
 	unsigned int	count;
-	const int	*regnums;	/* enum vm_reg_name */
-	uint64_t	*regvals;
+	const int	* __kerncap regnums;	/* enum vm_reg_name */
+	uintcap_t	* __kerncap regvals;
 };
 
 struct vm_run {
 	int		cpuid;
-	cpuset_t	*cpuset;	/* CPU set storage */
+	cpuset_t	* __kerncap cpuset;	/* CPU set storage */
 	size_t		cpusetsize;
-	struct vm_exit	*vm_exit;
+	struct vm_exit	* __kerncap vm_exit;
 };
 
 struct vm_exception {
@@ -128,7 +128,7 @@ struct vm_activate_cpu {
 struct vm_cpuset {
 	int		which;
 	int		cpusetsize;
-	cpuset_t	*cpus;
+	cpuset_t	* __kerncap cpus;
 };
 #define	VM_ACTIVE_CPUS		0
 #define	VM_SUSPENDED_CPUS	1
