7243f03f54d27d781b77843ce28c1d0affc8e00e
Brian Li <brianli@sri.com>
2023-09-05 11:04:13 -0700

    cheribsdtest: Add sample tests for cheribsd_spawn_child
    Add sample cheribsdtests showcasing cheribsd_spawn_child()
    functionality. The child process simply exits with success,
    and the parent process captures the child process state change.

---
+++ bin/cheribsdtest/cheribsdtest_internal.c
@@ -0,0 +1,122 @@
+/*-
+* SPDX-License-Identifier: BSD-2-Clause
+*
+* Copyright (c) 2023 SRI International
+*
+* This software was developed by SRI International, the University of
+* Cambridge Computer Laboratory (Department of Computer Science and
+* Technology), and Capabilities Limited under Defense Advanced Research
+* Projects Agency (DARPA) Contract No. HR001122S0003 ("MTSS").
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+* 1. Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in the
+*    documentation and/or other materials provided with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+* OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+* OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+* SUCH DAMAGE.
+*/
+
+#include <sys/cdefs.h>
+
+#if !__has_feature(capabilities)
+#error "This code requires a CHERI-aware compiler"
+#endif
+
+#include <sys/wait.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "cheribsdtest.h"
+
+static void
+exec_child_cf(void)
+{
+	if (verbose)
+		fprintf(stderr, "in child function\n");
+
+	exit(0);
+}
+
+CHERIBSDTEST(internal_spawn_child_posix_spawn,
+    "check that directly spawning a child process with posix_spawn runs properly",
+    .ct_child_func = exec_child_cf)
+{
+	int res;
+	pid_t pid;
+
+	pid = cheribsdtest_spawn_child(SC_MODE_POSIX_SPAWN);
+
+	CHERIBSDTEST_VERIFY2(pid > 0, "spawning child process failed");
+	waitpid(pid, &res, 0);
+	if (res != 0)
+		cheribsdtest_failure_errx("Bad child process exit");
+
+	cheribsdtest_success();
+}
+
+CHERIBSDTEST(internal_spawn_child_fork,
+    "check that directly spawning a child process with fork runs properly",
+    .ct_child_func = exec_child_cf)
+{
+	int res;
+	pid_t pid;
+
+	pid = cheribsdtest_spawn_child(SC_MODE_FORK);
+
+	CHERIBSDTEST_VERIFY2(pid > 0, "spawning child process failed");
+	waitpid(pid, &res, 0);
+	if (res != 0)
+		cheribsdtest_failure_errx("Bad child process exit");
+
+	cheribsdtest_success();
+}
+
+CHERIBSDTEST(internal_spawn_child_vfork,
+    "check that directly spawning a child process with vfork runs properly",
+    .ct_child_func = exec_child_cf)
+{
+	int res;
+	pid_t pid;
+
+	pid = cheribsdtest_spawn_child(SC_MODE_VFORK);
+
+	CHERIBSDTEST_VERIFY2(pid > 0, "spawning child process failed");
+	waitpid(pid, &res, 0);
+	if (res != 0)
+		cheribsdtest_failure_errx("Bad child process exit");
+
+	cheribsdtest_success();
+}
+
+CHERIBSDTEST(internal_spawn_child_rfork,
+    "check that directly spawning a child process with rfork runs properly",
+    .ct_child_func = exec_child_cf)
+{
+	int res;
+	pid_t pid;
+
+	pid = cheribsdtest_spawn_child(SC_MODE_RFORK);
+
+	CHERIBSDTEST_VERIFY2(pid > 0, "spawning child process failed");
+	waitpid(pid, &res, 0);
+	if (res != 0)
+		cheribsdtest_failure_errx("Bad child process exit");
+
+	cheribsdtest_success();
+}
