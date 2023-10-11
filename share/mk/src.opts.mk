4ab46c3158f7b7e9e1d2b63ec9924d7b32564552
Brooks Davis <brooks@one-eyed-alien.net>
2023-10-12 00:28:06 +0100

    Switch back to using jemalloc for CheriABI
    jemalloc is a known quantity and we need both CheriABI and hybrid
    binaries to use the same implementation (as much as possible) for
    performance comparisons to have any meaning.
    
    This reverts commit 24bb3e62502f1c833ab3b5f50c107949812b1d02
    ("LIBC_MALLOC: switch to snmalloc for CheriABI").

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -298,10 +298,6 @@ __DEFAULT_DEPENDENT_OPTIONS+=	LLVM_TARGET_${__llt:${__LLVM_TARGET_FILT}:tu}/LLVM
 .endif
 .endfor
 
-.if ${__T:Maarch64*c*} || ${__T:Mriscv*c*}
-__LIBC_MALLOC_DEFAULT:=	snmalloc
-.endif
-
 __DEFAULT_NO_OPTIONS+=LLVM_TARGET_BPF LLVM_TARGET_MIPS
 
 .include <bsd.compiler.mk>
