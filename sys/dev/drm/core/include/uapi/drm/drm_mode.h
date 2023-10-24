4854b0a1a51e21b9ab75630e467b12837e488707
Jessica Clarke <jrtc27@jrtc27.com>
2023-10-24 20:02:51 +0100

    drm: Use correct type for drm_mode_crtc_page_flip's user_data
    In practice this doesn't matter, since the type is entirely unused;
    instead, drm_mode_crtc_page_flip_target is always used (the same type
    but with reserved no longer reserved) to access the user data, whether
    it or the old type was passed. However, we should get the types right
    for documentation purposes and to guard against future issues if this
    does start to be used.

--- sys/dev/drm/core/include/uapi/drm/drm_mode.h
+++ sys/dev/drm/core/include/uapi/drm/drm_mode.h
@@ -898,12 +898,22 @@ struct hdr_output_metadata {
  * The reserved field must be zero.
  */
 
-struct drm_mode_crtc_page_flip {
+#ifdef _KERNEL
+struct drm_mode_crtc_page_flip64 {
 	__u32 crtc_id;
 	__u32 fb_id;
 	__u32 flags;
 	__u32 reserved;
 	__u64 user_data;
+};
+#endif
+
+struct drm_mode_crtc_page_flip {
+	__u32 crtc_id;
+	__u32 fb_id;
+	__u32 flags;
+	__u32 reserved;
+	kuint64cap_t user_data;
 };
 
 /*
