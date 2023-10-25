f3b7f9d902ebdebec3dcba91cb7b83cf75c4d79d
John Baldwin <jhb@FreeBSD.org>
2023-10-25 08:41:00 -0700

    Enable revocation by default

--- sys/cheri/cheri_sysctl.c
+++ sys/cheri/cheri_sysctl.c
@@ -76,7 +76,7 @@ SYSCTL_INT(_security_cheri, OID_AUTO, bound_legacy_capabilities,
  * compute the revocation flags in AT_BSDFLAGS but can be overridden
  * by elfctl(1) flags and procctl(2).
  */
-int security_cheri_runtime_revocation_default = 0;
+int security_cheri_runtime_revocation_default = 1;
 SYSCTL_INT(_security_cheri, OID_AUTO, runtime_revocation_default, CTLFLAG_RWTUN,
     &security_cheri_runtime_revocation_default, 0,
     "Userspace runtime revocation default");
