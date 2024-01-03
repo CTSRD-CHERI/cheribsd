2cbf8b26685d93c3095fd9537e200d9806cfee0f
Dapeng Gao <dapeng.gao@cl.cam.ac.uk>
2024-01-03 12:14:26 +0800

    c18n: Support compartment policy files
    Load a compartment policy file via the environment variable
    LD_C18N_COMPARTMENT_POLICY.
    
    Currently, the policy allows one to
    (a) define logical compartments comprising multiple libraries,
    (b) specify which symbols are trusted by a particular compartment, and
    (c) specify which libraries are allowed to link against any particular
    symbol.

--- libexec/rtld-elf/rtld.c
+++ libexec/rtld-elf/rtld.c
@@ -399,6 +399,7 @@ enum {
 #if defined(__CHERI_PURE_CAPABILITY__) && defined(RTLD_SANDBOX)
 	LD_UTRACE_COMPARTMENT,
 	LD_COMPARTMENT_ENABLE,
+	LD_COMPARTMENT_POLICY,
 	LD_COMPARTMENT_OVERHEAD,
 	LD_COMPARTMENT_SIG,
 	LD_COMPARTMENT_UNWIND,
@@ -445,6 +446,7 @@ static struct ld_env_var_desc ld_env_vars[] = {
 #if defined(__CHERI_PURE_CAPABILITY__) && defined(RTLD_SANDBOX)
 	LD_ENV_DESC(UTRACE_COMPARTMENT, false),
 	LD_ENV_DESC(COMPARTMENT_ENABLE, false),
+	LD_ENV_DESC(COMPARTMENT_POLICY, false),
 	LD_ENV_DESC(COMPARTMENT_OVERHEAD, false),
 	LD_ENV_DESC(COMPARTMENT_SIG, false),
 	LD_ENV_DESC(COMPARTMENT_UNWIND, false),
@@ -844,6 +846,7 @@ _rtld(Elf_Addr *sp, func_ptr_type *exit_proc, Obj_Entry **objp)
 #if defined(__CHERI_PURE_CAPABILITY__) && defined(RTLD_SANDBOX)
     ld_compartment_utrace = ld_get_env_var(LD_UTRACE_COMPARTMENT);
     ld_compartment_enable = ld_get_env_var(LD_COMPARTMENT_ENABLE);
+    ld_compartment_policy = ld_get_env_var(LD_COMPARTMENT_POLICY);
     ld_compartment_overhead = ld_get_env_var(LD_COMPARTMENT_OVERHEAD);
     ld_compartment_sig = ld_get_env_var(LD_COMPARTMENT_SIG);
     ld_compartment_unwind = ld_get_env_var(LD_COMPARTMENT_UNWIND);
@@ -960,6 +963,15 @@ _rtld(Elf_Addr *sp, func_ptr_type *exit_proc, Obj_Entry **objp)
     linkmap_add(obj_main);
     linkmap_add(&obj_rtld);
 
+#if defined(__CHERI_PURE_CAPABILITY__) && defined(RTLD_SANDBOX)
+    c18n_init();
+
+    /*
+     * Manually register the main object after the policy is loaded.
+     */
+    object_add_name(obj_main, "[main]");
+#endif
+
     /* Link the main program into the list of objects. */
     TAILQ_INSERT_HEAD(&obj_list, obj_main, next);
     obj_count++;
@@ -988,35 +1000,6 @@ _rtld(Elf_Addr *sp, func_ptr_type *exit_proc, Obj_Entry **objp)
     if (load_preload_objects(ld_preload, false) == -1)
 	rtld_die();
     preload_tail = globallist_curr(TAILQ_LAST(&obj_list, obj_entry_q));
-
-#if defined(__CHERI_PURE_CAPABILITY__) && defined(RTLD_SANDBOX)
-    /*
-     * XXX: This block of code looks for a preloaded library named
-     * _rtld_c18n_policy.so that contains user-defined compartment policies.
-     * This will be replaced with a proper policy-loading mechanism in the
-     * future.
-     */
-    TAILQ_FOREACH(obj, &obj_list, next) {
-	SymLook req;
-
-	if (!object_match_name(obj, "_rtld_c18n_policy.so"))
-	    continue;
-
-	/* Horrible hacks */
-	if (relocate_object(obj, false, &obj_rtld, SYMLOOK_EARLY, NULL) == -1)
-	    rtld_die();
-	obj->versyms = NULL;
-
-	symlook_init(&req, "_rtld_compartments");
-	req.lockstate = &lockstate;
-	if (symlook_obj(&req, obj) == 0)
-	    c18n_add_comparts(make_data_pointer(req.sym_out, req.defobj_out));
-	break;
-    }
-
-    /* Manually add name for main object after policy is loaded */
-    object_add_name(obj_main, "[main]");
-#endif
 
     dbg("loading needed objects");
     if (load_needed_objects(obj_main, ld_tracing != NULL ? RTLD_LO_TRACE :
@@ -2811,11 +2794,6 @@ init_rtld(caddr_t mapbase, Elf_Auxinfo **aux_info)
     r_debug.r_brk = make_rtld_local_function_pointer(r_debug_state);
     r_debug.r_state = RT_CONSISTENT;
     r_debug.r_ldbase = obj_rtld.relocbase;
-
-#if defined(__CHERI_PURE_CAPABILITY__) && defined(RTLD_SANDBOX)
-    obj_rtld.compart_id = C18N_RTLD_COMPART_ID;
-    c18n_init();
-#endif
 }
 
 /*
