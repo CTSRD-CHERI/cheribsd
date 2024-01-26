dcb219b6ffc169cfe5b0ee6d8504571f07ff60cb
Edward Tomasz Napierala <trasz@FreeBSD.org>
2024-01-26 17:36:37 +0000

    Fix panic on cosetup(2), caused by merge

--- sys/cheri/cheri_colocation.c
+++ sys/cheri/cheri_colocation.c
@@ -253,6 +253,13 @@ wakeupself(void)
 	wakeup(chan);
 }
 
+static void * __capability
+errno_cap(int error)
+{
+
+	return (cheri_capability_build_user_rwx_unchecked(0, 0, 0, error));
+}
+
 void
 colocation_get_peer(struct thread *td, struct thread **peertdp)
 {
@@ -310,7 +317,7 @@ colocation_thread_exit(struct thread *td)
 	 * Set scb_caller_scb to a special "null" capability, so that cocall(2)
 	 * can see the callee thread is dead.
 	 */
-	scb.scb_caller_scb = cheri_capability_build_user_data(0, 0, 0, EPIPE);
+	scb.scb_caller_scb = errno_cap(EPIPE);
 	scb.scb_td = NULL;
 	scb.scb_borrower_td = NULL;
 
@@ -517,7 +524,7 @@ setup_scb(struct thread *td)
 	scb.scb_unsealcap = switcher_sealcap2;
 	scb.scb_td = td;
 	scb.scb_borrower_td = NULL;
-	scb.scb_caller_scb = cheri_capability_build_user_data(0, 0, 0, EAGAIN);
+	scb.scb_caller_scb = errno_cap(EAGAIN);
 	colocation_store_scb(td, &scb);
 
 	return (0);
@@ -892,8 +899,7 @@ again:
 		COLOCATION_DEBUG("msleep failed with error %d", error);
 	}
 
-	colocation_store_caller_scb(targetscb,
-	    cheri_capability_build_user_data(0, 0, 0, EPROTOTYPE));
+	colocation_store_caller_scb(targetscb, errno_cap(EPROTOTYPE));
 
 	/*
 	 * Wake up other callers that might be waiting in kern_cocall_slow().
@@ -971,8 +977,7 @@ kern_coaccept_slow(void * __capability * __capability cookiep,
 		 * couldn't have raced with coaccept(2), because a thread cannot
 		 * call coaccept_slow(2) and coaccept(2) at the same time.
 		 */
-		colocation_store_caller_scb(curthread->td_scb,
-		    cheri_capability_build_user_data(0, 0, 0, EPROTOTYPE));
+		colocation_store_caller_scb(curthread->td_scb, errno_cap(EPROTOTYPE));
 	} else {
 		/*
 		 * There's a caller waiting for us, get them their data
