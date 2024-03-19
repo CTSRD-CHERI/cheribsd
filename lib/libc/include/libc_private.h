e924025ec3a958f8bad7934c491db8a3f87c15c0
Brooks Davis <brooks@FreeBSD.org>
2024-03-19 21:52:39 +0000

    syscalls.master: make __sys_fcntl take an intptr_t
    The (optional) third argument of fcntl is sometimes a pointer so change
    the type to intptr_t.  Update the libc-internal defintion (actually used
    by libthr) to take a fixed intptr_t argument rather than pretending it's
    a variadic function.  (That worked because all supported architectures
    pass variadic arguments as though the function was declared with those
    types.  In CheriBSD that changes because variadic arguments are passed
    via a bounded array.)
    
    Reviewed by:	kib
    Differential Revision:	https://reviews.freebsd.org/D44381
    
    (cherry picked from commit d0efabdf15d956e9bc0414356ed798ca3c846e08)

--- lib/libc/include/libc_private.h
+++ lib/libc/include/libc_private.h
@@ -359,7 +359,6 @@ int		__sys_clock_nanosleep(__clockid_t, int,
 int		__sys_close(int);
 int		__sys_close_range(unsigned, unsigned, int);
 int		__sys_connect(int, const struct sockaddr *, __socklen_t);
-		/* fcntl declared to match the kernel so we can call directly */
 int		__sys_fcntl(int, int, __intptr_t);
 int		__sys_fdatasync(int);
 int		__sys_fstatat(int, const char *, struct stat *, int);
