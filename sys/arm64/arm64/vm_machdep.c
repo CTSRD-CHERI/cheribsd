bbb8357bfec07badc162794562b3e7b1623424cc
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2024-05-10 15:47:36 +0000

    Don't use a trampoline for fork_trampoline()
    Install a pointer to fork_trampoline() rather than its
    compartmentalization trampoline in LR.

--- sys/arm64/arm64/vm_machdep.c
+++ sys/arm64/arm64/vm_machdep.c
@@ -130,7 +130,8 @@ SUPERVISOR_ENTRY(void, cpu_fork, (struct thread *td1, struct proc *p2,
 	/* Set the return value registers for fork() */
 	td2->td_pcb->pcb_x[PCB_X19] = (uintptr_t)fork_return;
 	td2->td_pcb->pcb_x[PCB_X20] = (uintptr_t)td2;
-	td2->td_pcb->pcb_x[PCB_LR] = (uintptr_t)fork_trampoline;
+	td2->td_pcb->pcb_x[PCB_LR] =
+	    (uintptr_t)supervisor_get_function((uintptr_t)fork_trampoline);
 	td2->td_pcb->pcb_sp = (uintptr_t)td2->td_frame;
 
 	vfp_new_thread(td2, td1, true);
@@ -208,7 +209,8 @@ SUPERVISOR_ENTRY(void, cpu_copy_thread, (struct thread *td,
 
 	td->td_pcb->pcb_x[PCB_X19] = (uintptr_t)fork_return;
 	td->td_pcb->pcb_x[PCB_X20] = (uintptr_t)td;
-	td->td_pcb->pcb_x[PCB_LR] = (uintptr_t)fork_trampoline;
+	td->td_pcb->pcb_x[PCB_LR] =
+	    (uintptr_t)supervisor_get_function((uintptr_t)fork_trampoline);
 	td->td_pcb->pcb_sp = (uintptr_t)td->td_frame;
 
 	/* Update VFP state for the new thread */
