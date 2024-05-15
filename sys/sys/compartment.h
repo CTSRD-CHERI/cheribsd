196b9598bc86fc0b5edb9c466a2348bce4c98cc3
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2024-05-15 10:58:38 +0000

    Remove unused compartment_call()

--- sys/sys/compartment.h
+++ sys/sys/compartment.h
@@ -58,7 +58,6 @@ struct compartment *compartment_create_for_thread(struct thread *td, int id);
 void compartment_destroy(struct compartment *compartment);
 void compartment_trampoline_destroy(uintptr_t func);
 vm_pointer_t compartment_entry_stackptr(int id, int type);
-void *compartment_call(uintptr_t func);
 void *compartment_entry_for_kernel(uintptr_t func);
 void *compartment_entry(uintptr_t func);
 void *supervisor_entry_for_kernel(uintptr_t func);
