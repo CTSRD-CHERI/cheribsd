d783ce8496869effd92baa7813c6a6cfb4dfd136
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2024-06-27 12:54:14 +0000

    netinet: correct SIOCDIFADDR{,_IN6} calls to use {,in6_}ifreq
    The SIOCDIFADDR{,_IN6} ioctls take an ifreq structure object, not an
    ifaliasreq/in_aliasreq/in6_aliasreq structure object, as their
    argument.
    
    On CHERI, the incorrect calls using the in6_aliasreq object result in
    CHERI capability violations. A pointer to the ifra_addr field in
    in6_aliasreq cast to the ifru_addr union member of in6_ifreq results in
    bounds being set to the union's larger size. Such bounds exceed the
    bounds of of in6_aliasreq object and the bounds-setting instruction
    clears a tag of the object's capability.

--- sys/netlink/route/iface.c
+++ sys/netlink/route/iface.c
@@ -1194,17 +1194,17 @@ static int
 handle_deladdr_inet(struct nlmsghdr *hdr, struct nl_parsed_ifa *attrs,
     if_t ifp, struct nlpcb *nlp, struct nl_pstate *npt)
 {
-	struct sockaddr_in *addr = (struct sockaddr_in *)attrs->ifa_local;
+	struct sockaddr *addr = attrs->ifa_local;
 
 	if (addr == NULL)
-		addr = (struct sockaddr_in *)attrs->ifa_address;
+		addr = attrs->ifa_address;
 
 	if (addr == NULL) {
 		nlmsg_report_err_msg(npt, "empty IFA_ADDRESS/IFA_LOCAL");
 		return (EINVAL);
 	}
 
-	struct in_aliasreq req = { .ifra_addr = *addr };
+	struct ifreq req = { .ifr_addr = *addr };
 
 	return (in_control_ioctl(SIOCDIFADDR, &req, ifp, nlp_get_cred(nlp)));
 }
@@ -1288,7 +1288,7 @@ handle_deladdr_inet6(struct nlmsghdr *hdr, struct nl_parsed_ifa *attrs,
 		return (EINVAL);
 	}
 
-	struct in6_aliasreq req = { .ifra_addr = *addr };
+	struct in6_ifreq req = { .ifr_addr = *addr };
 
 	return (in6_control_ioctl(SIOCDIFADDR_IN6, &req, ifp, nlp_get_cred(nlp)));
 }
