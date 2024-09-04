aafcf1aa215b5abe6124b7cb148b21c2c143af8e
Mark Johnston <markj@FreeBSD.org>
2024-09-04 15:53:00 +0000

    phys_pager: Avoid holding the object lock over the pager dtor
    As in dev_pager_dealloc(), drop the object lock around calls to the destructor.
    This might be necessary if the destructor wants to sleep.

--- sys/vm/phys_pager.c
+++ sys/vm/phys_pager.c
@@ -154,16 +154,16 @@ static void
 phys_pager_dealloc(vm_object_t object)
 {
 
+	VM_OBJECT_WUNLOCK(object);
 	if (object->handle != NULL) {
-		VM_OBJECT_WUNLOCK(object);
 		mtx_lock(&phys_pager_mtx);
 		TAILQ_REMOVE(&phys_pager_object_list, object, pager_object_list);
 		mtx_unlock(&phys_pager_mtx);
-		VM_OBJECT_WLOCK(object);
 	}
-	object->type = OBJT_DEAD;
 	if (object->un_pager.phys.ops->phys_pg_dtor != NULL)
 		object->un_pager.phys.ops->phys_pg_dtor(object);
+	VM_OBJECT_WLOCK(object);
+	object->type = OBJT_DEAD;
 	object->handle = NULL;
 }
 
