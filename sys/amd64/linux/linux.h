094d321f9b5a1d81a3a8889bdde0d82ca61110e5
w4123 <1840686745@qq.com>
2024-11-19 09:46:17 +0000

    linuxulator: Fix Too many auxargs

--- sys/amd64/linux/linux.h
+++ sys/amd64/linux/linux.h
@@ -87,7 +87,7 @@ typedef struct {
 /*
  * Miscellaneous
  */
-#define LINUX_AT_COUNT		21	/* Count of used aux entry types.
+#define LINUX_AT_COUNT		41	/* Count of used aux entry types.
 					 * Keep this synchronized with
 					 * linux_copyout_auxargs() code.
 					 */
