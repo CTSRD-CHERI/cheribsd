4e52d3dfbabaafd1caa23b4bfe79d86fe4cba008
John Baldwin <jhb@FreeBSD.org>
2024-12-05 17:08:09 -0500

    elf: Add definitions for PT_COMPARTMENT and DT_C18NSTRTAB*
    (cherry picked from commit 430fc8c01c59f64623da319b7c0be7e49f290254)

--- sys/sys/elf_common.h
+++ sys/sys/elf_common.h
@@ -536,6 +536,7 @@ typedef struct {
 #define	PT_LOOS		0x60000000	/* First OS-specific. */
 #define	PT_C18N_NAME	0x64331380	/* Sub-object compartment. */
 #define	PT_CHERI_PCC	0x64348450	/* CHERI PCC bounds. */
+#define	PT_COMPARTMENT	0x64331380	/* Sub-object compartment. */
 #define	PT_CHERI_BOUNDS	0x64348450	/* CHERI PCC bounds. */
 #define	PT_SUNW_UNWIND	0x6464e550	/* amd64 UNWIND program header */
 #define	PT_GNU_EH_FRAME	0x6474e550
