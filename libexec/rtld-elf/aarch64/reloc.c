6e75446efe0dd29bccc98752ffb8a01ad507c35f
Dapeng Gao <dapeng.gao@cl.cam.ac.uk>
2024-12-10 21:01:06 +0800

    rtld: Add support for R_{AARCH64,MORELLO}_FUNC_RELATIVE

--- libexec/rtld-elf/aarch64/reloc.c
+++ libexec/rtld-elf/aarch64/reloc.c
@@ -226,14 +226,23 @@ _rtld_relocate_nonplt_self(Elf_Dyn *dynp, Elf_Auxinfo *aux)
 	relalim = (const Elf_Rela *)((const char *)rela + relasz);
 	pcc = __builtin_cheri_program_counter_get();
 
-	/* Self-relocations should all be local, i.e. R_MORELLO_RELATIVE. */
+	/* Self-relocations should all be local, i.e. relative. */
 	for (; rela < relalim; rela++) {
-		if (ELF_R_TYPE(rela->r_info) != R_MORELLO_RELATIVE)
+		switch (ELF_R_TYPE(rela->r_info)) {
+		case R_AARCH64_RELATIVE:
+		case R_AARCH64_FUNC_RELATIVE:
+			*where = (Elf_Addr)(relocbase + rela->r_addend);
+			break;
+		case R_MORELLO_RELATIVE:
+		case R_MORELLO_FUNC_RELATIVE:
+			where = (Elf_Addr *)(relocbase + rela->r_offset);
+			*(uintcap_t *)where = init_cap_from_fragment(where,
+			    relocbase, pcc, (Elf_Addr)(uintptr_t)relocbase,
+			    rela->r_addend);
+			break;
+		default:
 			__builtin_trap();
-
-		where = (Elf_Addr *)(relocbase + rela->r_offset);
-		*(uintcap_t *)where = init_cap_from_fragment(where, relocbase,
-		    pcc, (Elf_Addr)(uintptr_t)relocbase, rela->r_addend);
+		}
 	}
 }
 #endif /* __CHERI_PURE_CAPABILITY__ */
@@ -889,6 +898,7 @@ reloc_non_plt(Obj_Entry *obj, Obj_Entry *obj_rtld, int flags,
 				return (-1);
 			break;
 		case R_MORELLO_RELATIVE:
+		case R_MORELLO_FUNC_RELATIVE:
 			*(uintcap_t *)(void *)where =
 			    init_cap_from_fragment(where, data_cap,
 				text_rodata_cap,
@@ -966,6 +976,7 @@ reloc_non_plt(Obj_Entry *obj, Obj_Entry *obj_rtld, int flags,
 			break;
 #endif
 		case R_AARCH64_RELATIVE:
+		case R_AARCH64_FUNC_RELATIVE:
 			*where = (Elf_Addr)(obj->relocbase + rela->r_addend);
 			break;
 		case R_AARCH64_NONE:
