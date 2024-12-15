36c8303f231320a221835393c618607bdbd34741
Dapeng Gao <dapeng.gao@cl.cam.ac.uk>
2024-12-15 18:22:22 +0800

    cheribsdtest: Increase signal_returncap length under c18n
    When library-based compartmentalisation causes every function pointer to
    be wrapped in a trampoline, signal handlers return back to a trampoline
    rather than sigcode. The length return address capability is thus
    increased to account for this.

--- bin/cheribsdtest/cheribsdtest_signal.c
+++ bin/cheribsdtest/cheribsdtest_signal.c
@@ -267,6 +267,8 @@ CHERIBSDTEST(signal_returncap,
 	CHERIBSDTEST_VERIFY2(v == CHERI_CAP_USER_CODE_LENGTH,
 	    "length 0x%jx (expected <= 0x%jx)", v,
 	    (uintmax_t)CHERI_CAP_USER_CODE_LENGTH);
+#elif defined(CHERIBSD_C18N_TESTS)
+	CHERIBSDTEST_VERIFY2(v <= 0x300, "length 0x%jx (expected <= 0x300)", v);
 #else
 	CHERIBSDTEST_VERIFY2(v <= 0x100, "length 0x%jx (expected <= 0x100)", v);
 #endif
