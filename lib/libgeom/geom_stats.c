779cd52486d8123c418a01f0c4a2bd34a69a7080
w4123 <1840686745@qq.com>
2025-01-17 03:48:15 +0800

    Test ptraddr_t

--- lib/libgeom/geom_stats.c
+++ lib/libgeom/geom_stats.c
@@ -75,7 +75,8 @@ geom_stats_resync(void)
 		err(1, "DIOCGMEDIASIZE(" _PATH_DEV DEVSTAT_DEVICE_NAME ")");
 
 	munmap(statp, npages * pagesize);
-	p = mmap((void *)(uintptr_t)(uint64_t)(uintptr_t)statp, mediasize, PROT_READ, MAP_SHARED, statsfd, 0);
+	p = mmap((void *)(uintptr_t)(ptraddr_t)statp, mediasize, PROT_READ, 
+	    MAP_SHARED, statsfd, 0);
 	if (p == MAP_FAILED)
 		err(1, "mmap(/dev/devstat):");
 	else {
