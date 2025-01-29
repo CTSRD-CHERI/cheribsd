b71524e0fb77f1e8c15bd883db6c58bdff3eb5ba
Mark Johnston <markj@FreeBSD.org>
2025-01-29 23:05:36 +0000

    dtrace/arm64: Synthesize patchpoint capabilities
    The kernel linker gives us read-only capabilities for function symbol
    values, but we need to be able to overwrite tracepoints with breakpoints.
    For each tracepoint, derive a patchpoint capability from kernel_root_cap.

--- sys/cddl/dev/fbt/aarch64/fbt_isa.c
+++ sys/cddl/dev/fbt/aarch64/fbt_isa.c
@@ -80,6 +80,36 @@ fbt_patch_tracepoint(fbt_probe_t *fbt, fbt_patchval_t val)
 	cpu_icache_sync_range(fbt->fbtp_patchpoint, 4);
 }
 
+#ifdef __CHERI_PURE_CAPABILITY__
+/*
+ * Given the address of an instruction that we might patch in order to enable a
+ * probe, return a capability that can be used to do just that and no more.
+ * Unfortunately, the capability provided by the linker cannot be used directly
+ * here since it does not provide store permissions.
+ */
+static fbt_patchval_t *
+fbt_make_tracepoint_capability(uint32_t *instr)
+{
+	fbt_patchval_t *cap;
+	ptraddr_t addr;
+
+	if (cheri_getsealed(instr))
+		return (NULL);
+	if (!cheri_gettag(instr))
+		return (NULL);
+	addr = (ptraddr_t)instr;
+	if (addr + INSN_SIZE < addr ||
+	    addr < cheri_getbase(instr) ||
+	    addr + INSN_SIZE > cheri_getbase(instr) + cheri_getlength(instr))
+		return (NULL);
+
+	cap = cheri_setaddress(kernel_root_cap, addr);
+	cap = cheri_setbounds(cap, INSN_SIZE);
+	cap = cheri_andperm(cap, CHERI_PERM_STORE);
+	return (cap);
+}
+#endif
+
 int
 fbt_provide_module_function(linker_file_t lf, int symindx,
     linker_symval_t *sym, void *opaque)
@@ -172,7 +202,7 @@ found:
 	fbt->fbtp_id = dtrace_probe_create(fbt_id, modname,
 	    name, FBT_ENTRY, FBT_AFRAMES, fbt);
 #ifdef __CHERI_PURE_CAPABILITY__
-	fbt->fbtp_patchpoint = cheri_setbounds(instr, INSN_SIZE);
+	fbt->fbtp_patchpoint = fbt_make_tracepoint_capability(instr);
 #else
 	fbt->fbtp_patchpoint = instr;
 #endif
@@ -223,7 +253,7 @@ again:
 	retfbt = fbt;
 
 #ifdef __CHERI_PURE_CAPABILITY__
-	fbt->fbtp_patchpoint = cheri_setbounds(instr, INSN_SIZE);
+	fbt->fbtp_patchpoint = fbt_make_tracepoint_capability(instr);
 #else
 	fbt->fbtp_patchpoint = instr;
 #endif
