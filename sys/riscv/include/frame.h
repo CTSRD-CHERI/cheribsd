freebsd/freebsd-src

riscv: Fix and generalise saving TP (PCPU pointer) whilst in userspace
In cpu_fork, we allocate sizeof(struct pcb) + sizeof(struct trapframe)
space on the stack, then round it for stack alignment. This not only
fails to include the space needed for TP but also doesn't round up the
trapframe allocation to be stack-aligned, yet TF_SIZE does, as is the
expectation of fork_trampoline and cpu_exception_handler. Given that
sizeof(struct pcb) + sizeof(struct trapframe) is a multiple of 16, this
causes the saved TP to be stored in the PCB's pcb_sp (the intended
trapframe padding aliasing pcb_ra), which is probably harmless in
practice as the PCB isn't expected to be current, but definitely not
intended.

In cpu_thread_alloc, we do include the 8 bytes for TP and then stack
align that. This again fails to include the padding for trapframe as
present in TF_SIZE, but sizeof(struct pcb) + sizeof(struct trapframe)
happens to be a multiple of 16, as above, so adding 8 then rounding to
stack alignment (16) includes an extra 8 bytes of padding, giving the
right result for the wrong reason (and could be broken by future struct
growth).

Extract the calculation into a shared function that rounds correctly
regardless of the struct layouts. Also introduce a new struct kernframe
to encapsulate and clearly document this shared state rather than using
magic constants, and also enable it to be easily extended in future, as
we intend to do downstream in CheriBSD.

Reviewed by:	jhb
MFC after:	1 week
Differential Revision:	https://reviews.freebsd.org/D48799

sys/riscv/include/frame.h
@@ -57,6 +57,10 @@ struct trapframe {
 	uint64_t tf_scause;
 };
 
+#ifdef _KERNEL
+#define	TF_SIZE	(roundup2(sizeof(struct trapframe), STACKALIGNBYTES + 1))
+#endif
+
 /*
  * Signal frame. Pushed onto user stack before calling sigcode.
  */
@@ -64,6 +68,16 @@ struct sigframe {
 	siginfo_t	sf_si;	/* actual saved siginfo */
 	ucontext_t	sf_uc;	/* actual saved ucontext */
 };
+
+#ifdef _KERNEL
+/*
+ * Kernel frame. Reserved near the top of kernel stacks for saving kernel
+ * state while in userspace.
+ */
+struct kernframe {
+	register_t	kf_tp;
+};
+#endif
 
 #endif /* !LOCORE */
 

CTSRD-CHERI/cheribsd

CheriBSD cherry-pick message:
We "fixed" the code downstream in the initial CHERI-RISC-V port to
allocate space for TP in both functions. However, there were two issues
with this.
Firstly, note that we had #if __has_feature(capabilities) backwards, so
we only allocated 8 bytes on CHERI kernels for TP, but because we stack
aligned before allocating the struct trapframe and struct pcb contains
capabilities in purecap kernels this ended up being rounded up to 16
bytes. On hybrid kernels though that's not the case, so struct pcb is a
multiple of 8 bytes in size and thus the stack aligning does nothing,
only allocating 8 bytes for TP when we're in fact storing a full
capability. In practice though it's used as an integer so the metadata
isn't relevant, and storing the capability metadata over pcb_ra has the
same non-effect as upstream has with storing TP over pcb_sp.
Secondly, because we stack aligned prior to allocating the trapframe yet
also failed to use TF_SIZE instead of sizeof(struct trapframe),
non-CHERI kernels end up having a misaligned stack throughout the
kernel (except for thread0, or if an overaligned stack allocation forces
the compiler to realign the stack).
(cherry picked from commit 21c534b9f5d5de07137f9a42e016876dd6ae66c1)

sys/riscv/include/frame.h
@@ -69,6 +69,10 @@ struct trapframe {
 	uint64_t tf_scause;
 };
 
+#ifdef _KERNEL
+#define	TF_SIZE	(roundup2(sizeof(struct trapframe), STACKALIGNBYTES + 1))
+#endif
+
 /*
  * Signal frame. Pushed onto user stack before calling sigcode.
  */
@@ -81,6 +85,16 @@ struct sigframe {
 struct sigframe64 {
 	struct __siginfo64 sf_si;	/* actual saved siginfo */
 	ucontext64_t	sf_uc;	/* actual saved ucontext */
+};
+#endif
+
+#ifdef _KERNEL
+/*
+ * Kernel frame. Reserved near the top of kernel stacks for saving kernel
+ * state while in userspace.
+ */
+struct kernframe {
+	uintcap_t	kf_tp;
 };
 #endif
 