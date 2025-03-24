d85aa6303b4b1b93086a8d27f0ea735f803e02e8
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2025-03-24 20:06:31 +0000

    cheri-rv64y: Implement Xcheri and RVY version of pointer atomics.

--- sys/riscv/include/atomic.h
+++ sys/riscv/include/atomic.h
@@ -462,19 +462,31 @@ atomic_set_ptr(volatile uintptr_t *p, uintptr_t val)
 #ifdef notyet
 	(void)__atomic_or_fetch(p, val, __ATOMIC_RELAXED);
 #else
-	uintptr_t temp1;
-	u_long temp2;
+	u_long temp1;
 
+#ifdef __riscv_xcheri
 	__asm __volatile(
-		"1:	clr.c	%1, %0\n"
-		"	cgetaddr %2, %1\n"
-		"	or	%2, %2, %3\n"
-		"	csetaddr %1, %1, %2\n"
-		"	csc.c	%2, %1, %0\n"
-		"	bnez	%2, 1b\n"
-		: "+A" (*p), "=&C" (temp1), "=&r" (temp2)
+		"1:	clr.c	ct0, %0\n"
+		"	mv	%1, t0\n"
+		"	or	%1, %1, %2\n"
+		"	csetaddr	ct0, ct0, %1\n"
+		"	csc.c	%1, ct0, %0\n"
+		"	bnez	%1, 1b\n"
+		: "+A" (*p), "=&r" (temp1)
 		: "r" ((ptraddr_t)val)
-		: "memory");
+		: "ct0", "memory");
+#else
+	__asm __volatile(
+		"1:	lr.c	ct0, %0\n"
+		"	mv	%1, t0\n"
+		"	or	%1, %1, %2\n"
+		"	scaddr	ct0, ct0, %1\n"
+		"	sc.c	%1, ct0, %0\n"
+		"	bnez	%1, 1b\n"
+		: "+A" (*p), "=&r" (temp1)
+		: "r" ((ptraddr_t)val)
+		: "ct0", "memory");
+#endif
 #endif
 }
 
@@ -485,19 +497,31 @@ atomic_clear_ptr(volatile uintptr_t *p, uintptr_t val)
 #ifdef notyet
 	(void)__atomic_and_fetch(p, ~val, __ATOMIC_RELAXED);
 #else
-	uintptr_t temp1;
-	u_long temp2;
+	u_long temp1;
 
+#ifdef __riscv_xcheri
 	__asm __volatile(
-		"1:	clr.c	%1, %0\n"
-		"	cgetaddr %2, %1\n"
-		"	and	%2, %2, %3\n"
-		"	csetaddr %1, %1, %2\n"
-		"	csc.c	%2, %1, %0\n"
-		"	bnez	%2, 1b\n"
-		: "+A" (*p), "=&C" (temp1), "=&r" (temp2)
+		"1:	clr.c	ct0, %0\n"
+		"	mv	%1, t0\n"
+		"	and	%1, %1, %2\n"
+		"	csetaddr	ct0, ct0, %1\n"
+		"	csc.c	%1, ct0, %0\n"
+		"	bnez	%1, 1b\n"
+		: "+A" (*p), "=&r" (temp1)
 		: "r" (~(ptraddr_t)val)
-		: "memory");
+		: "ct0", "memory");
+#else
+	__asm __volatile(
+		"1:	lr.c	ct0, %0\n"
+		"	mv	%1, t0\n"
+		"	and	%1, %1, %2\n"
+		"	scaddr	ct0, ct0, %1\n"
+		"	sc.c	%1, ct0, %0\n"
+		"	bnez	%1, 1b\n"
+		: "+A" (*p), "=&r" (temp1)
+		: "r" (~(ptraddr_t)val)
+		: "ct0", "memory");
+#endif
 #endif
 }
 
@@ -525,19 +549,33 @@ atomic_testandclear_ptr(volatile uintptr_t *p, u_int val)
 #ifdef notyet
 	old = __atomic_or_fetch(p, val, __ATOMIC_RELAXED);
 #else
-	uintptr_t temp1;
-	u_long temp2;
+	u_long temp1;
 
+#ifdef __riscv_xcheri
 	__asm __volatile(
-		"1:	clr.c	%1, %0\n"
-		"	cgetaddr %3, %1\n"
-		"	and	%3, %3, %4\n"
-		"	csetaddr %2, %1, %3\n"
-		"	csc.c	%3, %2, %0\n"
-		"	bnez	%3, 1b\n"
-		: "+A" (*p), "=&C" (old), "=&C" (temp1), "=&r" (temp2)
+		"1:	clr.c	ct0, %0\n"
+		"	mv	%2, t0\n"
+		"	and	%2, %2, %3\n"
+		"	csetaddr	%1, ct0, %2\n"
+		"	csc.c	%2, %1, %0\n"
+		"	bnez	%2, 1b\n"
+		"	cmove	%1, ct0\n"
+		: "+A" (*p), "=&C" (old), "=&r" (temp1)
 		: "r" (~(ptraddr_t)mask)
-		: "memory");
+		: "ct0", "memory");
+#else
+	__asm __volatile(
+		"1:	lr.c	ct0, %0\n"
+		"	mv	%2, t0\n"
+		"	and	%2, %2, %3\n"
+		"	scaddr	%1, ct0, %2\n"
+		"	sc.c	%2, %1, %0\n"
+		"	bnez	%2, 1b\n"
+		"	cmv	%1, ct0\n"
+		: "+A" (*p), "=&C" (old), "=&r" (temp1)
+		: "r" (~(ptraddr_t)mask)
+		: "ct0", "memory");
+#endif
 #endif
 
 	return ((old & mask) != 0);
@@ -553,19 +591,33 @@ atomic_testandset_ptr(volatile uintptr_t *p, u_int val)
 #ifdef notyet
 	old = __atomic_or_fetch(p, val, __ATOMIC_RELAXED);
 #else
-	uintptr_t temp1;
-	u_long temp2;
+	u_long temp1;
 
+#ifdef __riscv_xcheri
 	__asm __volatile(
-		"1:	clr.c	%1, %0\n"
-		"	cgetaddr %3, %1\n"
-		"	or	%3, %3, %4\n"
-		"	csetaddr %2, %1, %3\n"
-		"	csc.c	%3, %2, %0\n"
-		"	bnez	%3, 1b\n"
-		: "+A" (*p), "=&C" (old), "=&C" (temp1), "=&r" (temp2)
+		"1:	clr.c	ct0, %0\n"
+		"	mv	%2, t0\n"
+		"	or	%2, %2, %3\n"
+		"	csetaddr	%1, ct0, %2\n"
+		"	csc.c	%2, %1, %0\n"
+		"	bnez	%2, 1b\n"
+		"	cmove	%1, ct0\n"
+		: "+A" (*p), "=&C" (old), "=&r" (temp1)
 		: "r" ((ptraddr_t)mask)
-		: "memory");
+		: "ct0", "memory");
+#else
+	__asm __volatile(
+		"1:	lr.c	ct0, %0\n"
+		"	mv	%2, t0\n"
+		"	or	%2, %2, %3\n"
+		"	scaddr	%1, ct0, %2\n"
+		"	sc.c	%2, %1, %0\n"
+		"	bnez	%2, 1b\n"
+		"	cmv	%1, ct0\n"
+		: "+A" (*p), "=&C" (old), "=&r" (temp1)
+		: "r" ((ptraddr_t)mask)
+		: "ct0", "memory");
+#endif
 #endif
 
 	return ((old & mask) != 0);
