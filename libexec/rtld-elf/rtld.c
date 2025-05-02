98ad67f2abac15056f0c04ef576c37f4628ea63d
Dapeng Gao <dapeng.gao@cl.cam.ac.uk>
2025-05-02 15:12:17 +0100

    c18n: Protect trampoline creation by locks
    Ensure that _rtld_bind_lock is at least read-locked when tramp_intern is
    called.
    
    In a future commit, tramp_intern will need to record the trampoline in
    the Obj_Entry of callee so that if the callee is dlclose'd, its
    associated trampolines can be unmapped.
    
    Because the Obj_Entry is mutated, we need to ensure that a lock is held.
    A reader-lock is sufficient because concurrent insertion of trampolines
    is permitted---it is only when trampolines are removed that requires a
    writer-lock.

--- libexec/rtld-elf/rtld.c
+++ libexec/rtld-elf/rtld.c
@@ -1148,6 +1148,18 @@ _rtld(Elf_Addr *sp, func_ptr_type *exit_proc, Obj_Entry **objp)
     if (obj_enforce_relro(obj_main) == -1)
 	rtld_die();
 
+#ifdef CHERI_LIB_C18N
+    obj_main->entry = tramp_intern(NULL, RTLD_COMPART_ID,
+	&(struct tramp_data) {
+	    .target = cheri_sealentry(obj_main->entry),
+	    .defobj = obj_main,
+	    .sig = (struct func_sig) {
+		    .valid = true,
+		    .reg_args = 3, .mem_args = false, .ret_args = NONE
+	    }
+    });
+#endif
+
     lock_release(rtld_bind_lock, &lockstate);
 
     dbg("transferring control to program entry point = " PTR_FMT, obj_main->entry);
@@ -1158,19 +1170,7 @@ _rtld(Elf_Addr *sp, func_ptr_type *exit_proc, Obj_Entry **objp)
     *exit_proc = rtld_exit_ptr;
     *objp = obj_main;
 
-#ifdef CHERI_LIB_C18N
-    return ((func_ptr_type)tramp_intern(NULL, RTLD_COMPART_ID,
-	&(struct tramp_data) {
-	    .target = cheri_sealentry(obj_main->entry),
-	    .defobj = obj_main,
-	    .sig = (struct func_sig) {
-		    .valid = true,
-		    .reg_args = 3, .mem_args = false, .ret_args = NONE
-	    }
-    }));
-#else
     return ((func_ptr_type)obj_main->entry);
-#endif
 }
 
 void *
@@ -1178,9 +1178,13 @@ rtld_resolve_ifunc(const Obj_Entry *obj, const Elf_Sym *def)
 {
 	void *ptr;
 	uintptr_t target;
+#ifdef CHERI_LIB_C18N
+	RtldLockState lockstate;
+#endif
 
 	ptr = (void *)make_function_pointer(def, obj);
 #ifdef CHERI_LIB_C18N
+	rlock_acquire(rtld_bind_lock, &lockstate);
 	ptr = tramp_intern(NULL, RTLD_COMPART_ID, &(struct tramp_data) {
 		.target = ptr,
 		.defobj = obj,
@@ -1188,6 +1192,7 @@ rtld_resolve_ifunc(const Obj_Entry *obj, const Elf_Sym *def)
 		.sig = (struct func_sig) { .valid = true,
 		    .reg_args = 8, .mem_args = false, .ret_args = ONE }
 	});
+	lock_release(rtld_bind_lock, &lockstate);
 #endif
 	target = call_ifunc_resolver(ptr);
 	return ((void *)target);
@@ -4591,6 +4596,7 @@ do_dlsym(void *handle, const char *name, void *retaddr, const Ver_Entry *ve,
 		    retaddr);
 		abort();
 	    }
+	    rlock_acquire(rtld_bind_lock, &lockstate);
 	    sym = tramp_intern(NULL,
 		compart_id_for_address(obj, (Elf_Addr)retaddr),
 		&(struct tramp_data) {
@@ -4598,6 +4604,7 @@ do_dlsym(void *handle, const char *name, void *retaddr, const Ver_Entry *ve,
 		    .defobj = defobj,
 		    .def = def
 	    });
+	    lock_release(rtld_bind_lock, &lockstate);
 #endif
 	} else if (ELF_ST_TYPE(def->st_info) == STT_GNU_IFUNC) {
 	    sym = rtld_resolve_ifunc(defobj, def);
