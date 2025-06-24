0bf8ffdbd734edf664b91f0a53e786da0608c434
Mark Johnston <markj@FreeBSD.org>
2025-06-24 20:17:07 +0000

    file: Add a fd flag with O_RESOLVE_BENEATH semantics
    The O_RESOLVE_BENEATH openat(2) flag restricts name lookups such that
    they remain under the directory referenced by the dirfd.  This commit
    introduces an implicit version of the flag, FD_RESOLVE_BENEATH, stored
    in the file descriptor entry.  When the flag is set, any lookup relative
    to that fd automatically has O_RESOLVE_BENEATH semantics.  Furthermore,
    the flag is sticky, meaning that it cannot be cleared, and it is copied
    by dup() and openat().
    
    File descriptors with FD_RESOLVE_BENEATH set may not be passed to
    fchdir(2) or fchroot(2).  Various fd lookup routines are modified to
    return fd flags to the caller.
    
    This flag will be used to address a case where jails with different root
    directories and the ability to pass SCM_RIGHTS messages across the jail
    boundary can transfer directory fds in such as way as to allow a
    filesystem escape.
    
    PR:		262180
    Reviewed by:	kib
    MFC after:	3 weeks
    Differential Revision:	https://reviews.freebsd.org/D50371
    
    (cherry picked from commit f35525ff2053e026a423e852136d73ed93c95803)

--- sys/kern/vfs_extattr.c
+++ sys/kern/vfs_extattr.c
@@ -263,7 +263,7 @@ kern_extattr_set_fd(struct thread *td, int fd, int attrnamespace,
 	AUDIT_ARG_TEXT(attrname);
 
 	error = getvnode_path(td, fd,
-	    cap_rights_init_one(&rights, CAP_EXTATTR_SET), &fp);
+	    cap_rights_init_one(&rights, CAP_EXTATTR_SET), NULL, &fp);
 	if (error)
 		return (error);
 
@@ -461,7 +461,7 @@ kern_extattr_get_fd(struct thread *td, int fd, int attrnamespace,
 	AUDIT_ARG_TEXT(attrname);
 
 	error = getvnode_path(td, fd,
-	    cap_rights_init_one(&rights, CAP_EXTATTR_GET), &fp);
+	    cap_rights_init_one(&rights, CAP_EXTATTR_GET), NULL, &fp);
 	if (error)
 		return (error);
 
@@ -626,7 +626,7 @@ kern_extattr_delete_fd(struct thread *td, int fd, int attrnamespace,
 	AUDIT_ARG_TEXT(attrname);
 
 	error = getvnode_path(td, fd,
-	    cap_rights_init_one(&rights, CAP_EXTATTR_DELETE), &fp);
+	    cap_rights_init_one(&rights, CAP_EXTATTR_DELETE), NULL, &fp);
 	if (error)
 		return (error);
 
@@ -800,7 +800,7 @@ kern_extattr_list_fd(struct thread *td, int fd, int attrnamespace,
 	AUDIT_ARG_FD(fd);
 	AUDIT_ARG_VALUE(attrnamespace);
 	error = getvnode_path(td, fd,
-	    cap_rights_init_one(&rights, CAP_EXTATTR_LIST), &fp);
+	    cap_rights_init_one(&rights, CAP_EXTATTR_LIST), NULL, &fp);
 	if (error)
 		return (error);
 
