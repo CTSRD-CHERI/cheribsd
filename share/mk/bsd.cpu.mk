2ed2f056eff4adc2ddbee427778cb0d364d230ba
Dapeng Gao <dapeng.gao@cl.cam.ac.uk>
2025-06-24 16:37:15 +0100

    riscv: Build kernel and world bounded variadic arguments

--- share/mk/bsd.cpu.mk
+++ share/mk/bsd.cpu.mk
@@ -334,6 +334,15 @@ MACHINE_CPU += riscv
 . endif
 .endif
 
+.if ${MACHINE_CPUARCH} == "riscv"
+. if ${MACHINE_CPU:Mcheri}
+CFLAGS+=	-Xclang -target-feature -Xclang +cheri-bounded-vararg
+CFLAGS+=	-Xclang -target-feature -Xclang +cheri-bounded-memarg-caller
+CFLAGS+=	-Xclang -target-feature -Xclang +cheri-bounded-memarg-callee
+LDFLAGS+=	-z no-cheri-riscv-jump-slot
+. endif
+.endif
+
 .if ${MACHINE_CPUARCH} == "aarch64"
 . if ${MACHINE_CPU:Mcheri}
 CFLAGS+=	-march=morello
