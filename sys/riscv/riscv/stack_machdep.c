de657f7012ec94f587149921e394d4dc20632a6a
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2025-07-30 00:54:57 +0100

    cheri-rv64y: Re-introduce Xcheri inline assembly for machdep.

--- sys/riscv/riscv/stack_machdep.c
+++ sys/riscv/riscv/stack_machdep.c
@@ -84,7 +84,11 @@ stack_save(struct stack *st)
 	uintptr_t sp;
 
 #ifdef __CHERI_PURE_CAPABILITY__
+#ifdef __riscv_xcheri
+	__asm __volatile("cmove %0, csp" : "=&C" (sp));
+#else
 	__asm __volatile("cmv %0, csp" : "=&C" (sp));
+#endif
 #else
 	__asm __volatile("mv %0, sp" : "=&r" (sp));
 #endif
