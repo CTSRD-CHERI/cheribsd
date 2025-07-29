3c1f3cb3374fdddc84b8f2fb85187f5209474e6d
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2025-07-30 00:54:57 +0100

    cheri-rv64y: Re-introduce Xcheri inline assembly for machdep.

--- sys/riscv/riscv/mp_machdep.c
+++ sys/riscv/riscv/mp_machdep.c
@@ -172,7 +172,11 @@ init_secondary(uint64_t hart)
 	/* Setup the pcpu pointer */
 	pcpup = &__pcpu[cpuid];
 #ifdef __CHERI_PURE_CAPABILITY__
+#ifdef __riscv_xcheri
+        __asm __volatile("cmove ctp, %0" :: "C"(pcpup));
+#else
 	__asm __volatile("cmv ctp, %0" :: "C"(pcpup));
+#endif
 #else
 	__asm __volatile("mv tp, %0" :: "r"(pcpup));
 #endif
