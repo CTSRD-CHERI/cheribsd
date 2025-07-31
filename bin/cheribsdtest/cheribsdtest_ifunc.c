bc360a24b58eb26af231e58655c4ad4231b69ba3
Jessica Clarke <jrtc27@jrtc27.com>
2025-07-31 22:25:41 +0100

    cheribsdtest: Enable IFUNC tests for RISC-V
    These should all work for hybrid binaries, and the dynamic-only ones
    should now work for purecap binaries; the others require support for
    IRELATIVE-style caprelocs which is not currently present, so are
    disabled for now.

--- bin/cheribsdtest/cheribsdtest_ifunc.c
+++ bin/cheribsdtest/cheribsdtest_ifunc.c
@@ -33,6 +33,8 @@
 
 #include "cheribsdtest.h"
 
+/* TODO: Support IRELATIVE-style caprelocs */
+#if !defined(__riscv) || !defined(__CHERI_PURE_CAPABILITY__)
 static int
 simple_ifunc_impl(void)
 {
@@ -84,6 +86,7 @@ CHERIBSDTEST(global_data_ifunc_fptr,
 
 	cheribsdtest_success();
 }
+#endif
 #endif
 
 #ifdef CHERIBSD_DYNAMIC_TESTS
