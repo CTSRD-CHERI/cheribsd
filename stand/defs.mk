0125d073c07f72f49cebdb5e140c2d949bea5317
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2025-08-07 01:06:05 +0100

    cheri risc-v: Pass the right -mabi flag in the bootlader linker flags.
    The bootloader is built in hybrid mode because purecap support is not
    implemented. Morello already propagates -mabi=aapcs for both CFLAGS
    and LDFLAGS. This does the same for RISC-V.

--- stand/defs.mk
+++ stand/defs.mk
@@ -162,6 +162,7 @@ LDFLAGS+=	-march=morello+noa64c -mabi=aapcs
 CFLAGS+=	-march=rv64imac -mabi=lp64 -fPIC
 CFLAGS.clang+=	-mcmodel=medium
 CFLAGS.gcc+=	-mcmodel=medany
+LDFLAGS+=	-march=rv64imac -mabi=lp64 -fPIC
 .else
 CFLAGS+=	-msoft-float
 .endif
