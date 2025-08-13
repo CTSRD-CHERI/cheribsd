2bc4ca8904891d2c9b2f81f895937cd13fcf089a
Dapeng Gao <dapeng.gao@cl.cam.ac.uk>
2025-08-13 16:57:22 +0100

    cheri-rv64y: Add support for utidc
    Also add a sysarch(2) command for setting the utidc.

--- sys/riscv/riscv/sys_machdep.c
+++ sys/riscv/riscv/sys_machdep.c
@@ -72,6 +72,15 @@ sysarch(struct thread *td, struct sysarch_args *uap)
 			td->td_md.md_flags &= ~MDTD_QTRACE_USERMODE;
 		return (0);
 #endif
+#if __has_feature(capabilities)
+	case RISCV_SET_UTIDC: {
+		uintcap_t val;
+		int error = fuecap(uap->parms, &val);
+		if (!error)
+			scr_write(utidc, val);
+		return (error);
+	}
+#endif
 	default:
 		break;
 	}
