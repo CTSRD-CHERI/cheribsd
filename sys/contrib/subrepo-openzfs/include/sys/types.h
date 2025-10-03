35f41d2ce29b8fd33d311ba9730a5ac4243d0a94
Brooks Davis <brooks@one-eyed-alien.net>
2025-10-03 12:46:55 +0100

    openzfs: simply define ptraddr_t to size_t
    We don't have a defintion guard for __ptraddr_t and indentical
    redefinitions aren't allowed in C99 to just define ptraddr_t as size_t
    if needed.  This will be fine on any architecture OpenZFS supports.

--- sys/contrib/subrepo-openzfs/include/sys/types.h
+++ sys/contrib/subrepo-openzfs/include/sys/types.h
@@ -1,12 +1,7 @@
 #include_next <sys/types.h>
 
 #ifndef _PTRADDR_T_DECLARED
-#ifdef __PTRADDR_TYPE__
-typedef __PTRADDR_TYPE__        __ptraddr_t;
-#else
-typedef size_t     __ptraddr_t;
-#endif
-typedef __ptraddr_t             ptraddr_t;
+typedef	size_t			ptraddr_t;
 #define	_PTRADDR_T_DECLARED
 #endif
 
