18fac904e5e6a30e00659c01e970c6ab218dc6af
Brooks Davis <brooks@one-eyed-alien.net>
2025-10-21 14:38:59 +0100

    bsd.compat.mk: make lib64 legacy-only
    Remove "cheri" from MACHINE_CPU for lib64 since we're not planning to
    support hybrid on FreeBSD.  Otherwise, add it on 64C and pass it through
    elsewhere (I'm assuming we only care about 64CB on normal morello
    systems.)

--- share/mk/bsd.compat.mk
+++ share/mk/bsd.compat.mk
@@ -100,6 +100,7 @@ LIB32_MACHINE_ABI+=	time32
 .else
 LIB32_MACHINE_ABI+=	time64
 .endif
+LIB32_MACHINE_CPU=	${MACHINE_CPU}
 .endif # ${MK_LIB32} != "no"
 
 # -------------------------------------------------------------------
@@ -109,11 +110,11 @@ LIB32_MACHINE_ABI+=	time64
 HAS_COMPAT+=	64
 LIB64_MACHINE=	arm64
 LIB64_MACHINE_ARCH=aarch64
-LIB64WMAKEENV=	MACHINE_CPU="arm64 cheri"
-LIB64WMAKEFLAGS= LD="${XLD}" CPUTYPE=morello
+LIB64WMAKEENV=	MACHINE_CPU="arm64"
+LIB64WMAKEFLAGS= LD="${XLD}" CPUTYPE=
 # XXX: clang specific
 LIB64CPUFLAGS=	-target aarch64-unknown-freebsd${OS_REVISION}
-LIB64CPUFLAGS+=	-march=morello -mabi=aapcs
+LIB64CPUFLAGS+=	-mabi=aapcs -march=armv8-a
 .endif
 
 .if ${COMPAT_ARCH:Mriscv*c*}
@@ -121,20 +122,8 @@ HAS_COMPAT+=	64
 LIB64_RISCV_ABI=	lp64d
 LIB64_MACHINE=	riscv
 LIB64_MACHINE_ARCH=riscv64
-.if ${LIB64CPUTYPE} == "cheri"
-.if 0
-.warning "CPUTYPE=cheri is deprecated, please use xcheri or rvy"
-.endif
-LIB64_RISCV_CHERI_TYPE=	xcheri
-.elif empty(LIB64CPUTYPE)
-LIB64_RISCV_CHERI_TYPE=	xcheri
-.elif ${LIB64CPUTYPE} == "xcheri" || ${LIB64CPUTYPE} == "rvy"
-LIB64_RISCV_CHERI_TYPE=	${LIB64CPUTYPE}
-.else
-.error "Invalid CHERI variant ${LIB64CPUTYPE}"
-.endif
-LIB64WMAKEENV=	MACHINE_CPU="riscv cheri ${LIB64_RISCV_CHERI_TYPE}"
-LIB64WMAKEFLAGS= LD="${XLD}" CPUTYPE=${LIB64_RISCV_CHERI_TYPE}
+LIB64WMAKEENV=	MACHINE_CPU="riscv"
+LIB64WMAKEFLAGS= LD="${XLD}" CPUTYPE=
 # XXX: clang specific
 LIB64CPUFLAGS=	-target riscv64-unknown-freebsd${OS_REVISION}
 LIB64CPUFLAGS+=	-march=${LIB64_RISCV_MARCH} -mabi=${LIB64_RISCV_ABI}
@@ -144,6 +133,7 @@ LIB64WMAKEFLAGS+= NM="${XNM}" OBJCOPY="${XOBJCOPY}"
 
 LIB64DTRACE=	${DTRACE} -64
 LIB64_MACHINE_ABI=	${MACHINE_ABI:Npurecap:Nptr*} ptr64
+LIB64_MACHINE_CPU=	${MACHINE_CPU:Ncheri}
 .endif # ${MK_LIB64} != "no"
 
 # -------------------------------------------------------------------
@@ -197,6 +187,7 @@ LIB${_LIBCOMPAT}_RISCV_MARCH:=	${LIB${_LIBCOMPAT}_RISCV_MARCH}zcherihybrid_zcher
 # Common CHERI flags
 .if defined(HAS_COMPAT) && ${HAS_COMPAT:M64C}
 LIB64C_MACHINE_ABI=	${MACHINE_ABI:Nptr*} purecap ptr128c
+LIB64C_MACHINE_CPU=	${MACHINE_CPU} cheri
 
 # This duplicates some logic in bsd.cpu.mk that is needed for the
 # WANT_COMPAT/NEED_COMPAT case.
@@ -230,6 +221,7 @@ LIB64CCFLAGS+=	-mllvm -cheri-subobject-bounds-clear-swperm=2
 HAS_COMPAT+=	64CB
 LIB64CB_MACHINE=	arm64
 LIB64CB_MACHINE_ARCH=aarch64cb
+LIB64CB_MACHINE_CPU=	${MACHINE_CPU}
 LIB64CBCPUFLAGS=	-target aarch64-unknown-freebsd${OS_REVISION}
 LIB64CBCPUFLAGS+=	-march=morello -mabi=purecap-benchmark
 LIB64CB_MACHINE_ABI=	${MACHINE_ABI:Nptr*:Npurecap} purecap ptr128c benchmark
@@ -295,7 +287,7 @@ libcompats=	${_LIBCOMPATS:tl}
 # Update MACHINE and MACHINE_ARCH so they can be used in bsd.opts.mk via
 # bsd.compiler.mk
 .if defined(USE_COMPAT)
-_LIBCOMPAT_MAKEVARS=	_MACHINE _MACHINE_ARCH _MACHINE_ABI
+_LIBCOMPAT_MAKEVARS=	_MACHINE _MACHINE_ARCH _MACHINE_ABI _MACHINE_CPU
 .for _var in ${_LIBCOMPAT_MAKEVARS}
 .if !empty(LIB${USE_COMPAT}${_var})
 LIBCOMPAT${_var}?=	${LIB${USE_COMPAT}${_var}}
@@ -305,6 +297,7 @@ LIBCOMPAT${_var}?=	${LIB${USE_COMPAT}${_var}}
 MACHINE:=	${LIBCOMPAT_MACHINE}
 MACHINE_ARCH:=	${LIBCOMPAT_MACHINE_ARCH}
 MACHINE_ABI:=	${LIBCOMPAT_MACHINE_ABI}
+MACHINE_CPU:=	${LIBCOMPAT_MACHINE_CPU}
 .endif
 
 .if !defined(COMPAT_COMPILER_TYPE)
