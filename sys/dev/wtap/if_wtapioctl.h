a32b372aa99d41768db71a24dc8e005dcea491fc
Brooks Davis <brooks@one-eyed-alien.net>
2025-11-05 23:37:01 +0000

    Revert "Renumber some stats ioctls for ath(4)."
    This commit addressed a problem on 256-bit CHERI which no longer exists.
    
    This reverts commit 424d41bceb9dd5d2c3d785c537f0ab38943885ab.

--- sys/dev/wtap/if_wtapioctl.h
+++ sys/dev/wtap/if_wtapioctl.h
@@ -43,8 +43,8 @@
 #include <sys/param.h>
 #include <net80211/ieee80211_radiotap.h>
 
-#define	SIOCGATHSTATS	_IOWR('i', 237, struct ifreq)
-#define	SIOCZATHSTATS	_IOWR('i', 239, struct ifreq)
+#define	SIOCGATHSTATS	_IOWR('i', 137, struct ifreq)
+#define	SIOCZATHSTATS	_IOWR('i', 139, struct ifreq)
 
 #define WTAPIOCTLCRT	_IOW('W', 1, int)
 #define WTAPIOCTLDEL	_IOW('W', 2, int)
@@ -177,12 +177,3 @@ struct wtap_tx_radiotap_header {
 } __packed;
 
 #endif
-// CHERI CHANGES START
-// {
-//   "updated": 20230509,
-//   "target_type": "header",
-//   "changes": [
-//     "ioctl:net"
-//   ]
-// }
-// CHERI CHANGES END
