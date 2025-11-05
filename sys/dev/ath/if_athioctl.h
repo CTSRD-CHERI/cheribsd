ae6b540300dd31eda7f2dc3e23deb68b9683a46c
Brooks Davis <brooks@one-eyed-alien.net>
2025-11-05 20:49:08 +0000

    ath: restore caddr_t use

--- sys/dev/ath/if_athioctl.h
+++ sys/dev/ath/if_athioctl.h
@@ -191,8 +191,8 @@ struct ath_diag {
 #define	ATH_DIAG_OUT	0x0000		/* copy out results (always) */
 #define	ATH_DIAG_ID	0x0fff
 	u_int16_t ad_in_size;		/* pack to fit, yech */
-	char *ad_in_data;
-	char *ad_out_data;
+	caddr_t ad_in_data;
+	caddr_t ad_out_data;
 	u_int	ad_out_size;
 
 };
@@ -240,7 +240,7 @@ struct ath_rateioctl {
 		uint64_t	pad;
 	} is_u;
 	uint32_t		len;
-	char *	buf;
+	caddr_t			buf;
 };
 #define	SIOCGATHNODERATESTATS	_IOWR('i', 149, struct ath_rateioctl)
 #define	SIOCGATHRATESTATS	_IOWR('i', 150, struct ath_rateioctl)
