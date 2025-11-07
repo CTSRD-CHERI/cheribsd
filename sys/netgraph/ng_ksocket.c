32f01efe51907f416aa4ed85ee7a5723ec832f77
Brooks Davis <brooks@one-eyed-alien.net>
2025-11-07 23:38:24 +0000

    sys/netgraph: reduce diffs

--- sys/netgraph/ng_ksocket.c
+++ sys/netgraph/ng_ksocket.c
@@ -828,7 +828,7 @@ ng_ksocket_rcvmsg(node_p node, item_p item, hook_p lasthook)
 			sopt.sopt_td = NULL;
 			sopt.sopt_valsize = NG_KSOCKET_MAX_OPTLEN;
 			ksopt = (struct ng_ksocket_sockopt *)resp->data;
-			sopt.sopt_val = &ksopt->value[0];
+			sopt.sopt_val = ksopt->value;
 			if ((error = sogetopt(so, &sopt)) != 0) {
 				NG_FREE_MSG(resp);
 				break;
@@ -857,7 +857,7 @@ ng_ksocket_rcvmsg(node_p node, item_p item, hook_p lasthook)
 			sopt.sopt_dir = SOPT_SET;
 			sopt.sopt_level = ksopt->level;
 			sopt.sopt_name = ksopt->name;
-			sopt.sopt_val = &ksopt->value[0];
+			sopt.sopt_val = ksopt->value;
 			sopt.sopt_valsize = valsize;
 			sopt.sopt_td = NULL;
 			error = sosetopt(so, &sopt);
@@ -1291,12 +1291,3 @@ ng_ksocket_parse(const struct ng_ksocket_alias *aliases,
 		return (-1);
 	return (val);
 }
-// CHERI CHANGES START
-// {
-//   "updated": 20230509,
-//   "target_type": "kernel",
-//   "changes": [
-//     "user_capabilities"
-//   ]
-// }
-// CHERI CHANGES END
