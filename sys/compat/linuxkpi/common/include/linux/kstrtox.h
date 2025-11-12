90d1f50b7a4c675fce0d91af0180f4f0f0b6368c
Brooks Davis <brooks@one-eyed-alien.net>
2025-11-12 09:24:10 +0000

    sys/compat: assorted diff reduction

--- sys/compat/linuxkpi/common/include/linux/kstrtox.h
+++ sys/compat/linuxkpi/common/include/linux/kstrtox.h
@@ -255,8 +255,7 @@ kstrtobool(const char *s, bool *res)
 }
 
 static inline int
-kstrtobool_from_user(const char __user *s, size_t count,
-    bool *res)
+kstrtobool_from_user(const char __user *s, size_t count, bool *res)
 {
 	char buf[8] = {};
 
@@ -270,8 +269,8 @@ kstrtobool_from_user(const char __user *s, size_t count,
 }
 
 static inline int
-kstrtoint_from_user(const char __user *s, size_t count,
-    unsigned int base, int *p)
+kstrtoint_from_user(const char __user *s, size_t count, unsigned int base,
+    int *p)
 {
 	char buf[36] = {};
 
@@ -285,8 +284,8 @@ kstrtoint_from_user(const char __user *s, size_t count,
 }
 
 static inline int
-kstrtouint_from_user(const char __user *s, size_t count,
-    unsigned int base, unsigned int *p)
+kstrtouint_from_user(const char __user *s, size_t count, unsigned int base,
+    unsigned int *p)
 {
 	char buf[36] = {};
 
@@ -300,16 +299,16 @@ kstrtouint_from_user(const char __user *s, size_t count,
 }
 
 static inline int
-kstrtou32_from_user(const char __user *s, size_t count,
-    unsigned int base, unsigned int *p)
+kstrtou32_from_user(const char __user *s, size_t count, unsigned int base,
+    unsigned int *p)
 {
 
 	return (kstrtouint_from_user(s, count, base, p));
 }
 
 static inline int
-kstrtou8_from_user(const char __user *s, size_t count,
-    unsigned int base, uint8_t *p)
+kstrtou8_from_user(const char __user *s, size_t count, unsigned int base,
+    uint8_t *p)
 {
 	char buf[8] = {};
 
