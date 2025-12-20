041a53f58ea63f0901ff3096ddd56e99789ff800
huangFF0904 <625173hhh@gmail.com>
2025-12-20 17:35:06 +0800

    Fix a security issue in radius_get_vendor_attr().

--- lib/libradius/radlib.c
+++ lib/libradius/radlib.c
@@ -1364,14 +1364,23 @@ split(char *str, char *fields[], int maxfields, char *msg, size_t msglen)
 }
 
 int
-rad_get_vendor_attr(u_int32_t *vendor, const void **data, size_t *len)
+rad_get_vendor_attr(u_int32_t *vendor, unsigned char *type, const void **data, size_t *len, const void *raw, size_t raw_len)
 {
 	struct vendor_attribute *attr;
 
-	attr = (struct vendor_attribute *)*data;
+	if (raw_len < sizeof(struct vendor_attribute)) {
+		return -1;
+	}
+
+	attr = (struct vendor_attribute *) raw;
 	*vendor = ntohl(attr->vendor_value);
+	*type = attr->attrib_type;
 	*data = attr->attrib_data;
 	*len = attr->attrib_len - 2;
+
+	if ((attr->attrib_len + 4) > raw_len) {
+		return -1;
+	}
 
 	return (attr->attrib_type);
 }
