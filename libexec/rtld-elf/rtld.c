7c1cafe7c7f1684cc8c85c63bb8b617bd86bb2c0
John Baldwin <jhb@FreeBSD.org>
2026-01-08 13:44:10 -0500

    rtld: Reject DT_INIT and DT_FINI for purecap more nicely
    Rather than exiting the entire process at the point of invoking _init
    or _fini, reject loading of objects with DT_INIT or DT_FINI tags at
    load time.

--- libexec/rtld-elf/rtld.c
+++ libexec/rtld-elf/rtld.c
@@ -1901,9 +1901,15 @@ digest_dynamic1(Obj_Entry *obj, int early, const Elf_Dyn **dyn_rpath,
 			break;
 
 		case DT_INIT:
+#ifdef __CHERI_PURE_CAPABILITY__
+			_rtld_error("%s: _init not supported for CheriABI",
+			    obj->path);
+			return (false);
+#else
 			obj->init = (uintptr_t)(obj->relocbase +
 			    dynp->d_un.d_ptr);
 			break;
+#endif
 
 		case DT_PREINIT_ARRAY:
 			obj->preinit_array = (uintptr_t *)(obj->relocbase +
@@ -1926,9 +1932,15 @@ digest_dynamic1(Obj_Entry *obj, int early, const Elf_Dyn **dyn_rpath,
 			break;
 
 		case DT_FINI:
+#ifdef __CHERI_PURE_CAPABILITY__
+			_rtld_error("%s: _fini not supported for CheriABI",
+			    obj->path);
+			return (false);
+#else
 			obj->fini = (uintptr_t)(obj->relocbase +
 			    dynp->d_un.d_ptr);
 			break;
+#endif
 
 		case DT_FINI_ARRAY:
 			obj->fini_array = (uintptr_t *)(obj->relocbase +
