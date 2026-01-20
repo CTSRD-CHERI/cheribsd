CTSRD-CHERI/cheribsd

commit 63d744d76fa697d07c356fe84b0c458d9240bdba

Review and update most CHERI changes annotations.

contrib/subrepo-cheri-libc++/include/locale
@@ -12,7 +12,7 @@
 
 // CHERI CHANGES START
 // {
-//   "updated": 20190722,
+//   "updated": 20221129,
 //   "target_type": "header",
 //   "changes": [
 //     "subobject_bounds"

contrib/subrepo-cheri-libc++/include/regex
@@ -12,7 +12,7 @@
 
 // CHERI CHANGES START
 // {
-//   "updated": 20190509,
+//   "updated": 20221129,
 //   "target_type": "header",
 //   "changes": [
 //     "subobject_bounds"

contrib/subrepo-cheri-libc++/include/string
@@ -13,7 +13,7 @@
 /*
  * CHERI CHANGES START
  * {
- *   "updated": 20190426,
+ *   "updated": 20221129,
  *   "target_type": "header",
  *   "changes": [
  *     "subobject_bounds"

contrib/subrepo-cheri-libc++/src/filesystem/operations.cpp
@@ -8,7 +8,7 @@
 
 // CHERI CHANGES START
 // {
-//   "updated": 20190429,
+//   "updated": 20221129,
 //   "target_type": "lib",
 //   "changes": [
 //     "subobject_bounds"

contrib/subrepo-cheri-libc++/src/string.cpp
@@ -8,7 +8,7 @@
 
 // CHERI CHANGES START
 // {
-//   "updated": 20190426,
+//   "updated": 20221129,
 //   "target_type": "lib",
 //   "changes": [
 //     "subobject_bounds"

contrib/subrepo-cheri-libc++/test/support/platform_support.h
@@ -10,7 +10,7 @@
 /*
  * CHERI CHANGES START
  * {
- *   "updated": 20190426,
+ *   "updated": 20221129,
  *   "target_type": "header",
  *   "changes": [
  *     "subobject_bounds"

commit a12ed3fe7b9f00b6f52bb1a5deae0d9d4be67c1d

contrib/subrepo-cheri-libc++: Drop downstream CHERI CHANGES diff
These files are vendored so we shouldn't be touching the annotations
here as a downstream diff. If updating these is important, do it in
CHERI LLVM and loop that back via a git subrepo pull.

Fixes: 63d744d76fa6 ("Review and update most CHERI changes annotations.")

contrib/subrepo-cheri-libc++/include/locale
@@ -12,7 +12,7 @@
 
 // CHERI CHANGES START
 // {
-//   "updated": 20221129,
+//   "updated": 20190722,
 //   "target_type": "header",
 //   "changes": [
 //     "subobject_bounds"

contrib/subrepo-cheri-libc++/include/regex
@@ -12,7 +12,7 @@
 
 // CHERI CHANGES START
 // {
-//   "updated": 20221129,
+//   "updated": 20190509,
 //   "target_type": "header",
 //   "changes": [
 //     "subobject_bounds"

contrib/subrepo-cheri-libc++/include/string
@@ -13,7 +13,7 @@
 /*
  * CHERI CHANGES START
  * {
- *   "updated": 20221129,
+ *   "updated": 20190426,
  *   "target_type": "header",
  *   "changes": [
  *     "subobject_bounds"

contrib/subrepo-cheri-libc++/src/filesystem/operations.cpp
@@ -8,7 +8,7 @@
 
 // CHERI CHANGES START
 // {
-//   "updated": 20221129,
+//   "updated": 20190429,
 //   "target_type": "lib",
 //   "changes": [
 //     "subobject_bounds"

contrib/subrepo-cheri-libc++/src/filesystem/time_utils.h
@@ -35,7 +35,7 @@
 
 // CHERI CHANGES START
 // {
-//   "updated": 20221129,
+//   "updated": 20190426,
 //   "target_type": "lib",
 //   "changes": [
 //     "subobject_bounds"

contrib/subrepo-cheri-libc++/src/string.cpp
@@ -8,7 +8,7 @@
 
 // CHERI CHANGES START
 // {
-//   "updated": 20221129,
+//   "updated": 20190426,
 //   "target_type": "lib",
 //   "changes": [
 //     "subobject_bounds"

contrib/subrepo-cheri-libc++/test/support/platform_support.h
@@ -10,7 +10,7 @@
 /*
  * CHERI CHANGES START
  * {
- *   "updated": 20221129,
+ *   "updated": 20190426,
  *   "target_type": "header",
  *   "changes": [
  *     "subobject_bounds"