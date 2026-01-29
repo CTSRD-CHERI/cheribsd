83a6e72f6e1f99ac249351effbb96b4ffb29adc0
Mark Johnston <markj@FreeBSD.org>
2026-01-29 19:25:32 +0000

    jemalloc: Use a local variable for the thread-specific data
    Just a nit I noticed while reading code.  No functional change intended.  No
    changes to generated code.

--- contrib/jemalloc/src/jemalloc.c
+++ contrib/jemalloc/src/jemalloc.c
@@ -2880,7 +2880,7 @@ bool free_fastpath(void *ptr, size_t size, bool size_hint) {
 	cache_bin_t *bin = tcache_small_bin_get(tcache, alloc_ctx.szind);
 	cache_bin_info_t *bin_info = &tcache_bin_info[alloc_ctx.szind];
 	if (!cache_bin_dalloc_easy(bin, bin_info,
-	    unbound_ptr(tsd_tsdn(tsd_get(false)), ptr))) {
+	    unbound_ptr(tsd_tsdn(tsd), ptr))) {
 		return false;
 	}
 
