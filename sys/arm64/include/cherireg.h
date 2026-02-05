357f14f0b6276953b41e725c47eb6772edfa1e13
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2026-02-05 20:11:09 +0000

    cheri: Rewowrk for CHERI_PERM bits definition in the kernel.
    Note that while this should be API-compatible with cheriintrin.h, we do not
    use an enum for these definitions to allow use from assembly.
    
    Define additional macros to detect presence of architecture-specific permission bits.
    These macros are non-portable and are exposed to userland to be
    used by libc and rtld if necessary.

--- sys/arm64/include/cherireg.h
+++ sys/arm64/include/cherireg.h
@@ -42,39 +42,45 @@
  * CHERI ISA-defined constants for capabilities -- suitable for inclusion from
  * assembly source code.
  */
-#define	CHERI_PERM_GLOBAL			(1 << 0)	/* 0x00000001 */
-#define	CHERI_PERM_EXECUTIVE			(1 << 1)	/* 0x00000002 */
-#define	CHERI_PERM_SW0				(1 << 2)	/* 0x00000004 */
-#define	CHERI_PERM_SW1				(1 << 3)	/* 0x00000008 */
-#define	CHERI_PERM_SW2				(1 << 4)	/* 0x00000010 */
-#define	CHERI_PERM_SW3				(1 << 5)	/* 0x00000020 */
-#define	CHERI_PERM_MUTABLE_LOAD			(1 << 6)	/* 0x00000040 */
-#define	CHERI_PERM_COMPARTMENT_ID		(1 << 7)	/* 0x00000080 */
-#define	CHERI_PERM_BRANCH_SEALED_PAIR		(1 << 8)	/* 0x00000100 */
-#define	CHERI_PERM_INVOKE			CHERI_PERM_BRANCH_SEALED_PAIR
-#define	CHERI_PERM_SYSTEM			(1 << 9)	/* 0x00000200 */
-#define	CHERI_PERM_SYSTEM_REGS			CHERI_PERM_SYSTEM
-#define	CHERI_PERM_UNSEAL			(1 << 10)	/* 0x00000400 */
-#define	CHERI_PERM_SEAL				(1 << 11)	/* 0x00000800 */
-#define	CHERI_PERM_STORE_LOCAL_CAP		(1 << 12)	/* 0x00001000 */
-#define	CHERI_PERM_STORE_CAP			(1 << 13)	/* 0x00002000 */
-#define	CHERI_PERM_LOAD_CAP			(1 << 14)	/* 0x00004000 */
-#define	CHERI_PERM_EXECUTE			(1 << 15)	/* 0x00008000 */
-#define	CHERI_PERM_STORE			(1 << 16)	/* 0x00010000 */
-#define	CHERI_PERM_LOAD				(1 << 17)	/* 0x00020000 */
+#define	CHERI_PERM_GLOBAL		(1 << 0)	/* 0x00000001 */
+#define	CHERI_PERM_EXECUTIVE		(1 << 1)	/* 0x00000002 */
+#define	CHERI_PERM_SW0			(1 << 2)	/* 0x00000004 */
+#define	CHERI_PERM_SW1			(1 << 3)	/* 0x00000008 */
+#define	CHERI_PERM_SW2			(1 << 4)	/* 0x00000010 */
+#define	CHERI_PERM_SW3			(1 << 5)	/* 0x00000020 */
+#define	CHERI_PERM_MUTABLE_LOAD		(1 << 6)	/* 0x00000040 */
+#define	CHERI_PERM_COMPARTMENT_ID	(1 << 7)	/* 0x00000080 */
+#define	CHERI_PERM_BRANCH_SEALED_PAIR	(1 << 8)	/* 0x00000100 */
+#define	CHERI_PERM_SYSTEM		(1 << 9)	/* 0x00000200 */
+#define	CHERI_PERM_UNSEAL		(1 << 10)	/* 0x00000400 */
+#define	CHERI_PERM_SEAL			(1 << 11)	/* 0x00000800 */
+#define	CHERI_PERM_STORE_LOCAL_CAP	(1 << 12)	/* 0x00001000 */
+#define	CHERI_PERM_STORE_CAP		(1 << 13)	/* 0x00002000 */
+#define	CHERI_PERM_LOAD_CAP		(1 << 14)	/* 0x00004000 */
+#define	CHERI_PERM_EXECUTE		(1 << 15)	/* 0x00008000 */
+#define	CHERI_PERM_STORE		(1 << 16)	/* 0x00010000 */
+#define	CHERI_PERM_LOAD			(1 << 17)	/* 0x00020000 */
+
+#define	CHERI_PERM_INVOKE	CHERI_PERM_BRANCH_SEALED_PAIR
+#define	CHERI_PERM_SYSTEM_REGS	CHERI_PERM_SYSTEM
 #else
 /*
  * These should be defined in cheriintrin.h, but aren't yet.
  */
-#define	CHERI_PERM_EXECUTIVE			(1 << 1)	/* 0x00000002 */
-#define	CHERI_PERM_SW0				(1 << 2)	/* 0x00000004 */
-#define	CHERI_PERM_SW1				(1 << 3)	/* 0x00000008 */
-#define	CHERI_PERM_SW2				(1 << 4)	/* 0x00000010 */
-#define	CHERI_PERM_SW3				(1 << 5)	/* 0x00000020 */
-#define	CHERI_PERM_MUTABLE_LOAD			(1 << 6)	/* 0x00000040 */
-#define	CHERI_PERM_BRANCH_SEALED_PAIR		(1 << 8)	/* 0x00000100 */
+#define	CHERI_PERM_EXECUTIVE		(1 << 1)	/* 0x00000002 */
+#define	CHERI_PERM_SW0			(1 << 2)	/* 0x00000004 */
+#define	CHERI_PERM_SW1			(1 << 3)	/* 0x00000008 */
+#define	CHERI_PERM_SW2			(1 << 4)	/* 0x00000010 */
+#define	CHERI_PERM_SW3			(1 << 5)	/* 0x00000020 */
+#define	CHERI_PERM_MUTABLE_LOAD		(1 << 6)	/* 0x00000040 */
+#define	CHERI_PERM_BRANCH_SEALED_PAIR	(1 << 8)	/* 0x00000100 */
 #define	CHERI_PERM_INVOKE CHERI_PERM_BRANCH_SEALED_PAIR
 #endif
+
+/* Supported architecture permission bits feature flags */
+#define	HAS_CHERI_PERM_LOAD_STORE_CAP
+#define	HAS_CHERI_PERM_LOAD_MUTABLE
+#define	HAS_CHERI_PERM_EXECUTIVE
 
 /*
  * Macros defining initial permission sets:
