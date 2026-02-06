CTSRD-CHERI/cheribsd

commit cc81830701076706ee30e52071831082635a6840

rtld-elf: Support CHERI TGOT TLS on Morello

libexec/rtld-elf/aarch64/reloc.c
@@ -59,14 +59,26 @@
  * This is not the correct prototype, but we only need it for
  * a function pointer to a simple asm function.
  */
+#ifndef TLS_TGOT
 void *_rtld_tlsdesc_static(void *);
 void *_rtld_tlsdesc_undef(void *);
 void *_rtld_tlsdesc_dynamic(void *);
+#endif
+#ifdef TLS_TGOT
+void *_rtld_tgot_tlsdesc_static(void *);
+void *_rtld_tgot_tlsdesc_dynamic(void *);
+#endif
 
 void (*rtld_bind_start_fptr)(void) = &_rtld_bind_start;
+#ifndef TLS_TGOT
 void *(*rtld_tlsdesc_static_fptr)(void *) = &_rtld_tlsdesc_static;
 void *(*rtld_tlsdesc_undef_fptr)(void *) = &_rtld_tlsdesc_undef;
 void *(*rtld_tlsdesc_dynamic_fptr)(void *) = &_rtld_tlsdesc_dynamic;
+#endif
+#ifdef TLS_TGOT
+void *(*rtld_tgot_tlsdesc_static_fptr)(void *) = &_rtld_tgot_tlsdesc_static;
+void *(*rtld_tgot_tlsdesc_dynamic_fptr)(void *) = &_rtld_tgot_tlsdesc_dynamic;
+#endif
 
 bool
 arch_digest_dynamic(struct Struct_Obj_Entry *obj, const Elf_Dyn *dynp)
@@ -322,6 +334,7 @@ do_copy_relocations(Obj_Entry *dstobj)
 	return (0);
 }
 
+#ifndef TLS_TGOT
 struct tls_data {
 	uintptr_t	dtv_gen;
 	int		tls_index;
@@ -414,6 +427,52 @@ reloc_tlsdesc(const Obj_Entry *obj, const Elf_Rela *rela,
 #endif
 	}
 }
+#endif /* !TLS_TGOT */
+
+#ifdef TLS_TGOT
+struct tgot_tls_data {
+	ptraddr_t	dtv_gen;
+	int		tls_index;
+	Elf_Addr	tgot_offs;
+};
+
+static struct tgot_tls_data *
+reloc_tgot_tlsdesc_alloc(int tlsindex, Elf_Addr tgotoffs)
+{
+	struct tgot_tls_data *tlsdesc;
+
+	tlsdesc = xmalloc(sizeof(struct tgot_tls_data));
+	tlsdesc->dtv_gen = tls_dtv_generation;
+	tlsdesc->tls_index = tlsindex;
+	tlsdesc->tgot_offs = tgotoffs;
+
+	return (tlsdesc);
+}
+
+struct tgot_tlsdesc_entry {
+	void	*(*func)(void *);
+	union {
+		Elf_Size		offset;
+		struct tgot_tls_data	*data;
+	};
+};
+
+static void
+reloc_tgot_tlsdesc(const Obj_Entry *obj, const Elf_Rela *rela,
+    struct tgot_tlsdesc_entry *where)
+{
+	if (obj->tgotoffset != 0) {
+		/* Object's TGOT is in initially allocated TGOT segment */
+		where->func = rtld_tgot_tlsdesc_static_fptr;
+		where->offset = obj->tgotoffset + rela->r_addend;
+	} else {
+		/* Object's TGOT is dynamically allocated */
+		where->func = rtld_tgot_tlsdesc_dynamic_fptr;
+		where->data = reloc_tgot_tlsdesc_alloc(obj->tlsindex,
+		    rela->r_addend);
+	}
+}
+#endif /* TLS_TGOT */
 
 /*
  * Process the PLT relocations.
@@ -528,9 +587,26 @@ reloc_plt(Plt_Entry *plt, int flags, RtldLockState *lockstate)
 #else
 		case R_AARCH64_TLSDESC:
 #endif
+#ifndef TLS_TGOT
 			reloc_tlsdesc(obj, rela, (struct tlsdesc_entry *)where,
 			    SYMLOOK_IN_PLT | flags, lockstate);
 			break;
+#else
+			_rtld_error("%s: Traditional TLS not supported",
+			    obj->path);
+			return (-1);
+#endif
+#ifdef __CHERI_PURE_CAPABILITY__
+		case R_MORELLO_TGOT_TLSDESC:
+#ifdef TLS_TGOT
+			reloc_tgot_tlsdesc(obj, rela,
+			    (struct tgot_tlsdesc_entry *)where);
+			break;
+#else
+			_rtld_error("%s: TGOT not supported", obj->path);
+			return (-1);
+#endif
+#endif
 #ifdef __CHERI_PURE_CAPABILITY__
 		case R_MORELLO_IRELATIVE:
 #else
@@ -846,7 +922,9 @@ reloc_non_plt(Obj_Entry *obj, Obj_Entry *obj_rtld, int flags,
 		case R_AARCH64_ABS64:
 		case R_AARCH64_GLOB_DAT:
 #ifdef __CHERI_PURE_CAPABILITY__
+#ifndef TLS_TGOT
 		case R_MORELLO_TLS_TPREL128:
+#endif
 #else
 		case R_AARCH64_TLS_TPREL64:
 		case R_AARCH64_TLS_DTPREL64:
@@ -974,14 +1052,32 @@ reloc_non_plt(Obj_Entry *obj, Obj_Entry *obj_rtld, int flags,
 #else
 		case R_AARCH64_TLSDESC:
 #endif
+#ifndef TLS_TGOT
 			reloc_tlsdesc(obj, rela, (struct tlsdesc_entry *)where,
 			    flags, lockstate);
 			break;
+#else
+			_rtld_error("%s: Traditional TLS not supported",
+			    obj->path);
+			return (-1);
+#endif
+#ifdef __CHERI_PURE_CAPABILITY__
+		case R_MORELLO_TGOT_TLSDESC:
+#ifdef TLS_TGOT
+			reloc_tgot_tlsdesc(obj, rela,
+			    (struct tgot_tlsdesc_entry *)where);
+			break;
+#else
+			_rtld_error("%s: TGOT not supported", obj->path);
+			return (-1);
+#endif
+#endif
 #ifdef __CHERI_PURE_CAPABILITY__
 		case R_MORELLO_TLS_TPREL128:
 #else
 		case R_AARCH64_TLS_TPREL64:
 #endif
+#ifndef TLS_TGOT
 			/*
 			 * We lazily allocate offsets for static TLS as we
 			 * see the first relocation that references the
@@ -1006,6 +1102,30 @@ reloc_non_plt(Obj_Entry *obj, Obj_Entry *obj_rtld, int flags,
 				where[1] = def->st_size;
 #endif
 			break;
+#else
+			_rtld_error("%s: Traditional TLS not supported",
+			    obj->path);
+			return (-1);
+#endif
+#ifdef __CHERI_PURE_CAPABILITY__
+		case R_MORELLO_TLS_TGOTREL64:
+#ifdef TLS_TGOT
+			if (!obj->tgot_static) {
+				if (!allocate_tgot_offset(
+				    __DECONST(Obj_Entry *, obj))) {
+					_rtld_error(
+					    "%s: No space available for static "
+					    "Thread Local Storage", obj->path);
+					return (-1);
+				}
+			}
+			*where = obj->tgotoffset + rela->r_addend;
+			break;
+#else
+			_rtld_error("%s: TGOT not supported", obj->path);
+			return (-1);
+#endif
+#endif
 
 #ifndef __CHERI_PURE_CAPABILITY__
 		/*
@@ -1053,6 +1173,59 @@ reloc_non_plt(Obj_Entry *obj, Obj_Entry *obj_rtld, int flags,
 	return (0);
 }
 
+#ifdef TLS_TGOT
+/*
+ * Process the TGOT relocations.
+ */
+int
+reloc_tgot(Obj_Entry *obj, struct tcb *tcb, void *tgot, int flags,
+    tls_get_block_cb get_block, RtldLockState *lockstate)
+{
+	const Obj_Entry *defobj;
+	const Elf_Rela *relalim;
+	const Elf_Rela *rela;
+	const Elf_Sym *def;
+	Elf_Addr *fragment;
+	Elf_Addr tgotinit;
+	uintptr_t *where;
+	uintptr_t val;
+	void *tls;
+
+	tls = NULL;
+	relalim = (const Elf_Rela *)((const char *)obj->tgotrela +
+	    obj->tgotrelasize);
+	tgotinit = (const char *)obj->tgotinit - obj->relocbase;
+	for (rela = obj->tgotrela; rela < relalim; rela++) {
+		where = (uintptr_t *)((uintptr_t)tgot +
+		    (rela->r_offset - tgotinit));
+		fragment = (Elf_Addr *)where;
+
+		assert(ELF_R_TYPE(rela->r_info) == R_MORELLO_TLS_TGOT_SLOT);
+
+		if (ELF_R_SYM(rela->r_info) != 0) {
+			def = find_symdef(ELF_R_SYM(rela->r_info), obj,
+			    &defobj, flags, NULL, lockstate);
+			if (def == NULL)
+				return (-1);
+			if (def->st_shndx == SHN_UNDEF)
+				val = 0;
+			else {
+				val = (uintptr_t)get_block(tcb,
+				    defobj->tlsindex) + def->st_value;
+				val = cheri_bounds_set(val, def->st_size);
+			}
+		} else {
+			if (tls == NULL)
+				tls = get_block(tcb, obj->tlsindex);
+			val = init_cap_from_fragment(fragment, tls, NULL,
+			    (Elf_Addr)tls, 0);
+		}
+		*where = val;
+	}
+	return (0);
+}
+#endif
+
 void
 allocate_initial_tls(Obj_Entry *objs)
 {
@@ -1062,8 +1235,14 @@ allocate_initial_tls(Obj_Entry *objs)
 	* offset allocated so far and adding a bit for dynamic modules to
 	* use.
 	*/
+#ifndef TLS_TGOT
 	tls_static_space = tls_last_offset + tls_last_size +
 	    ld_static_tls_extra;
+#endif
+#ifdef TLS_TGOT
+	tgot_static_space = tgot_last_offset + tgot_last_size +
+	    ld_static_tgot_extra;
+#endif
 
 	_tcb_set(allocate_tls(objs, NULL, TLS_TCB_SIZE, TLS_TCB_ALIGN, NULL));
 }

libexec/rtld-elf/aarch64/rtld_machdep.h
@@ -105,12 +105,22 @@ uintptr_t reloc_jmpslot(uintptr_t *where, uintptr_t target,
 
 #define	round(size, align)				\
 	(((size) + (align) - 1) & ~((align) - 1))
+
+#ifndef TLS_TGOT
 #define	calculate_first_tls_offset(size, align, offset)	\
 	round(TLS_TCB_SIZE, align)
 #define	calculate_tls_offset(prev_offset, prev_size, size, align, offset) \
 	round(prev_offset + prev_size, align)
 #define calculate_tls_post_size(align) \
 	round(TLS_TCB_SIZE, align) - TLS_TCB_SIZE
+#endif
+
+#ifdef TLS_TGOT
+#define	calculate_first_tgot_offset(size, align, offset)	\
+	TLS_TCB_SIZE
+#define	calculate_tgot_offset(prev_offset, prev_size, size, align, offset) \
+	round(prev_offset + prev_size, align)
+#endif
 
 typedef struct {
     unsigned long ti_module;
@@ -122,8 +132,14 @@ extern void *__tls_get_addr(tls_index *ti);
 #define md_abi_variant_hook(x)
 
 extern void (*rtld_bind_start_fptr)(void);
+#ifndef TLS_TGOT
 extern void *(*rtld_tlsdesc_static_fptr)(void *);
 extern void *(*rtld_tlsdesc_undef_fptr)(void *);
 extern void *(*rtld_tlsdesc_dynamic_fptr)(void *);
+#endif
+#ifdef TLS_TGOT
+extern void *(*rtld_tgot_tlsdesc_static_fptr)(void *);
+extern void *(*rtld_tgot_tlsdesc_dynamic_fptr)(void *);
+#endif
 
 #endif

libexec/rtld-elf/aarch64/rtld_start.S
@@ -193,6 +193,7 @@ ENTRY(_rtld_bind_start)
 #endif
 END(_rtld_bind_start)
 
+#ifndef __CHERI_TGOT_TLS__
 /*
  * See reloc.c for struct tlsdesc_entry's layout.
  *
@@ -470,5 +471,134 @@ ENTRY(_rtld_tlsdesc_dynamic)
 	ret
 #endif /* defined(__CHERI_PURE_CAPABILITY__) */
 END(_rtld_tlsdesc_dynamic)
+#endif /* !__CHERI_TGOT_TLS__ */
+
+#ifdef __CHERI_TGOT_TLS__
+/*
+ * void *_rtld_tgot_tlsdesc_static(struct tlsdesc_entry *);
+ *
+ * ctpidr_el0 is passed in c1 and preserved.
+ *
+ * Resolver function for TLS symbols resolved at load time
+ */
+ENTRY(_rtld_tgot_tlsdesc_static)
+	ldr	x0, [c0, #16]
+	ldr	c0, [c1, x0]
+	RETURN
+END(_rtld_tgot_tlsdesc_static)
+
+/*
+ * void *_rtld_tgot_tlsdesc_dynamic(struct tlsdesc_entry *);
+ *
+ * ctpidr_el0 is passed in c1 and preserved.
+ *
+ * Resolver function for objects from dlopen()
+ */
+ENTRY(_rtld_tgot_tlsdesc_dynamic)
+	/* Save registers used in fast path */
+	str	c2,     [csp, #(-3 * 16)]!
+	stp	c3, c4, [csp, #(1 * 16)]
+	.cfi_adjust_cfa_offset	3 * 16
+	.cfi_rel_offset		c2, 0
+	.cfi_rel_offset		c3, 16
+	.cfi_rel_offset		c4, 32
+
+	/* Test fastpath - inlined version of tls_get_addr_common(). */
+	ldr	c2, [c0, #16]
+	ldr	c0, [c1]		/* DTV pointer */
+	ldr	x3, [c0]		/* dtv[0] (generation count) */
+	ldr	x4, [c2]		/* tlsdesc->dtv_gen */
+	cmp	x3, x4
+	b.ne	1f			/* dtv[0] != tlsdec->dtv_gen */
+
+	ldr	w3, [c2, #16]		/* tlsdesc->tls_index */
+	lsl	w3, w3, #1
+	add	w3, w3, #1
+	ldr     c3, [c0, w3, sxtw #4]	/* dtv->dtv_slots[tlsdesc->tls_index - 1].dtvs_tgot */
+	cbz	x3, 1f
+
+	/* Return (*(dtv->dtv_slots[tlsdesc->tls_index - 1].dtvs_tgot + tlsdesc->tgot_offs)) */
+	ldr	x4, [c2, #24]		/* tlsdesc->tgot_offs */
+	ldr	c0, [c3, x4]
+
+	/* Restore registers and return */
+	ldp	c3,  c4, [csp, #(1 * 16)]
+	ldr	c2,      [csp], #(3 * 16)
+	.cfi_adjust_cfa_offset 	-3 * 16
+	RETURN
+
+	/*
+	 * Slow path
+	 * return(
+	 *    tls_get_addr_common(tcb, tlsdesc->tls_index, tlsdesc->tls_offs));
+	 *
+	 */
+1:
+	/* Save non-callee-saved capability registers */
+	stp	c29, c30, [csp, #-(17 * 16)]!
+	.cfi_adjust_cfa_offset	17 * 16
+	.cfi_rel_offset		c29, 0
+	.cfi_rel_offset		c30, 16
+
+	mov	c29, csp
+	str	c1,       [csp, #(2 * 16)]
+	stp	c5,   c6, [csp, #(3 * 16)]
+	stp	c7,   c8, [csp, #(5 * 16)]
+	stp	c9,  c10, [csp, #(7 * 16)]
+	stp	c11, c12, [csp, #(9 * 16)]
+	stp	c13, c14, [csp, #(11 * 16)]
+	stp	c15, c16, [csp, #(13 * 16)]
+	stp	c17, c18, [csp, #(15 * 16)]
+	.cfi_rel_offset		 c1, 32
+	.cfi_rel_offset		 c5, 48
+	.cfi_rel_offset		 c6, 64
+	.cfi_rel_offset		 c7, 80
+	.cfi_rel_offset		 c8, 96
+	.cfi_rel_offset		 c9, 112
+	.cfi_rel_offset		c10, 128
+	.cfi_rel_offset		c11, 144
+	.cfi_rel_offset		c12, 160
+	.cfi_rel_offset		c13, 176
+	.cfi_rel_offset		c14, 192
+	.cfi_rel_offset		c15, 208
+	.cfi_rel_offset		c16, 224
+	.cfi_rel_offset		c17, 240
+	.cfi_rel_offset		c18, 256
+
+	/* Find the tls offset */
+	mov	c0, c1			/* tcb */
+	mov	c3, c2			/* tlsdesc ptr */
+	ldr	w1, [c3, #8]		/* tlsdesc->tls_index */
+	ldr	x2, [c3, #16]		/* tlsdesc->tgot_offs */
+	adrp	PTR(10), :got:tls_get_addr_common_fptr
+	ldr	PTR(10), [PTR(10), :got_lo12:tls_get_addr_common_fptr]
+	ldr	PTR(10), [PTR(10)]
+#ifdef __ARM_MORELLO_PURECAP_BENCHMARK_ABI
+	blr	x10
+#else
+	blr	PTR(10)
+#endif
+
+	/* Restore slow path registers */
+	ldp	c17, c18, [csp, #(15 * 16)]
+	ldp	c15, c16, [csp, #(13 * 16)]
+	ldp	c13, c14, [csp, #(11 * 16)]
+	ldp	c11, c12, [csp, #(9 * 16)]
+	ldp	c9, c10,  [csp, #(7 * 16)]
+	ldp	c7, c8,   [csp, #(5 * 16)]
+	ldp	c5, c6,   [csp, #(3 * 16)]
+	ldr	c1,       [csp, #(2 * 16)]
+	ldp	c29, c30, [csp], #(17 * 16)
+	.cfi_adjust_cfa_offset 	-17 * 16
+	.cfi_restore		c29
+	.cfi_restore		c30
+
+	/* Restore fast path registers and return */
+	ldp	c3,  c4, [csp, #(1 * 16)]
+	ldr	c2,      [csp], #(3 * 16)
+	.cfi_adjust_cfa_offset 	-3 * 16
+	RETURN
+END(_rtld_tgot_tlsdesc_dynamic)
+#endif /* __CHERI_TGOT_TLS__ */
 
 GNU_PROPERTY_AARCH64_FEATURE_1_NOTE(GNU_PROPERTY_AARCH64_FEATURE_1_VAL)

commit c78b56633bbf76978d638751661b39337db8333e

rtld-elf: Fix build error enabling Morello TGOT from merging conflicting PRs
In isolation, this code was correct, but it was merged immediately after
a20d37a2265d ("rtld: Use tight PCC bounds if PT_CHERI_PCC is present"),
which changed init_cap_from_fragment to take an additional parameter. In
this case we're passing NULL as pcc_cap so it doesn't matter what value
for use_code_bounds we pick, but we might was well pick true since
eventually the parameter will go away and be assumed true.

Fixes: cc8183070107 ("rtld-elf: Support CHERI TGOT TLS on Morello")

libexec/rtld-elf/aarch64/reloc.c
@@ -1226,7 +1226,7 @@ reloc_tgot(Obj_Entry *obj, struct tcb *tcb, void *tgot, int flags,
 			if (tls == NULL)
 				tls = get_block(tcb, obj->tlsindex);
 			val = init_cap_from_fragment(fragment, tls, NULL,
-			    (Elf_Addr)tls, 0);
+			    (Elf_Addr)tls, 0, true);
 		}
 		*where = val;
 	}