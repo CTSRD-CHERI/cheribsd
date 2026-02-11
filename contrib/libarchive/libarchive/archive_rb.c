1805f5ddbfca09914867a98a93f42d6b7834c133
John Baldwin <jhb@FreeBSD.org>
2026-02-11 09:52:46 -0500

    libarchive: Stop using cheri_*_low_ptr_bits
    This mostly reverts local changes.  A single ptraddr_t cast remains to
    ensure proper provenance for a uintcap_t arithmetic operation.

--- contrib/libarchive/libarchive/archive_rb.c
+++ contrib/libarchive/libarchive/archive_rb.c
@@ -34,7 +34,7 @@
  *   "updated": 20221129,
  *   "target_type": "lib",
  *   "changes": [
- *     "pointer_bit_flags",
+ *     "intcap_arithmetic",
  *     "unsupported"
  *   ],
  *   "change_comment": "can't use xor to swap bit flags"
@@ -48,15 +48,6 @@
 
 #include "archive_rb.h"
 
-#ifdef __CHERI_PURE_CAPABILITY__
-#include <cheri/cheric.h>
-#define	__clear_bits(ptr, mask) cheri_clear_low_ptr_bits((ptr), (mask))
-#define	__get_bits(ptr, mask) cheri_get_low_ptr_bits((ptr), (mask))
-#else
-#define	__clear_bits(ptr, mask) ((ptr) & ~(mask))
-#define	__get_bits(ptr, mask)	((ptr) & (mask))
-#endif
-
 /* Keep in sync with archive_rb.h */
 #define	RB_DIR_LEFT		0
 #define	RB_DIR_RIGHT		1
@@ -64,13 +55,13 @@
 #define	rb_left			rb_nodes[RB_DIR_LEFT]
 #define	rb_right		rb_nodes[RB_DIR_RIGHT]
 
-#define	RB_FLAG_POSITION	(uintptr_t)0x2
-#define	RB_FLAG_RED		(uintptr_t)0x1
+#define	RB_FLAG_POSITION	0x2
+#define	RB_FLAG_RED		0x1
 #define	RB_FLAG_MASK		(RB_FLAG_POSITION|RB_FLAG_RED)
 #define	RB_FATHER(rb) \
-    ((struct archive_rb_node *)__clear_bits((rb)->rb_info, RB_FLAG_MASK))
+    ((struct archive_rb_node *)((rb)->rb_info & ~RB_FLAG_MASK))
 #define	RB_SET_FATHER(rb, father) \
-    ((void)((rb)->rb_info = (uintptr_t)(father)|__get_bits((rb)->rb_info, RB_FLAG_MASK)))
+    ((void)((rb)->rb_info = (uintptr_t)(father)|((ptraddr_t)(rb)->rb_info & RB_FLAG_MASK)))
 
 #define	RB_SENTINEL_P(rb)	((rb) == NULL)
 #define	RB_LEFT_SENTINEL_P(rb)	RB_SENTINEL_P((rb)->rb_left)
@@ -82,21 +73,21 @@
     (!RB_SENTINEL_P(rb) && !RB_LEFT_SENTINEL_P(rb) && !RB_RIGHT_SENTINEL_P(rb))
 
 #define	RB_POSITION(rb)	\
-    (__get_bits((rb)->rb_info, RB_FLAG_POSITION) ? RB_DIR_RIGHT : RB_DIR_LEFT)
+    (((rb)->rb_info & RB_FLAG_POSITION) ? RB_DIR_RIGHT : RB_DIR_LEFT)
 #define	RB_RIGHT_P(rb)		(RB_POSITION(rb) == RB_DIR_RIGHT)
 #define	RB_LEFT_P(rb)		(RB_POSITION(rb) == RB_DIR_LEFT)
-#define	RB_RED_P(rb) 		(!RB_SENTINEL_P(rb) && __get_bits((rb)->rb_info, RB_FLAG_RED) != 0)
-#define	RB_BLACK_P(rb) 		(RB_SENTINEL_P(rb) || __get_bits((rb)->rb_info, RB_FLAG_RED) == 0)
+#define	RB_RED_P(rb) 		(!RB_SENTINEL_P(rb) && ((rb)->rb_info & RB_FLAG_RED) != 0)
+#define	RB_BLACK_P(rb) 		(RB_SENTINEL_P(rb) || ((rb)->rb_info & RB_FLAG_RED) == 0)
 #define	RB_MARK_RED(rb) 	((void)((rb)->rb_info |= RB_FLAG_RED))
-#define	RB_MARK_BLACK(rb) 	((void)((rb)->rb_info = __clear_bits((rb)->rb_info, RB_FLAG_RED)))
+#define	RB_MARK_BLACK(rb) 	((void)((rb)->rb_info &= ~RB_FLAG_RED))
 #ifndef __CHERI_PURE_CAPABILITY__
 #define	RB_INVERT_COLOR(rb) 	((void)((rb)->rb_info ^= RB_FLAG_RED))
 #endif
 #define	RB_ROOT_P(rbt, rb)	((rbt)->rbt_root == (rb))
 #define	RB_SET_POSITION(rb, position) \
     ((void)((position) ? ((rb)->rb_info |= RB_FLAG_POSITION) : \
-    ((rb)->rb_info = __clear_bits((rb)->rb_info, RB_FLAG_POSITION))))
-#define	RB_ZERO_PROPERTIES(rb)	((void)((rb)->rb_info = __clear_bits((rb)->rb_info, RB_FLAG_MASK)))
+    ((rb)->rb_info &= ~RB_FLAG_POSITION)))
+#define	RB_ZERO_PROPERTIES(rb)	((void)((rb)->rb_info &= ~RB_FLAG_MASK))
 #ifndef __CHERI_PURE_CAPABILITY__
 #define	RB_COPY_PROPERTIES(dst, src) \
     ((void)((dst)->rb_info ^= ((dst)->rb_info ^ (src)->rb_info) & RB_FLAG_MASK))
@@ -114,16 +105,16 @@
 static inline void
 RB_COPY_PROPERTIES(struct archive_rb_node * const dst, struct archive_rb_node * const src)
 {
-	ptraddr_t src_bits = __get_bits(src->rb_info, RB_FLAG_MASK);
-	dst->rb_info = __clear_bits(dst->rb_info, RB_FLAG_MASK) | src_bits;
+	ptraddr_t src_bits = src->rb_info & RB_FLAG_MASK;
+	dst->rb_info = (dst->rb_info & ~RB_FLAG_MASK) | src_bits;
 }
 static inline void
 RB_SWAP_PROPERTIES(struct archive_rb_node * const a, struct archive_rb_node * const b)
 {
-	ptraddr_t a_bits = __get_bits(a->rb_info, RB_FLAG_MASK);
-	ptraddr_t b_bits = __get_bits(b->rb_info, RB_FLAG_MASK);
-	a->rb_info = __clear_bits(a->rb_info, RB_FLAG_MASK) | b_bits;
-	b->rb_info = __clear_bits(b->rb_info, RB_FLAG_MASK) | a_bits;
+	ptraddr_t a_bits = a->rb_info & RB_FLAG_MASK;
+	ptraddr_t b_bits = b->rb_info & RB_FLAG_MASK;
+	a->rb_info = (a->rb_info & ~RB_FLAG_MASK) | b_bits;
+	b->rb_info = (b->rb_info & ~RB_FLAG_MASK) | a_bits;
 }
 #endif
 
