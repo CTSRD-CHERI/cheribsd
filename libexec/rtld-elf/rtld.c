bff991ba4396d0ab76ed5aa996c6c0f363e8d73e
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2026-02-19 16:48:16 +0000

    cheri rtld: Use HAS_CHERI_PERM_* to check for permission bit existence.

--- libexec/rtld-elf/rtld.c
+++ libexec/rtld-elf/rtld.c
@@ -5052,10 +5052,19 @@ static void
 rtld_fill_dl_phdr_info(const Obj_Entry *obj, struct dl_phdr_info *phdr_info)
 {
 #ifdef __CHERI_PURE_CAPABILITY__
+#ifdef HAS_CHERI_PERM_LOAD_STORE_CAP
 	phdr_info->dlpi_addr = (uintptr_t)cheri_perms_and(obj->relocbase,
 	    CHERI_PERM_LOAD | CHERI_PERM_LOAD_CAP);
 	phdr_info->dlpi_phdr = cheri_perms_and(obj->phdr,
 	    CHERI_PERM_LOAD | CHERI_PERM_LOAD_CAP);
+#elif defined(HAS_CHERI_PERM_CAP)
+	phdr_info->dlpi_addr = (uintptr_t)cheri_perms_and(obj->relocbase,
+	    CHERI_PERM_LOAD | CHERI_PERM_CAP);
+	phdr_info->dlpi_phdr = cheri_perms_and(obj->phdr,
+	    CHERI_PERM_LOAD | CHERI_PERM_CAP);
+#else
+#error "Missing LOAD_CAP permission"
+#endif
 #else
 	phdr_info->dlpi_addr = (Elf_Addr)obj->relocbase;
 	phdr_info->dlpi_phdr = obj->phdr;
