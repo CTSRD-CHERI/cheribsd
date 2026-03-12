d6e66e10228727832a9dcaaafb32849666521f2e
John Baldwin <jhb@FreeBSD.org>
2026-03-12 16:14:03 -0400

    Revert "lindebugfs: Don't pass a kernel pointer to copy_from_user"
    This reverts commit 52a04d66ae5426db7b256f61e238f5cb4f4317e2.

--- sys/compat/lindebugfs/lindebugfs.c
+++ sys/compat/lindebugfs/lindebugfs.c
@@ -700,12 +700,10 @@ fops_str_write(struct file *filp, const char *buf, size_t write_size,
 		return (-ENOMEM);
 
 	memcpy(new, old, old_len);
-
-	/*
-	 * In Linux this would use copy_from_user, but pseudofs passes
-	 * a pointer to a kernel buffer in *buf, not a user pointer.
-	 */
-	memcpy(new + old_len, buf, write_size);
+	if (copy_from_user(new + old_len, buf, write_size) != 0) {
+		kfree(new);
+		return (-EFAULT);
+	}
 
 	new[new_len] = '\0';
 	strim(new);
