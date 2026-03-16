4ee62d5baadaa49464ef2e2eeb52554023205744
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2026-03-16 10:05:56 +0000

    cheri-rv64y: Add HAS_CHERI_PERM_SEAL to check otype existence.
    This should be defined for architectures that support CHERI seal/unseal
    permission and have an otype space beyond the sentry capability.

--- sys/arm64/include/cherireg.h
+++ sys/arm64/include/cherireg.h
@@ -80,6 +80,7 @@
 #define	HAS_CHERI_PERM_LOAD_STORE_CAP
 #define	HAS_CHERI_PERM_LOAD_MUTABLE
 #define	HAS_CHERI_PERM_EXECUTIVE
+#define	HAS_CHERI_PERM_SEAL
 
 /* Define alias for MUTABLE_LOAD permission bit to have consistent spelling */
 #define	CHERI_PERM_LOAD_MUTABLE CHERI_PERM_MUTABLE_LOAD
