58888f313eaee4a50b9d557147bf471fb9549f5f
Robert N. M. Watson <robert.watson@cl.cam.ac.uk>
2026-04-05 15:43:07 +0100

    Locally bump __FreeBSD_version in the msetname branch.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -73,7 +73,7 @@
  * cannot include sys/param.h and should only be updated here.
  */
 #undef __FreeBSD_version
-#define __FreeBSD_version 1500039
+#define __FreeBSD_version 1500040
 
 /*
  * __CheriBSD_version numbers describe CheriBSD ABIs.
@@ -120,7 +120,7 @@
 #define	P_OSREL_POWERPC_NEW_AUX_ARGS	1300070
 #define	P_OSREL_TIDPID			1400079
 #define	P_OSREL_ARM64_SPSR		1400084
-#define	P_OSREL_MSETNAME		1500039
+#define	P_OSREL_MSETNAME		1500040
 
 #define	P_OSREL_MAJOR(x)		((x) / 100000)
 #endif
