55168cef9ffb0acc08910cccb2ada905af66ea85
Jessica Clarke <jrtc27@jrtc27.com>
2026-04-17 15:56:57 +0100

    loader.efi: Support metadata in pure-capability Morello kernels
    Currently we treat pure-capability kernels as normal elf64 ones, which
    is fine from a loading perspective, but not when parsing the metadata.
    Module metadata is a linker set of, i.e. an array of pointers pointing
    to, structures that themselves contain pointers, and so for loader to be
    able to properly parse it, in particular to extract dependency-related
    information, it needs to be able to process the relocations associated
    with those pointers, and more fundamentally to have the right layout for
    the linker set section and structures pointed to. In a pure-capability
    kernel these pointers are of course all capabilities, and so we need a
    separate variant just like elf32 vs elf64, so introduce a new elf64c
    one.

---
+++ stand/efi/loader/arch/arm64/exec_elf.c
@@ -0,0 +1,96 @@
+/*-
+ * Copyright (c) 2006 Marcel Moolenaar
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stand.h>
+#include <string.h>
+
+#include <sys/param.h>
+#include <sys/linker.h>
+#include <machine/elf.h>
+
+#include <bootstrap.h>
+
+#include <efi.h>
+#include <efilib.h>
+
+#include "loader_efi.h"
+#include "cache.h"
+
+static int __elfN(exec)(struct preloaded_file *amp);
+static int __elfN(obj_exec)(struct preloaded_file *amp);
+
+struct file_format __elfN(arm64) = {
+	__elfN(loadfile),
+	__elfN(exec)
+};
+
+static int
+__elfN(exec)(struct preloaded_file *fp)
+{
+	vm_offset_t modulep, kernendp;
+	vm_offset_t clean_addr;
+	size_t clean_size;
+	struct file_metadata *md;
+	Elf_Ehdr *ehdr;
+	int err;
+	void (*entry)(vm_offset_t);
+
+	if ((md = file_findmetadata(fp, MODINFOMD_ELFHDR)) == NULL)
+        	return(EFTYPE);
+
+	ehdr = (Elf_Ehdr *)&(md->md_data);
+
+	efi_time_fini();
+	err = bi_load(fp->f_args, &modulep, &kernendp, true);
+	if (err != 0) {
+		efi_time_init();
+		return (err);
+	}
+
+	dev_cleanup();
+
+	entry = efi_translate(ehdr->e_entry);
+
+	/* Clean D-cache under kernel area and invalidate whole I-cache */
+	clean_addr = (vm_offset_t)efi_translate(fp->f_addr);
+	clean_size = (vm_offset_t)efi_translate(kernendp) - clean_addr;
+
+	cpu_flush_dcache((void *)clean_addr, clean_size);
+	cpu_inval_icache();
+
+	(*entry)(modulep);
+	panic("exec returned");
+}
+
+static int
+__elfN(obj_exec)(struct preloaded_file *fp)
+{
+
+	printf("%s called for preloaded file %p (=%s):\n", __func__, fp,
+	    fp->f_name);
+	return (ENOSYS);
+}
+
