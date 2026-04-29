075e47e50785f2c7b9d40d1326a86d39f9fc4e4f
Jessica Clarke <jrtc27@jrtc27.com>
2026-04-29 20:17:26 +0100

    openssl: Remove trailing whitespace added downstream in vpaes-armv8.pl
    Fixes:	f5d1a2d192a9 ("OpenSSL vpaes-armv8.pl: Add purecap support")

--- crypto/openssl/crypto/aes/asm/vpaes-armv8.pl
+++ crypto/openssl/crypto/aes/asm/vpaes-armv8.pl
@@ -225,7 +225,7 @@ _vpaes_encrypt_core:
 	eor	v0.16b, v0.16b, v3.16b		// vpxor	%xmm3,	%xmm0,	%xmm0	# 3 = 2A+B+D
 #ifdef __CHERI_PURE_CAPABILITY__
 	alignd	c11, c11, #6			// and		\$0x30,	%r11		# ... mod 4
-#else		     
+#else
 	and	x11, x11, #~(1<<6)		// and		\$0x30,	%r11		# ... mod 4
 #endif
 	eor	v0.16b, v0.16b, v4.16b		// vpxor	%xmm4,	%xmm0, %xmm0	# 0 = 2A+3B+C+D
@@ -334,7 +334,7 @@ _vpaes_encrypt_2x:
 	 eor	v8.16b,  v8.16b,  v11.16b
 #ifdef __CHERI_PURE_CAPABILITY__
 	alignd	c11, c11, #6			// and		\$0x30,	%r11		# ... mod 4
-#else		     
+#else
 	and	x11, x11, #~(1<<6)		// and		\$0x30,	%r11		# ... mod 4
 #endif
 	eor	v0.16b,  v0.16b,  v4.16b	// vpxor	%xmm4,	%xmm0, %xmm0	# 0 = 2A+3B+C+D
@@ -654,7 +654,7 @@ ___
 }
 {
 my ($inp,$bits,$out,$dir)=("PTR(0)","w1","PTR(2)","w3");
-my ($inpx)=("x0");    
+my ($inpx)=("x0");
 my ($invlo,$invhi,$iptlo,$ipthi,$rcon) = map("v$_.16b",(18..21,8));
 
 $code.=<<___;
@@ -1055,7 +1055,7 @@ _vpaes_schedule_mangle:
 	add	PTR(8), PTR(8), #64-16		// add	\$-16,	%r8
 #ifdef __CHERI_PURE_CAPABILITY__
 	alignd	c8, c8, #6			// and	\$0x30,	%r8
-#else		     
+#else
 	and	x8, x8, #~(1<<6)		// and	\$0x30,	%r8
 #endif
 	st1	{v3.2d}, [$out]			// vmovdqu	%xmm3,	(%rdx)
