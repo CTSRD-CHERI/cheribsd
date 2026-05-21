2aa724accae9c21fbf54edaed7f97207220d5802
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2026-05-21 14:40:40 +0100

    loader.efi: check the alignment in copyin()/readin()
    Return an error if a copyin()/readin() call results in a misaligned
    stage_offset, which later would result in a misaligned physical base
    address of the kernel.

--- stand/efi/loader/copy.c
+++ stand/efi/loader/copy.c
@@ -478,6 +478,13 @@ efi_copyin(const void *src, vm_offset_t dest, const size_t len)
 
 	if (!stage_offset_set) {
 		stage_offset = (vm_offset_t)staging - dest;
+#if EFI_STAGING_2M_ALIGN
+		if (!__is_aligned(stage_offset, M(2))) {
+			printf("%s: dest is not aligned to 2M\n", __func__);
+			errno = EINVAL;
+			return (-1);
+		}
+#endif
 		stage_offset_set = true;
 	}
 
@@ -509,6 +516,13 @@ efi_readin(readin_handle_t fd, vm_offset_t dest, const size_t len)
 
 	if (!stage_offset_set) {
 		stage_offset = (vm_offset_t)staging - dest;
+#if EFI_STAGING_2M_ALIGN
+		if (!__is_aligned(stage_offset, M(2))) {
+			printf("%s: dest is not aligned to 2M\n", __func__);
+			errno = EINVAL;
+			return (-1);
+		}
+#endif
 		stage_offset_set = true;
 	}
 
