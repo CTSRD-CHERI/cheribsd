8e74d59f0dd7aebc1fa00e181eedc8b9aa57687b
Robert N. M. Watson <robert.watson@cl.cam.ac.uk>
2026-05-23 00:20:37 +0100

    Do some structure and function renaming.

--- sys/dev/tagscan/tagscan_stats.c
+++ sys/dev/tagscan/tagscan_stats.c
@@ -28,73 +28,73 @@
 #define	TS_INTERVAL_SECS_MIN		10
 #define	TS_INTERVAL_SECS_MAX		120
 
-/* static int	ts_interval_secs = TS_INTERVAL_SECS_DEFAULT; */
+/* static int	tps_interval_secs = TS_INTERVAL_SECS_DEFAULT; */
 
 /*
  * Statistics structure decribing both direct measurements and summary stats +
  * overhead estimates for a set of pages.  Most (but not all) stats are
  * calculated relative to its own total pages/words/bytes counted.
  */
-struct tagscan_stats {
+struct tagscan_pageset_stats {
 	/*
 	 * Stats at page granularity.
 	 */
-	uint64_t	ts_pages_count_scanned;			/* Measured. */
-	uint64_t	ts_pages_count_with_tags;		/* Measured. */
-	uint64_t	ts_pages_percent_with_tags_int;		/* Summary. */
-	uint64_t	ts_pages_percent_with_tags_frac;	/* Summary. */
-	uint64_t	ts_pages_tagstore_overhead_bytes;	/* Summary. */
-	uint64_t	ts_pages_tagstore_overhead_percent_int;	/* Summary. */
-	uint64_t	ts_pages_tagstore_overhead_percent_frac;/* Summary. */
+	uint64_t	tps_pages_count_scanned;		/* Measured. */
+	uint64_t	tps_pages_count_with_tags;		/* Measured. */
+	uint64_t	tps_pages_percent_with_tags_int;	/* Summary. */
+	uint64_t	tps_pages_percent_with_tags_frac;	/* Summary. */
+	uint64_t	tps_pages_tagstore_overhead_bytes;	/* Summary. */
+	uint64_t	tps_pages_tagstore_overhead_percent_int;/* Summary. */
+	uint64_t	tps_pages_tagstore_overhead_percent_frac;/* Summary. */
 
 	/*
 	 * Stats at (capability) word granularity.
 	 */
-	uint64_t	ts_words_count_scanned;			/* Measured. */
-	uint64_t	ts_words_count_with_tags;		/* Measured. */
-	uint64_t	ts_words_count_percent_with_tags_int;	/* Summary. */
-	uint64_t	ts_words_count_percent_with_tags_frac;	/* Summary. */
-	uint64_t	ts_words_memory_overhead_bytes;		/* Summary. */
-	uint64_t	ts_words_memory_overhead_percent_int;	/* Summary. */
-	uint64_t	ts_words_memory_overhead_percent_frac;	/* Summary. */
+	uint64_t	tps_words_count_scanned;		/* Measured. */
+	uint64_t	tps_words_count_with_tags;		/* Measured. */
+	uint64_t	tps_words_count_percent_with_tags_int;	/* Summary. */
+	uint64_t	tps_words_count_percent_with_tags_frac;	/* Summary. */
+	uint64_t	tps_words_memory_overhead_bytes;	/* Summary. */
+	uint64_t	tps_words_memory_overhead_percent_int;	/* Summary. */
+	uint64_t	tps_words_memory_overhead_percent_frac;	/* Summary. */
 
 	/*
 	 * Stats at byte granularity.
 	 */
-	uint64_t	ts_bytes_count_scanned;
-	uint64_t	ts_bytes_count_in_capabilities;
+	uint64_t	tps_bytes_count_scanned;		/* Measured. */
+	uint64_t	tps_bytes_count_in_capabilities;	/* Measured. */
 
 	/*
 	 * Misc.
 	 */
-	uint64_t	ts_percent_of_global_memory_int;	/* Summary. */
-	uint64_t	ts_percent_of_global_memory_frac;	/* Summary. */
+	uint64_t	tps_percent_of_global_memory_int;	/* Summary. */
+	uint64_t	tps_percent_of_global_memory_frac;	/* Summary. */
 };
 
 /*-
  * Various collections of pages for which we generate / maintain statistics:
  *
- * ts_global - All present pages
- * ts_allocated - All present pages allocated by the buddy allocator
- * ts_unallocated - All present pages unallocated by the buddy allocator
- * ts_object - All present allocated pages with an associated VM object
- * ts_no_object - All present allocated pages without an associated VM object
- * ts_vnode_object - All present allocated pages in vnode objects
- * ts_swap_object - All present allocated pages in swap objects
- * ts_anon_object - All present allocated pages in anonymous objects
- * ts_kernel_object - All present allocated pages in the kernel object
- * ts_phys_object - All present allocated pages consisting of physical pages
- * ts_device_object - All present allocated pages in device objects
- * ts_misc_object - All present allocated pages in other object types
+ * tps_global - All present pages
+ * tps_allocated - All present pages allocated by the buddy allocator
+ * tps_unallocated - All present pages unallocated by the buddy allocator
+ * tps_object - All present allocated pages with an associated VM object
+ * tps_no_object - All present allocated pages without an associated VM object
+ * tps_vnode_object - All present allocated pages in vnode objects
+ * tps_swap_object - All present allocated pages in swap objects
+ * tps_anon_object - All present allocated pages in anonymous objects
+ * tps_kernel_object - All present allocated pages in the kernel object
+ * tps_phys_object - All present allocated pages consisting of physical pages
+ * tps_device_object - All present allocated pages in device objects
+ * tps_misc_object - All present allocated pages in other object types
  */
-struct tagscan_stats	ts_global;
-struct tagscan_stats	ts_allocated, ts_unallocated;
-struct tagscan_stats	ts_object, ts_no_object;
-struct tagscan_stats	ts_vnode_object, ts_swap_object, ts_phys_object;
-struct tagscan_stats	ts_anon_object;
-struct tagscan_stats	ts_device_object;
-struct tagscan_stats	ts_kernel_object;
-struct tagscan_stats	ts_misc_object;
+struct tagscan_pageset_stats	tps_global;
+struct tagscan_pageset_stats	tps_allocated, tps_unallocated;
+struct tagscan_pageset_stats	tps_object, tps_no_object;
+struct tagscan_pageset_stats	tps_vnode_object, tps_swap_object;
+struct tagscan_pageset_stats	tps_phys_object, tps_device_object;
+struct tagscan_pageset_stats	tps_anon_object;
+struct tagscan_pageset_stats	tps_kernel_object;
+struct tagscan_pageset_stats	tps_misc_object;
 
 SYSCTL_NODE(_dev, OID_AUTO, tagscan, CTLFLAG_RW | CTLFLAG_MPSAFE, NULL,
     "Physical memory CHERI tag scanning");
@@ -105,14 +105,15 @@ SYSCTL_NODE(_dev, OID_AUTO, tagscan, CTLFLAG_RW | CTLFLAG_MPSAFE, NULL,
  * page may be associated with multiple statistics blocks.
  */
 static void
-tagscan_rescan_page_update_stats(struct tagscan_stats *tsp, uint64_t tags)
+tagscan_pageset_stats_update(struct tagscan_pageset_stats *tpsp,
+    uint64_t tags)
 {
 
-	tsp->ts_pages_count_scanned++;
+	tpsp->tps_pages_count_scanned++;
 	if (tags)
-		tsp->ts_pages_count_with_tags++;
-	tsp->ts_words_count_scanned += PAGE_SIZE / sizeof(uintcap_t);
-	tsp->ts_words_count_with_tags += tags;
+		tpsp->tps_pages_count_with_tags++;
+	tpsp->tps_words_count_scanned += PAGE_SIZE / sizeof(uintcap_t);
+	tpsp->tps_words_count_with_tags += tags;
 }
 
 static void
@@ -136,51 +137,46 @@ tagscan_rescan_page(vm_page_t m)
 			tags++;
 	}
 
-	tagscan_rescan_page_update_stats(&ts_global, tags);
+	tagscan_pageset_stats_update(&tps_global, tags);
 
 	/* If not allocated by the buddy allocator, don't analyse further. */
 	if (m->order < VM_NFREEORDER) {
-		tagscan_rescan_page_update_stats(&ts_unallocated, tags);
+		tagscan_pageset_stats_update(&tps_unallocated, tags);
 		return;
 	}
-	tagscan_rescan_page_update_stats(&ts_allocated, tags);
+	tagscan_pageset_stats_update(&tps_allocated, tags);
 	obj = atomic_load_ptr(&m->object);
 	if (obj != NULL) {
-		tagscan_rescan_page_update_stats(&ts_object, tags);
+		tagscan_pageset_stats_update(&tps_object, tags);
 		if (obj == kernel_object)
-			tagscan_rescan_page_update_stats(&ts_kernel_object,
+			tagscan_pageset_stats_update(&tps_kernel_object,
 			    tags);
 
 		switch (obj->type) {
 		case OBJT_SWAP:
-			tagscan_rescan_page_update_stats(&ts_swap_object,
-			    tags);
+			tagscan_pageset_stats_update(&tps_swap_object, tags);
 			break;
 
 		case OBJT_VNODE:
-			tagscan_rescan_page_update_stats(&ts_vnode_object,
-			    tags);
+			tagscan_pageset_stats_update(&tps_vnode_object, tags);
 			break;
 
 		case OBJT_PHYS:
-			tagscan_rescan_page_update_stats(&ts_phys_object,
-			    tags);
+			tagscan_pageset_stats_update(&tps_phys_object, tags);
 			break;
 
 		case OBJT_DEVICE:
-			tagscan_rescan_page_update_stats(&ts_device_object,
+			tagscan_pageset_stats_update(&tps_device_object,
 			    tags);
 
 		default:
-			tagscan_rescan_page_update_stats(&ts_misc_object,
-			    tags);
+			tagscan_pageset_stats_update(&tps_misc_object, tags);
 		}
 
 		if (obj->flags & OBJ_ANON)
-			tagscan_rescan_page_update_stats(&ts_anon_object,
-			    tags);
+			tagscan_pageset_stats_update(&tps_anon_object, tags);
 	} else
-		tagscan_rescan_page_update_stats(&ts_no_object, tags);
+		tagscan_pageset_stats_update(&tps_no_object, tags);
 }
 
 /*
@@ -191,27 +187,27 @@ tagscan_rescan_page(vm_page_t m)
  * XXX: Some values here use floor rather than proper rounding.
  */
 static void
-tagscan_recalculate_stats(struct tagscan_stats *tsp)
+tagscan_pageset_stats_recalculate(struct tagscan_pageset_stats *tpsp)
 {
 	uint64_t percent;
 
 	/*
 	 * Overall percentage of system memory this type takes up.
 	 *
-	 * Note the absolute reference to ts_global here.
+	 * Note the absolute reference to tps_global here.
 	 */
-	percent = (1000 * tsp->ts_pages_count_scanned) /
-	    ts_global.ts_pages_count_scanned;
-	tsp->ts_percent_of_global_memory_int = percent / 10;
-	tsp->ts_percent_of_global_memory_frac = percent % 10;
+	percent = (1000 * tpsp->tps_pages_count_scanned) /
+	    tps_global.tps_pages_count_scanned;
+	tpsp->tps_percent_of_global_memory_int = percent / 10;
+	tpsp->tps_percent_of_global_memory_frac = percent % 10;
 
 	/*
 	 * Calculate % of pages that contain one or more tagged values.
 	 */
-	percent = (1000 * tsp->ts_pages_count_with_tags) /
-	    tsp->ts_pages_count_scanned;
-	tsp->ts_pages_percent_with_tags_int = percent / 10;
-	tsp->ts_pages_percent_with_tags_frac = percent % 10;
+	percent = (1000 * tpsp->tps_pages_count_with_tags) /
+	    tpsp->tps_pages_count_scanned;
+	tpsp->tps_pages_percent_with_tags_int = percent / 10;
+	tpsp->tps_pages_percent_with_tags_frac = percent % 10;
 
 	/*
 	 * Estimate of the size taken up by tag storage for pages with tagged
@@ -221,28 +217,28 @@ tagscan_recalculate_stats(struct tagscan_stats *tsp)
 	 *
 	 * 2. What overhead does that represent against the total memory size?
 	 */
-	tsp->ts_pages_tagstore_overhead_bytes =
-	    (tsp->ts_pages_count_with_tags *
+	tpsp->tps_pages_tagstore_overhead_bytes =
+	    (tpsp->tps_pages_count_with_tags *
 	    (PAGE_SIZE / sizeof(uintcap_t))) / NBBY;
-	percent = (1000 * tsp->ts_pages_tagstore_overhead_bytes) /
-	    (tsp->ts_pages_count_scanned * PAGE_SIZE);
-	tsp->ts_pages_tagstore_overhead_percent_int = percent / 10;
-	tsp->ts_pages_tagstore_overhead_percent_frac = percent % 10;
+	percent = (1000 * tpsp->tps_pages_tagstore_overhead_bytes) /
+	    (tpsp->tps_pages_count_scanned * PAGE_SIZE);
+	tpsp->tps_pages_tagstore_overhead_percent_int = percent / 10;
+	tpsp->tps_pages_tagstore_overhead_percent_frac = percent % 10;
 
 	/*
 	 * Calculate % of capability-width words that contain tagged values.
 	 */
 	percent =
-	    (1000 * tsp->ts_words_count_with_tags) /
-	    tsp->ts_words_count_scanned;
-	tsp->ts_words_count_percent_with_tags_int = percent / 10;
-	tsp->ts_words_count_percent_with_tags_frac = percent % 10;
+	    (1000 * tpsp->tps_words_count_with_tags) /
+	    tpsp->tps_words_count_scanned;
+	tpsp->tps_words_count_percent_with_tags_int = percent / 10;
+	tpsp->tps_words_count_percent_with_tags_frac = percent % 10;
 
 	/*
 	 * Estimate bytes of memory overhead due to tagged values.
 	 */
-	tsp->ts_words_memory_overhead_bytes = tsp->ts_words_count_with_tags *
-	    (sizeof(uintcap_t) / 2);
+	tpsp->tps_words_memory_overhead_bytes =
+	    tpsp->tps_words_count_with_tags * (sizeof(uintcap_t) / 2);
 
 	/*
 	 * Estimate percent of memory overhead due to tagged values.
@@ -255,18 +251,18 @@ tagscan_recalculate_stats(struct tagscan_stats *tsp)
 	 *    what is the percentage overhead of the subtracted capability
 	 *    metadata?
 	 */
-	tsp->ts_bytes_count_scanned =
-	    tsp->ts_words_count_scanned * sizeof(uintcap_t);
-	tsp->ts_bytes_count_in_capabilities =
-	    tsp->ts_words_count_with_tags * sizeof(uintcap_t);
-	tsp->ts_words_memory_overhead_bytes =
-	    tsp->ts_bytes_count_in_capabilities / 2;
+	tpsp->tps_bytes_count_scanned =
+	    tpsp->tps_words_count_scanned * sizeof(uintcap_t);
+	tpsp->tps_bytes_count_in_capabilities =
+	    tpsp->tps_words_count_with_tags * sizeof(uintcap_t);
+	tpsp->tps_words_memory_overhead_bytes =
+	    tpsp->tps_bytes_count_in_capabilities / 2;
 
-	percent = (1000 * tsp->ts_words_memory_overhead_bytes) /
-	    (tsp->ts_bytes_count_scanned -
-	    tsp->ts_words_memory_overhead_bytes);
-	tsp->ts_words_memory_overhead_percent_int = percent / 10;
-	tsp->ts_words_memory_overhead_percent_frac = percent % 10;
+	percent = (1000 * tpsp->tps_words_memory_overhead_bytes) /
+	    (tpsp->tps_bytes_count_scanned -
+	    tpsp->tps_words_memory_overhead_bytes);
+	tpsp->tps_words_memory_overhead_percent_int = percent / 10;
+	tpsp->tps_words_memory_overhead_percent_frac = percent % 10;
 }
 
 static void
@@ -278,32 +274,32 @@ tagscan_rescan(void)
 	 * XXXRW: One could do it into temporary structs and then swap in
 	 * to prevent intermediate values being overly visible?
 	 */
-	bzero(&ts_global, sizeof(ts_global));
-	bzero(&ts_allocated, sizeof(ts_allocated));
-	bzero(&ts_unallocated, sizeof(ts_unallocated));
-	bzero(&ts_object, sizeof(ts_object));
-	bzero(&ts_kernel_object, sizeof(ts_kernel_object));
-	bzero(&ts_swap_object, sizeof(ts_swap_object));
-	bzero(&ts_anon_object, sizeof(ts_anon_object));
-	bzero(&ts_vnode_object, sizeof(ts_vnode_object));
-	bzero(&ts_phys_object, sizeof(ts_phys_object));
-	bzero(&ts_device_object, sizeof(ts_device_object));
-	bzero(&ts_no_object, sizeof(ts_misc_object));
-	bzero(&ts_no_object, sizeof(ts_no_object));
+	bzero(&tps_global, sizeof(tps_global));
+	bzero(&tps_allocated, sizeof(tps_allocated));
+	bzero(&tps_unallocated, sizeof(tps_unallocated));
+	bzero(&tps_object, sizeof(tps_object));
+	bzero(&tps_kernel_object, sizeof(tps_kernel_object));
+	bzero(&tps_swap_object, sizeof(tps_swap_object));
+	bzero(&tps_anon_object, sizeof(tps_anon_object));
+	bzero(&tps_vnode_object, sizeof(tps_vnode_object));
+	bzero(&tps_phys_object, sizeof(tps_phys_object));
+	bzero(&tps_device_object, sizeof(tps_device_object));
+	bzero(&tps_no_object, sizeof(tps_misc_object));
+	bzero(&tps_no_object, sizeof(tps_no_object));
 	for (pi = 0; pi < vm_page_array_size; pi++)
 		tagscan_rescan_page(&vm_page_array[pi]);
-	tagscan_recalculate_stats(&ts_global);
-	tagscan_recalculate_stats(&ts_allocated);
-	tagscan_recalculate_stats(&ts_unallocated);
-	tagscan_recalculate_stats(&ts_object);
-	tagscan_recalculate_stats(&ts_kernel_object);
-	tagscan_recalculate_stats(&ts_swap_object);
-	tagscan_recalculate_stats(&ts_anon_object);
-	tagscan_recalculate_stats(&ts_vnode_object);
-	tagscan_recalculate_stats(&ts_phys_object);
-	tagscan_recalculate_stats(&ts_device_object);
-	tagscan_recalculate_stats(&ts_misc_object);
-	tagscan_recalculate_stats(&ts_no_object);
+	tagscan_pageset_stats_recalculate(&tps_global);
+	tagscan_pageset_stats_recalculate(&tps_allocated);
+	tagscan_pageset_stats_recalculate(&tps_unallocated);
+	tagscan_pageset_stats_recalculate(&tps_object);
+	tagscan_pageset_stats_recalculate(&tps_kernel_object);
+	tagscan_pageset_stats_recalculate(&tps_swap_object);
+	tagscan_pageset_stats_recalculate(&tps_anon_object);
+	tagscan_pageset_stats_recalculate(&tps_vnode_object);
+	tagscan_pageset_stats_recalculate(&tps_phys_object);
+	tagscan_pageset_stats_recalculate(&tps_device_object);
+	tagscan_pageset_stats_recalculate(&tps_misc_object);
+	tagscan_pageset_stats_recalculate(&tps_no_object);
 }
 
 static int
@@ -323,40 +319,41 @@ SYSCTL_PROC(_dev_tagscan, OID_AUTO, rescan,
     &sysctl_dev_tagscan_rescan, "I", "Trigger tag rescan");
 
 static void
-tagscan_dump_stats(struct sbuf *sb, struct tagscan_stats *tsp)
+tagscan_pageset_stats_dump(struct sbuf *sb,
+    struct tagscan_pageset_stats *tpsp)
 {
 
 	sbuf_printf(sb, "  pages_count_scanned: %ju\n",
-	    tsp->ts_pages_count_scanned);
+	    tpsp->tps_pages_count_scanned);
 	sbuf_printf(sb, "  pages_count_with_tags: %ju\n",
-	    tsp->ts_pages_count_with_tags);
+	    tpsp->tps_pages_count_with_tags);
 	sbuf_printf(sb, "  pages_percent_with_tags: %ju.%01ju\n",
-	    tsp->ts_pages_percent_with_tags_int,
-	    tsp->ts_pages_percent_with_tags_frac);
+	    tpsp->tps_pages_percent_with_tags_int,
+	    tpsp->tps_pages_percent_with_tags_frac);
 	sbuf_printf(sb, "  pages_tagstore_overhead_bytes: %ju\n",
-	    tsp->ts_pages_tagstore_overhead_bytes);
+	    tpsp->tps_pages_tagstore_overhead_bytes);
 	sbuf_printf(sb, "  pages_tagstore_overhead_percent: %ju.%01ju\n",
-	    tsp->ts_pages_tagstore_overhead_percent_int,
-	    tsp->ts_pages_tagstore_overhead_percent_frac);
+	    tpsp->tps_pages_tagstore_overhead_percent_int,
+	    tpsp->tps_pages_tagstore_overhead_percent_frac);
 	sbuf_printf(sb, "  words_count_scanned: %ju\n",
-	    tsp->ts_words_count_scanned);
+	    tpsp->tps_words_count_scanned);
 	sbuf_printf(sb, "  words_count_with_tags: %ju\n",
-	    tsp->ts_words_count_with_tags);
+	    tpsp->tps_words_count_with_tags);
 	sbuf_printf(sb, "  words_percent_with_tags: %ju.%01ju\n",
-	    tsp->ts_words_count_percent_with_tags_int,
-	    tsp->ts_words_count_percent_with_tags_frac);
+	    tpsp->tps_words_count_percent_with_tags_int,
+	    tpsp->tps_words_count_percent_with_tags_frac);
 	sbuf_printf(sb, "  words_memory_overhead_bytes: %ju\n",
-	    tsp->ts_words_memory_overhead_bytes);
+	    tpsp->tps_words_memory_overhead_bytes);
 	sbuf_printf(sb, "  words_memory_overhead_percent: %ju.%01ju\n",
-	    tsp->ts_words_memory_overhead_percent_int,
-	    tsp->ts_words_memory_overhead_percent_frac);
+	    tpsp->tps_words_memory_overhead_percent_int,
+	    tpsp->tps_words_memory_overhead_percent_frac);
 	sbuf_printf(sb, "  bytes_count_scanned: %ju\n",
-	    tsp->ts_bytes_count_scanned);
+	    tpsp->tps_bytes_count_scanned);
 	sbuf_printf(sb, "  bytes_count_in_capabilities: %ju\n",
-	    tsp->ts_bytes_count_in_capabilities);
+	    tpsp->tps_bytes_count_in_capabilities);
 	sbuf_printf(sb, "  percent_of_global_memory: %ju.%01ju\n",
-	    tsp->ts_percent_of_global_memory_int,
-	    tsp->ts_percent_of_global_memory_frac);
+	    tpsp->tps_percent_of_global_memory_int,
+	    tpsp->tps_percent_of_global_memory_frac);
 }
 
 static int
@@ -367,29 +364,29 @@ sysctl_dev_tagscan_dump(SYSCTL_HANDLER_ARGS)
 
 	sbuf_new(&sb, NULL, 256, SBUF_AUTOEXTEND | SBUF_INCLUDENUL);
 	sbuf_printf(&sb, "global:\n");
-	tagscan_dump_stats(&sb, &ts_global);
+	tagscan_pageset_stats_dump(&sb, &tps_global);
 	sbuf_printf(&sb, "allocated:\n");
-	tagscan_dump_stats(&sb, &ts_allocated);
+	tagscan_pageset_stats_dump(&sb, &tps_allocated);
 	sbuf_printf(&sb, "unallocated:\n");
-	tagscan_dump_stats(&sb, &ts_unallocated);
+	tagscan_pageset_stats_dump(&sb, &tps_unallocated);
 	sbuf_printf(&sb, "object:\n");
-	tagscan_dump_stats(&sb, &ts_object);
+	tagscan_pageset_stats_dump(&sb, &tps_object);
 	sbuf_printf(&sb, "no_object:\n");
-	tagscan_dump_stats(&sb, &ts_no_object);
+	tagscan_pageset_stats_dump(&sb, &tps_no_object);
 	sbuf_printf(&sb, "phys_object:\n");
-	tagscan_dump_stats(&sb, &ts_phys_object);
+	tagscan_pageset_stats_dump(&sb, &tps_phys_object);
 	sbuf_printf(&sb, "swap_object:\n");
-	tagscan_dump_stats(&sb, &ts_swap_object);
+	tagscan_pageset_stats_dump(&sb, &tps_swap_object);
 	sbuf_printf(&sb, "anon_object:\n");
-	tagscan_dump_stats(&sb, &ts_anon_object);
+	tagscan_pageset_stats_dump(&sb, &tps_anon_object);
 	sbuf_printf(&sb, "vnode_object:\n");
-	tagscan_dump_stats(&sb, &ts_vnode_object);
+	tagscan_pageset_stats_dump(&sb, &tps_vnode_object);
 	sbuf_printf(&sb, "device_object:\n");
-	tagscan_dump_stats(&sb, &ts_device_object);
+	tagscan_pageset_stats_dump(&sb, &tps_device_object);
 	sbuf_printf(&sb, "kernel_object:\n");
-	tagscan_dump_stats(&sb, &ts_kernel_object);
+	tagscan_pageset_stats_dump(&sb, &tps_kernel_object);
 	sbuf_printf(&sb, "misc_object:\n");
-	tagscan_dump_stats(&sb, &ts_misc_object);
+	tagscan_pageset_stats_dump(&sb, &tps_misc_object);
 	error = sbuf_finish(&sb);
 	if (error == 0)
 		error = SYSCTL_OUT(req, sbuf_data(&sb), sbuf_len(&sb));
