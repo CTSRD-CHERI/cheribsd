e20e9262be9975fdf2947d10fc27e70325be2eba
Brooks Davis <brooks@one-eyed-alien.net>
2026-05-28 15:04:47 +0100

    remove __kerncap from tgot header

--- sys/sys/_tls_tgot.h
+++ sys/sys/_tls_tgot.h
@@ -51,8 +51,8 @@ struct dtv {
 };
 
 struct tcb {
-	struct dtv		* __kerncap tcb_dtv;	/* required by rtld */
-	struct pthread		* __kerncap tcb_thread;
+	struct dtv		*tcb_dtv;	/* required by rtld */
+	struct pthread		*tcb_thread;
 };
 
 #define	TLS_TCB_SIZE	sizeof(struct tcb)
