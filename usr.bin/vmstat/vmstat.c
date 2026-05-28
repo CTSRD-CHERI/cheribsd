84d13b9f1e4b36ede6d6eecd3749124f9b7dc41d
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2026-05-28 17:30:57 +0100

    vmstat: introduce the -k flag
    The flag uses kinfo_getcompartments(3) to get and print information on
    kernel compartments, if the running kernel is compartmentalized and
    handles the kern.kc18n_compartments sysctl.

--- usr.bin/vmstat/vmstat.c
+++ usr.bin/vmstat/vmstat.c
@@ -47,6 +47,8 @@
 
 #include <vm/vm_param.h>
 
+#include <cheri/c18n.h>
+
 #include <ctype.h>
 #include <devstat.h>
 #include <errno.h>
@@ -170,6 +172,7 @@ static kvm_t	*kd;
 #define	VMSTAT		0x20
 #define	ZMEMSTAT	0x40
 #define	OBJSTAT		0x80
+#define	COMPARTMENTS	0x100
 
 static void	cpustats(void);
 static void	pcpustats(u_long, int);
@@ -179,6 +182,7 @@ static void	dointr(unsigned int, int);
 static void	doobjstat(void);
 static void	dosum(void);
 static void	dovmstat(unsigned int, int);
+static void	docompartments(void);
 static void	domemstat_malloc(void);
 static void	domemstat_zone(void);
 static void	kread(int, void *, size_t);
@@ -215,7 +219,7 @@ main(int argc, char *argv[])
 
 	hflag = isatty(1);
 
-	while ((c = getopt(argc, argv, "ac:fhHiM:mN:n:oPp:sw:z")) != -1) {
+	while ((c = getopt(argc, argv, "ac:fhHiM:kmN:n:oPp:sw:z")) != -1) {
 		switch (c) {
 		case 'a':
 			aflag++;
@@ -237,6 +241,9 @@ main(int argc, char *argv[])
 			break;
 		case 'i':
 			todo |= INTRSTAT;
+			break;
+		case 'k':
+			todo |= COMPARTMENTS;
 			break;
 		case 'M':
 			memf = optarg;
@@ -385,6 +392,8 @@ nlist_ok:
 		dointr(interval, reps);
 	if (todo & VMSTAT)
 		dovmstat(interval, reps);
+	if (todo & COMPARTMENTS)
+		docompartments();
 	xo_close_container("vmstat");
 	if (xo_finish() < 0)
 		xo_err(EXIT_FAILURE, "stdout");
@@ -1589,6 +1598,35 @@ doobjstat(void)
 	xo_close_list("object");
 }
 
+static void
+display_compartment(struct kinfo_cheri_kc18n_compart *kckc)
+{
+	xo_open_instance("compartment");
+	xo_emit("{:id/%5ju} ", (uintmax_t)kckc->kckc_id);
+	xo_emit("{:flags/%5s} ", kckc->kckc_executive ? "E" : "-");
+	xo_emit("{:name/%-s}\n", kckc->kckc_name);
+	xo_close_instance("compartment");
+}
+
+static void
+docompartments(void)
+{
+	struct kinfo_cheri_kc18n_compart *kckc;
+	int cnt, i;
+
+	kckc = kinfo_getcompartments(&cnt);
+	if (kckc == NULL) {
+		xo_warn("Failed to fetch kernel compartments list");
+		return;
+	}
+	xo_emit("{T:ID/%5s} {T:FLAGS/%5s} {T:NAME/%s}\n");
+	xo_open_list("compartment");
+	for (i = 0; i < cnt; i++)
+		display_compartment(&kckc[i]);
+	free(kckc);
+	xo_close_list("compartment");
+}
+
 /*
  * kread reads something from the kernel, given its nlist index.
  */
@@ -1631,7 +1669,7 @@ static void __dead2
 usage(void)
 {
 	xo_error("%s%s",
-	    "usage: vmstat [-afHhimoPsz] [-M core [-N system]] [-c count] [-n devs]\n",
+	    "usage: vmstat [-afHhikmoPsz] [-M core [-N system]] [-c count] [-n devs]\n",
 	    "              [-p type,if,pass] [-w wait] [disks] [wait [count]]\n");
 	if (xo_finish() < 0)
 		xo_err(EXIT_FAILURE, "stdout");
