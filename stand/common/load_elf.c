f4949ccb708fdb961d956f9bcf12e61f11944851
Jessica Clarke <jrtc27@jrtc27.com>
2026-06-11 09:45:07 -0700

    stand: Pass loaded program headers in new MODINFOMD_PHDR module metadata

--- stand/common/load_elf.c
+++ stand/common/load_elf.c
@@ -755,6 +755,8 @@ __elfN(loadimage)(struct preloaded_file *fp, elf_file_t ef, uint64_t off)
 	}
 	lastaddr = roundup(lastaddr, sizeof(long));
 
+	file_addmetadata(fp, MODINFOMD_PHDR, ef->phdrslen, phdr)
+
 	/*
 	 * Get the section headers.  We need this for finding the .ctors
 	 * section as well as for loading any symbols.  Both may be hard
