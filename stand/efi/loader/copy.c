a85dc160da608961a67b733debe5abba09212152
Jessica Clarke <jrtc27@jrtc27.com>
2026-06-11 16:35:17 -0700

    stand: Respect segment misalignment in efi_copyin / efi_readin

--- stand/efi/loader/copy.c
+++ stand/efi/loader/copy.c
@@ -478,6 +478,10 @@ efi_copyin(const void *src, vm_offset_t dest, const size_t len)
 
 	if (!stage_offset_set) {
 		stage_offset = (vm_offset_t)staging - dest;
+#if EFI_STAGING_2M_ALIGN
+		/* Keep stage_offset congruent to dest mod 2M */
+		stage_offset += dest & (M(2) - 1);
+#endif
 		stage_offset_set = true;
 	}
 
@@ -509,6 +513,10 @@ efi_readin(readin_handle_t fd, vm_offset_t dest, const size_t len)
 
 	if (!stage_offset_set) {
 		stage_offset = (vm_offset_t)staging - dest;
+#if EFI_STAGING_2M_ALIGN
+		/* Keep stage_offset congruent to dest mod 2M */
+		stage_offset += dest & (M(2) - 1);
+#endif
 		stage_offset_set = true;
 	}
 
