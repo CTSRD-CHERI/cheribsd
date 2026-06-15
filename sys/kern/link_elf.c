dc9053c3e50eab785f42670f194dd3ceac363afe
Jessica Clarke <jrtc27@jrtc27.com>
2026-06-15 01:41:12 +0100

    link_elf: Turn have_phdrs into preload_search_phdrs
    This ensures that the code to find the phdrs is kept consistent with the
    code to determine whether you can find the phdrs, rather than requiring
    the code to find the phdrs know what method has been determined to work.
    This also makes the code to find the phdrs reusable when merged into the
    kernel compartmentalisation branch, rather than creating more copies of
    it.

--- sys/kern/link_elf.c
+++ sys/kern/link_elf.c
@@ -478,23 +478,30 @@ ef_symbol_address(elf_file_t ef, const Elf_Sym *sym)
 
 #ifdef __CHERI_PURE_CAPABILITY__
 /*
- * Check for a valid ELF executable header at ef->mapbase.
+ * Find a copy of the module's program headers by probing for a valid ELF
+ * executable header at ef->mapbase that we can then follow.
  *
  * NB: RISC-V kernels do not map phdrs into memory.
  */
-static bool
-have_phdrs(elf_file_t ef)
+static const Elf_Phdr *
+preload_search_phdrs(elf_file_t ef, size_t *phnum)
 {
 	const Elf_Ehdr *hdr;
+
 	hdr = (const Elf_Ehdr *)ef->mapbase;
-	return (IS_ELF(*hdr) &&
+	if (IS_ELF(*hdr) &&
 	    hdr->e_ident[EI_CLASS] == ELF_TARG_CLASS &&
 	    hdr->e_ident[EI_DATA] == ELF_TARG_DATA &&
 	    hdr->e_ident[EI_VERSION] == EV_CURRENT &&
 	    hdr->e_machine == ELF_TARG_MACH &&
 	    hdr->e_version == ELF_TARG_VER &&
 	    hdr->e_phentsize == sizeof(Elf_Phdr) &&
-	    ELF_IS_CHERI(hdr));
+	    ELF_IS_CHERI(hdr)) {
+		*phnum = hdr->e_phnum;
+		return ((const Elf_Phdr *)((const char *)hdr + hdr->e_phoff));
+	}
+
+	return (NULL);
 }
 
 static bool
@@ -555,15 +562,14 @@ ef_create_pcc_caps(elf_file_t ef, const Elf_Phdr *phstart,
 static bool
 preload_init_pcc_caps(elf_file_t ef)
 {
-	const Elf_Ehdr *hdr;
 	const Elf_Phdr *phdr;
+	size_t phnum;
 
-	if (!have_phdrs(ef))
+	phdr = preload_search_phdrs(ef, &phnum);
+	if (phdr == NULL)
 		return (true);
 
-	hdr = (const Elf_Ehdr *)ef->mapbase;
-	phdr = (const Elf_Phdr *)((const char *)hdr + hdr->e_phoff);
-	return (ef_create_pcc_caps(ef, phdr, phdr + hdr->e_phnum));
+	return (ef_create_pcc_caps(ef, phdr, phdr + phnum));
 }
 #endif
 
