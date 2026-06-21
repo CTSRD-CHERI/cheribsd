8226d2f6f4bdc858d3f43ccb7abd12bb5ce3f3e2
Brooks Davis <brooks@one-eyed-alien.net>
2026-06-21 18:34:53 +0100

    rtld: remove an unneeded diff

--- libexec/rtld-elf/map_object.c
+++ libexec/rtld-elf/map_object.c
@@ -265,9 +265,8 @@ map_object(int fd, const char *path, const struct stat *sb, bool ismain,
 		data_prot = convert_prot(segs[i]->p_flags);
 		data_flags = convert_flags(segs[i]->p_flags) | MAP_FIXED;
 
-		size_t data_len = data_vlimit - data_vaddr;
-		if (data_len != 0 && mmap(data_addr,
-		    data_len, data_prot, data_flags |
+		if (data_vlimit != data_vaddr && mmap(data_addr,
+		    data_vlimit - data_vaddr, data_prot, data_flags |
 		    MAP_PREFAULT_READ, fd, data_offset) == MAP_FAILED) {
 			_rtld_error("%s: mmap of data failed: %s",
 			    path, rtld_strerror(errno));
