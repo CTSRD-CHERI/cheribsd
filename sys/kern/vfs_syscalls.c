f5c2e95bdc0b0c3b03b293c42bbcafcb1a7e331e
Mark Johnston <markj@FreeBSD.org>
2026-06-23 20:43:53 +0000

    unlinkat: Pass flags down to the work functions
    The unlinkat(2) and funlinkat(2) system calls were ignoring
    AT_RESOLVE_BENEATH.
    
    Also pass pathseg through instead of assuming it's UIO_USERSPACE.
    
    Add some tests which make sure that AT_RESOLVE_BENEATH is handled
    properly by various system calls.
    
    Approved by:	so
    Security:	FreeBSD-SA-26:42.unlinkat
    Security:	CVE-2026-49421
    Reported by:	Yuxiang Yang, Yizhou Zhao, Ao Wang, Xuewei Feng, Qi Li, and Ke Xu from Tsinghua University using GLM-5.1 from Z.ai
    Reviewed by:	kib
    Sponsored by:	The FreeBSD Foundation
    Differential Revision:	https://reviews.freebsd.org/D57790
    
    (cherry picked from commit 8b24b19d08f9dc10e6af9a1695b9b0d62fb80cc2)

--- sys/kern/vfs_syscalls.c
+++ sys/kern/vfs_syscalls.c
@@ -2021,16 +2021,17 @@ sys_unlink(struct thread *td, struct unlink_args *uap)
 
 int
 kern_funlinkat_ex(struct thread *td, int dfd, const char * __capability path,
-    int fd, int flag, enum uio_seg pathseg, ino_t oldinum)
+    int fd, int flags, enum uio_seg pathseg, ino_t oldinum)
 {
 
-	if ((flag & ~(AT_REMOVEDIR | AT_RESOLVE_BENEATH)) != 0)
+	if ((flags & ~(AT_REMOVEDIR | AT_RESOLVE_BENEATH)) != 0)
 		return (EINVAL);
 
-	if ((flag & AT_REMOVEDIR) != 0)
-		return (kern_frmdirat(td, dfd, path, fd, UIO_USERSPACE, 0));
+	if ((flags & AT_REMOVEDIR) != 0)
+		return (kern_frmdirat(td, dfd, path, fd, pathseg,
+		    flags & ~AT_REMOVEDIR));
 
-	return (kern_funlinkat(td, dfd, path, fd, UIO_USERSPACE, 0, 0));
+	return (kern_funlinkat(td, dfd, path, fd, pathseg, flags, 0));
 }
 
 #ifndef _SYS_SYSPROTO_H_
