99bf45c219a7df4afbc160252065c670b9123638
Robert N. M. Watson <robert.watson@cl.cam.ac.uk>
2026-07-19 18:40:34 +0100

    Fix up JSON and XML renderings of mrs data.

--- usr.bin/procstat/procstat_mrs.c
+++ usr.bin/procstat/procstat_mrs.c
@@ -63,7 +63,7 @@ procstat_mrs(struct procstat *procstat, struct kinfo_proc *kipp)
 			    "START");
 	}
 
-	xo_emit("{k:process_id/%5d/%d}", kipp->ki_pid);
+	xo_emit("{:process_id/%5d/%d}", kipp->ki_pid);
 	xo_emit(" {:command/%-19s/%s}", kipp->ki_comm);
 
 	if (procstat_getmrs(procstat, kipp, &cms) != 0 ||
@@ -86,40 +86,40 @@ procstat_mrs(struct procstat *procstat, struct kinfo_proc *kipp)
 		xo_emit("\n");
 		return;
 	}
-	xo_emit(" {:mrs_flags/%c%c%c%c%c/%c%c%c%c%c%c}",
+	xo_emit(" {:mrs_flags/%c%c%c%c%c/%c%c%c%c%c}",
 	    (cms.cms_mrs_flags & CHERI_MRS_FLAGS_ASYNCREVOKE) ? 'a' : '-',
 	    (cms.cms_mrs_flags & CHERI_MRS_FLAGS_BOUNDPTRS) ? 'b' : '-',
 	    (cms.cms_mrs_flags & CHERI_MRS_FLAGS_EVERYFREE) ? 'e' : '-',
 	    (cms.cms_mrs_flags & CHERI_MRS_FLAGS_ABORTONFAIL) ? 'f' : '-',
 	    (cms.cms_mrs_flags & CHERI_MRS_FLAGS_QUARANTINING) ? 'q' : '-');
-	xo_emit(" {:mrs_max_ratio/%4zu/%s}",
+	xo_emit(" {:mrs_max_ratio/%4zu/%zu}",
 	    (100 * cms.cms_mrs_quarantine_numerator) /
 	    cms.cms_mrs_quarantine_denominator);
-	xo_emit(" {:mrs_ratio/%4zu/%s}",
+	xo_emit(" {:mrs_ratio/%4zu/%zu}",
 	    (100 * cms.cms_mrs_bytes_inquarantine) /
 	    (cms.cms_mrs_bytes_inquarantine +
 	    cms.cms_mrs_bytes_inheap));
-	xo_emit(" {:mrs_count_inheap/%7zu/%s}",
+	xo_emit(" {:mrs_count_inheap/%7zu/%zu",
 	    cms.cms_mrs_count_inheap);
-	xo_emit(" {:mrs_bytes_inheap/%9zu/%s}",
+	xo_emit(" {:mrs_bytes_inheap/%9zu/%zu}",
 	    cms.cms_mrs_bytes_inheap);
-	xo_emit(" {:mrs_count_inquarantine/%7zu/%s}",
+	xo_emit(" {:mrs_count_inquarantine/%7zu/%zu}",
 	    cms.cms_mrs_count_inquarantine);
-	xo_emit(" {:mrs_bytes_inquarantine/%9zu/%s}",
+	xo_emit(" {:mrs_bytes_inquarantine/%9zu/%zu}",
 	    cms.cms_mrs_bytes_inquarantine);
 	if ((procstat_opts & PS_OPT_VERBOSE) != 0) {
-		xo_emit(" {:mrs_allocated_size/%9zu/%s}",
+		xo_emit(" {:mrs_allocated_size/%9zu/%zu}",
 		    cms.cms_mrs_allocated_size);
-		xo_emit(" {:mrs_max_allocated_size/%9zu/%s}",
+		xo_emit(" {:mrs_max_allocated_size/%9zu/%zu}",
 		    cms.cms_mrs_max_allocated_size);
-		xo_emit(" {:mrs_revocation_minimum/%9zu/%s}",
+		xo_emit(" {:mrs_revocation_minimum/%9zu/%zu}",
 		    cms.cms_mrs_revocation_minimum);
-		xo_emit(" {:mrs_epoch/%6zu/%s}",
+		xo_emit(" {:mrs_epoch/%6zu/%zu}",
 		    cms.cms_mrs_epoch);
 		kernel_epoch = get_revoker_epoch(procstat, kipp);
 		xo_emit(" {:kernel_epoch/%6s/%s}",
 		    kernel_epoch != NULL ? kernel_epoch : "-");
-		xo_emit(" {:mrs_s_start/%10u.%09u}",
+		xo_emit(" {:mrs_s_start/%10u.%09u/%10u.%09u}",
 		    cms.cms_mrs_ts_start.tv_sec,
 		    cms.cms_mrs_ts_start.tv_nsec);
 	}
