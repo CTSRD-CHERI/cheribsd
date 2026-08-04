09900f354462cf475914d16d30539d70a4c03fd2
John Baldwin <jhb@FreeBSD.org>
2026-08-04 15:11:03 -0400

    rtld: Use AT_PHDR to derive data capabilities in the direct-exec case

--- libexec/rtld-elf/rtld.c
+++ libexec/rtld-elf/rtld.c
@@ -3113,6 +3113,19 @@ init_rtld(caddr_t mapbase, Elf_Auxinfo **aux_info)
 	const Elf_Dyn *dyn_soname;
 	const Elf_Dyn *dyn_runpath;
 
+#ifdef __CHERI_PURE_CAPABILITY__
+	/*
+	 * In the direct-exec case, derive mapbase from AT_PHDR rather
+	 * than AT_BASE.
+	 */
+	if (aux_info[AT_PHDR] != NULL &&
+	    cheri_is_address_inbounds(aux_info[AT_PHDR]->a_un.a_ptr,
+	    (uintptr_t)mapbase)) {
+		mapbase = cheri_address_copy(aux_info[AT_PHDR]->a_un.a_ptr,
+		    mapbase);
+	}
+#endif
+
 	/*
 	 * Conjure up an Obj_Entry structure for the dynamic linker.
 	 *
