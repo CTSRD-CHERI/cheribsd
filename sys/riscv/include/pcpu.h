7f4f09c99de8f2aaab33cf741609fa4231cdea29
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2026-08-22 16:18:20 +0100

    cheri-rv64y: Update pcpu assembly for RVY 0.9.9.

--- sys/riscv/include/pcpu.h
+++ sys/riscv/include/pcpu.h
@@ -66,10 +66,10 @@ get_pcpu(void)
 	struct pcpu *pcpu;
 
 #ifdef __CHERI_PURE_CAPABILITY__
-#ifdef __riscv_xcheri
-	__asm __volatile("cmove %0, ctp" : "=&C"(pcpu));
+#ifdef __riscv_y
+        __asm __volatile("ymv %0, tp" : "=&C"(pcpu));
 #else
-        __asm __volatile("cmv %0, ctp" : "=&C"(pcpu));
+	__asm __volatile("cmv %0, ctp" : "=&C"(pcpu));
 #endif
 #else
 	__asm __volatile("mv %0, tp" : "=&r"(pcpu));
@@ -84,8 +84,8 @@ get_curthread(void)
 	struct thread *td;
 
 #ifdef __CHERI_PURE_CAPABILITY__
-#ifdef __riscv_xcheri
-	__asm __volatile("clc %0, 0(ctp)" : "=&C"(td));
+#ifdef __riscv_y
+	__asm __volatile("ly %0, 0(tp)" : "=&C"(td));
 #else
 	__asm __volatile("lc %0, 0(ctp)" : "=&C"(td));
 #endif
